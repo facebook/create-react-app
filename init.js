f0c3888779fd1cd29a30b8acae4780e3a089757e
Christopher Chedeau <vjeux@fb.com>
2016-07-15 14:23:37 -0700

    .
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:
    
    Task ID: #

--- init.js
+++ init.js
@@ -1,3 +1,24 @@
-module.exports = function(root, appName) {
-  console.log('Creating the app', appName, 'at', root);
+module.exports = function(hostPath, appName) {
+  var selfPath = hostPath + '/node_modules/create-react-app-scripts';
+
+  var hostPackage = require(hostPath + '/package.json');
+  var selfPackage = require(selfPath + '/package.json');
+
+  // Copy over devDependencies
+  for (var key in selfPackage.devDependencies) {
+    hostPackage.dependencies[key] = selfPackage.devDependencies[key];
+  }
+
+  // Setup the script rules
+  hostPackage.scripts = {};
+  ['start', 'build', 'publish-gh-pages'].forEach(function(command) {
+    hostPackage.scripts[command] = 'node node_modules/create-react-app-scripts/' + command + '.js';
+  });
+
+  fs.writeFileSync(hostPath + '/package.json', JSON.stringify(hostPackage, null, 2));
+
+  // Move the src folder
+  fs.renameSync(selfPackage + '/src', hostPackage + '/src');
+
+  console.log('Creating the app', appName, 'at', hostPath);
 };
