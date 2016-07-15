9800d053166fdbc36a9661a79d99c63ef2bd45c0
Dan Abramov <dan.abramov@gmail.com>
2016-07-15 20:58:47 +0100

    Add linting and nicer output

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        Hello!
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      Hello world!
+    </div>
+  );
 }
