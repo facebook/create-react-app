4b4a8a155dc3e051fd61b3845a6091f95cf547bb
Dan Abramov <dan.abramov@gmail.com>
2016-07-17 13:04:44 +0100

    Separate index.css and App.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
 import logo from './logo.png';
 
 export default function App() {
   return (
-    <div>
+    <div className="App">
       <h1>
-        Welcome to <img src={logo} className="App--logo" alt="logo" /> React
+        Welcome to <img src={logo} className="App-logo" alt="logo" /> React
       </h1>
       <p>
         To get started, edit <code>src/App.js</code> and save to reload.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './index.css';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
