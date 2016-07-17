5afb14a07b147cdbb66e03e9f20435047c0dd0ea
Jonny Buchanan <jonathan.buchanan@gmail.com>
2016-07-17 23:37:16 +0100

    Strip comments from the production build (#5)
    * Strip comments from the production build
    
    * Add screw_ie8 options to UglifyJsPlugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -91,6 +91,18 @@ module.exports = {
     }),
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ compressor: { warnings: false } })
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+        screw_ie8: true
+      }
+    })
   ]
 };
