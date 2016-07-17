2cc636a6e832e38bd764c914a8d8e2877148c60f
Christopher Chedeau <vjeux@fb.com>
2016-07-16 19:49:19 -0700

    Make default app look less sketchy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
-import reactImage from './react.png';
+import logo from './logo.png';
 
 export default function App() {
   return (
-    <div className="App">
-      Hello world!
-      <img src={reactImage} alt="React rocks!" />
+    <div>
+      <h1>
+        Welcome to
+        <img src={logo} className="App--logo" alt="React logo" />
+        React
+      </h1>
+      <p>
+        To get started, edit <code>src/App.js</code> and
+        save the file to update.
+      </p>
     </div>
   );
 }
