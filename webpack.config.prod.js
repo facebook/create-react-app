d4f1237c4b45d4d33db9cca8259d0b6a4ecef619
Jonny Buchanan <jonathan.buchanan@gmail.com>
2016-07-18 04:59:42 +1000

    Strip comments from the production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -91,6 +91,9 @@ module.exports = {
     }),
     new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ compressor: { warnings: false } })
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: { warnings: false },
+      output: { comments: false }
+    })
   ]
 };
