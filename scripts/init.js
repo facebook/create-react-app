1d3bf12a525dc2acefc6e9bb5f159d65fe5c9e3c
eanplatter <eanplatter@gmail.com>
2016-07-19 21:27:51 -0500

    Add README to generated project

--- scripts/init.js
+++ scripts/init.js
@@ -40,13 +40,20 @@ module.exports = function(hostPath, appName, verbose) {
     return fs.writeFileSync(dest, fs.readFileSync(src));
   }
   fs.mkdirSync(path.join(hostPath, 'src'));
-  fs.readdirSync(path.join(selfPath, 'src')).forEach(function(filename) {
+  fs.readdirSync(path.join(selfPath, 'template/src')).forEach(function(filename) {
     copySync(
-      path.join(selfPath, 'src', filename),
+      path.join(selfPath, 'template/src', filename),
       path.join(hostPath, 'src', filename)
     );
   });
-  copySync(path.join(selfPath, 'index.html'), path.join(hostPath, 'index.html'));
+  fs.readdirSync(path.join(selfPath, 'template')).forEach(function(filename) {
+    console.log('what even is this?', path.join(selfPath, 'template', filename))
+    if (fs.lstatSync(path.join(selfPath, 'template', filename)).isDirectory()) return
+    copySync(
+      path.join(selfPath, 'template', filename),
+      path.join(hostPath, filename)
+    );    
+  });
 
   // Run another npm install for react and react-dom
   console.log('Installing react and react-dom from npm...');
