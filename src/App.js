24019802cc87ec70a5a289e8af9f5e56ca3f8a95
Dan Abramov <dan.abramov@gmail.com>
2016-07-20 14:52:12 +0100

    Avoid CSS targeting elements

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React.</h2>
       </div>
-      <p>
+      <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
     </div>
