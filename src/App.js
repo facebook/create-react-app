b4636631413aed66447da5823dc15334d5c005cd
Christopher Chedeau <vjeux@fb.com>
2016-07-20 08:53:58 -0700

    Simplify boilerplate css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React.</h2>
+        <h2>Welcome to React</h2>
       </div>
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
