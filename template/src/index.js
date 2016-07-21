3809cf12834ddfdad4ec50655e2b5a9ef91d5165
Dan Abramov <dan.abramov@gmail.com>
2016-07-21 17:04:36 +0100

    Tweak conventions

--- template/src/index.js
+++ template/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-const rootEl = document.getElementById('root');
-render(<App />, rootEl);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
