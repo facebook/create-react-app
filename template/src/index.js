9983c19c897ec4d7526120c0826c705ee69d0c24
Chris Shepherd <chris@chrisshepherd.me>
2016-07-23 13:00:00 +0100

    Adds simple HMR

--- template/src/index.js
+++ template/src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const rootEl = document.getElementById('root');
+
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  rootEl
 );
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    );
+  });
+}
