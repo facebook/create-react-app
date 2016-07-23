8382ae8067c09f891ba3eee68737cd90fe99be6d
Zakaria Ridouh <zridouh@live.com>
2016-07-23 14:31:13 -0700

    only load render function, not whole ReactDOM library

--- template/src/index.js
+++ template/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+render(
   <App />,
   document.getElementById('root')
 );
