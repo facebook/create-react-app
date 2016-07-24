e2dc775cd929f4615a6ffe92035edde124b40f5a
Donald Hruska <donaldhruska@gmail.com>
2016-07-24 16:21:13 -0500

    Update successful bundle generation message

--- scripts/build.js
+++ scripts/build.js
@@ -28,12 +28,6 @@ webpack(config).run(function(err, stats) {
 
   var openCommand = process.platform === 'win32' ? 'start' : 'open';
   console.log('Successfully generated a bundle in the build folder!');
-  console.log();
-  console.log('You can now serve it with any static server, for example:');
-  console.log('  cd build');
-  console.log('  npm install -g http-server');
-  console.log('  hs');
-  console.log('  ' + openCommand + ' http://localhost:8080');
-  console.log();
+  console.log('You can now deploy and serve it from <homepage>.');
   console.log('The bundle is optimized and ready to be deployed to production.');
 });
