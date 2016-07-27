b79a438eed02cd5bf6f3721bf5039d9d2d7aad2d
Florian Goße <florian.gosse@truffls.de>
2016-07-27 12:18:41 +0200

    Remove extra apostrophe

--- config/eslint.js
+++ config/eslint.js
@@ -10,7 +10,7 @@
 // Inspired by https://github.com/airbnb/javascript but less opinionated.
 
 // We use eslint-loader so even warnings are very visibile.
-// This is why we only use "WARNING'" level for potential errors,
+// This is why we only use "WARNING" level for potential errors,
 // and we don't use "ERROR" level at all.
 
 // In the future, we might create a separate list of rules for production.
