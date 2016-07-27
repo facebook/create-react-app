97b7568cdcd9c70f8bb1ccb384ffc3b099d4f07c
Florian Goße <florian.gosse@truffls.de>
2016-07-27 11:38:27 +0200

    Fix comment which where WARNING was replaced

--- config/eslint.js
+++ config/eslint.js
@@ -9,8 +9,8 @@
 
 // Inspired by https://github.com/airbnb/javascript but less opinionated.
 
-// We use eslint-loader so even 'warn's are very visibile.
-// This is why we only use "'warn'" level for potential errors,
+// We use eslint-loader so even warnings are very visibile.
+// This is why we only use "WARNING'" level for potential errors,
 // and we don't use "ERROR" level at all.
 
 // In the future, we might create a separate list of rules for production.
