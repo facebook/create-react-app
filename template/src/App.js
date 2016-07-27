9885e08c191cd9d967e875678977b445f6ed1cef
Dan Abramov <dan.abramov@gmail.com>
2016-07-27 16:54:10 +0100

    Remove accidental exclamation

--- template/src/App.js
+++ template/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React!</h2>
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
