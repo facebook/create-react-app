1f19c9b44602d972aba0caf505b2e4bc9448b6a5
steelbrain <de.steelbrain@gmail.com>
2016-07-28 14:09:50 -0700

    :new: Use the standard ports for dev server

--- scripts/start.js
+++ scripts/start.js
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+process.env.NODE_ENV = 'development';
+
 require('process-bootstrap')('react-app', 'REACT-APP')
 
 const Path = require('path')
@@ -6,7 +17,7 @@ const Server = require('pundle-dev')
 const debugInfo = debug('REACT-APP:Info')
 const debugError = debug('REACT-APP:Error')
 
-const port = 8056
+const port = process.env.PORT || 3000
 
 const server = new Server({
   pundle: {
