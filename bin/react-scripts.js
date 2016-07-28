91d0b3c9687e9a0acb83523a4eba52783819f0ac
John Weis <weis.john@gmail.com>
2016-07-28 10:35:06 -0400

    Return proper exit code in bin/react-scripts via spawnSync (#252)

--- bin/react-scripts.js
+++ bin/react-scripts.js
@@ -7,11 +7,12 @@ switch (script) {
 case 'build':
 case 'start':
 case 'eject':
-  spawn(
+  var result = spawn.sync(
     'node',
     [require.resolve('../scripts/' + script)].concat(args),
     {stdio: 'inherit'}
   );
+  process.exit(result.status);
   break;
 default:
   console.log('Unknown script "' + script + '".');
