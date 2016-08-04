0611e0b4ad72966fcdd617552130916cb1aa824e
Rifat Nabi <to.rifat@gmail.com>
2016-08-04 07:26:50 +0600

    added initial POC for flow support

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -55,6 +55,11 @@ module.exports = {
         test: /\.js$/,
         loader: 'eslint',
         include: paths.appSrc,
+      },
+      {
+        test: /\.js$/,
+        loader: 'flowtype',
+        include: paths.appSrc,
       }
     ],
     loaders: [
