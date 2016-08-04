c1a984d98e9a8b5d08c133ca509d7a9348b87258
Dan Abramov <dan.abramov@gmail.com>
2016-08-04 20:56:52 +0100

    Document configuration and build process (#362)

--- config/env.js
+++ config/env.js
@@ -7,6 +7,9 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
+// Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
+// injected into the application via DefinePlugin in Webpack configuration.
+
 var REACT_APP = /^REACT_APP_/i;
 var NODE_ENV = JSON.stringify(process.env.NODE_ENV || 'development');
 
