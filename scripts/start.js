5f26734b682caeae6306e14dd6a30f277d281735
Anton Kaminsky <antonwizkam@gmail.com>
2016-08-05 09:29:53 -0400

    Fix another grammatical error for consistency

--- scripts/start.js
+++ scripts/start.js
@@ -266,7 +266,7 @@ detect(DEFAULT_PORT).then(port => {
   clearConsole();
   var question =
     chalk.yellow('Something is already running on port ' + DEFAULT_PORT + '.') +
-    '\n\nWould you like to run the app at another port instead?';
+    '\n\nWould you like to run the app on another port instead?';
 
   prompt(question, true).then(shouldChangePort => {
     if (shouldChangePort) {
