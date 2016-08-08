c00791dc7d46295da08c8d68f2daf08c163b0a37
Dan Abramov <dan.abramov@gmail.com>
2016-08-08 13:50:49 +0100

    Add Object.assign() polyfill (#399)

--- config/polyfills.js
+++ config/polyfills.js
@@ -8,3 +8,7 @@ if (typeof Promise === 'undefined') {
 
 // fetch() polyfill for making API calls.
 require('whatwg-fetch');
+
+// Object.assign() is commonly used with React.
+// It will use the native implementation if it's present and isn't buggy.
+Object.assign = require('object-assign');
