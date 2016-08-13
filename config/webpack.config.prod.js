66d47566b0bad66cdac7077de2ae8116f439a948
Gregory Shehet <a.gambit.gregory@gmail.com>
2016-08-13 17:15:05 +0300

    add off font format to loaders

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -136,7 +136,7 @@ module.exports = {
       // "file" loader makes sure those assets end up in the `build` folder.
       // When you `import` an asset, you get its filename.
       {
-        test: /\.(ico|jpg|png|gif|eot|svg|ttf|woff|woff2)(\?.*)?$/,
+        test: /\.(ico|jpg|png|gif|eot|otf|svg|ttf|woff|woff2)(\?.*)?$/,
         exclude: /\/favicon.ico$/,
         include: [paths.appSrc, paths.appNodeModules],
         loader: 'file',
