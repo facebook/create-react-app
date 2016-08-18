b4fea92dc7c084dfe5699b58628feb6513efb611
Michael Cereda <info@michaelcereda.com>
2016-08-18 17:41:28 +0200

    minor variable name fix

--- scripts/utils/cli-template.js
+++ scripts/utils/cli-template.js
@@ -12,11 +12,11 @@ function block(msg){
 }
 
 var types =  {
-  'info': {initial: 'I', blockBg:['bgBlue','black'], titleColor:'blue'},
-  'error': {initial: 'E', blockBg:['bgRed','white'], titleColor:'red'},
-  'warning': {initial: 'W', blockBg:['bgYellow','black'], titleColor:'yellow'},
-  'success': {initial: 'S', blockBg:['bgGreen','black'], titleColor:'green'},
-  'note': {initial: 'N', blockBg:['bgBlack','yellow'], titleColor:'yellow'},
+  'info': {initial: 'I', blockColor:['bgBlue','black'], titleColor:'blue'},
+  'error': {initial: 'E', blockColor:['bgRed','white'], titleColor:'red'},
+  'warning': {initial: 'W', blockColor:['bgYellow','black'], titleColor:'yellow'},
+  'success': {initial: 'S', blockColor:['bgGreen','black'], titleColor:'green'},
+  'note': {initial: 'N', blockColor:['bgBlack','yellow'], titleColor:'yellow'},
 }
 
 var template = {};
@@ -33,29 +33,29 @@ function indentLines(lines, spaces){
 Object.keys(types).map(function(type){
   var _specs = types[type];
 
-  template[type] = function(content){
-    var line;
-
+  template[type] = function(content, override){
+    var line, specs={};
+    Object.assign(specs, _specs, override)
     if(typeof content !== 'string'){
       var blk;
       if(isArray(content)){
-        blk = block(_specs.initial)
-        line = chalk[_specs.blockBg[0]][_specs.blockBg[1]](blk)
+        blk = block(specs.initial)
+        line = chalk[specs.blockColor[0]][specs.blockColor[1]](blk)
                 +" " + indentLines(content, blk.length+2)
       } else {
 
         if(content.type=='title'){
           blk = block(content.name);
-          line = chalk[_specs.blockBg[0]][_specs.blockBg[1]](blk)
-                  + " " + chalk[_specs.titleColor](indentLines(content.message, blk.length+2));
+          line = chalk[specs.blockColor[0]][specs.blockColor[1]](blk)
+                  + " " + chalk[specs.titleColor](indentLines(content.message, blk.length+2));
         } else {
-          blk = block(_specs.initial);
-          line = chalk[_specs.blockBg[0]][_specs.blockBg[1]](blk)
+          blk = block(specs.initial);
+          line = chalk[specs.blockColor[0]][specs.blockColor[1]](blk)
                   +" " + indentLines(content.message, blk.length);
         }
       }
     } else {
-      line = chalk[_specs.blockBg[0]][_specs.blockBg[1]](block(_specs.initial))
+      line = chalk[specs.blockColor[0]][specs.blockColor[1]](block(_specs.initial))
               +" " + content
     }
     return line;
