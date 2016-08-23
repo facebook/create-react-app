59453c1c064ad57a53ca97263b8d2d09b12f7a44
Jimmy Miller <jimmy.miller@nextgearcapital.com>
2016-08-23 09:11:23 -0400

    Added ability to specify multiple directories in node_path.

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -67,7 +67,7 @@ module.exports = {
   resolve: {
     // This allows you to set a root for where webpack should look for modules.
     // This enables you to use absolute imports from the root.
-    root: path.resolve(process.env.NODE_PATH || ''),
+    root: paths.nodePath,
     // These are the reasonable defaults supported by the Node ecosystem.
     extensions: ['.js', '.json', ''],
     alias: {
