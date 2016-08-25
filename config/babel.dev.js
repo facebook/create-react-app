01825465c6ae06775987dee888d7c4957fcc570f
Kristóf Poduszló <kripod@protonmail.com>
2016-08-25 14:52:16 +0200

    Started using babel-preset-latest (#490)
    * Started using babel-preset-latest
    
    * Updated babel dependencies
    
    * Remove redundant babel-plugin-syntax-async-functions
    
    * Disabled redundant regenerator transform

--- config/babel.dev.js
+++ config/babel.dev.js
@@ -14,24 +14,21 @@ module.exports = {
   // It enables caching results in OS temporary directory for faster rebuilds.
   cacheDirectory: true,
   presets: [
-    // let, const, destructuring, classes, modules
-    require.resolve('babel-preset-es2015'),
-    // exponentiation
-    require.resolve('babel-preset-es2016'),
+    // Latest stable ECMAScript features
+    require.resolve('babel-preset-latest'),
     // JSX, Flow
     require.resolve('babel-preset-react')
   ],
   plugins: [
-    // function x(a, b, c,) { }
-    require.resolve('babel-plugin-syntax-trailing-function-commas'),
-    // await fetch()
-    require.resolve('babel-plugin-syntax-async-functions'),
     // class { handleClick = () => { } }
     require.resolve('babel-plugin-transform-class-properties'),
     // { ...todo, completed: true }
     require.resolve('babel-plugin-transform-object-rest-spread'),
     // function* () { yield 42; yield 43; }
-    require.resolve('babel-plugin-transform-regenerator'),
+    [require.resolve('babel-plugin-transform-regenerator'), {
+      // Async functions are converted to generators by babel-preset-latest
+      async: false
+    }],
     // Polyfills the runtime needed for async/await and generators
     [require.resolve('babel-plugin-transform-runtime'), {
       helpers: false,
