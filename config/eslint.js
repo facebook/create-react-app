f994f3b7a750ec487bff264c7335b05d7c6ba322
Sophia <szensius@gmail.com>
2016-08-31 14:24:34 -0700

    removing no-return-assign lint rule (#528) (#529)

--- config/eslint.js
+++ config/eslint.js
@@ -115,7 +115,6 @@ module.exports = {
       'LabeledStatement',
       'WithStatement',
     ],
-    'no-return-assign': 'warn',
     'no-script-url': 'warn',
     'no-self-assign': 'warn',
     'no-self-compare': 'warn',
