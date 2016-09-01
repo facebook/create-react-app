70535b0e3140ca17119f0263007bdef9080bd097
Dan Abramov <dan.abramov@gmail.com>
2016-09-01 18:26:07 +0100

    Remove unnecessary describe()

--- template/src/App.test.js
+++ template/src/App.test.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-describe('App', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-  });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
