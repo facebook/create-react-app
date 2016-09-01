c2a56cbc438d4748c2769db16ea366637e688e77
Dan Abramov <dan.abramov@gmail.com>
2016-09-01 18:01:11 +0100

    Resolve babel-runtime relative to the config (#535)
    This makes generators work with Jest.
    
    Fixes #255.
    Replaces #262.

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -68,15 +68,8 @@ module.exports = {
     // These are the reasonable defaults supported by the Node ecosystem.
     extensions: ['.js', '.json', ''],
     alias: {
-      // This `alias` section can be safely removed after ejection.
-      // We do this because `babel-runtime` may be inside `react-scripts`,
-      // so when `babel-plugin-transform-runtime` imports it, it will not be
-      // available to the app directly. This is a temporary solution that lets
-      // us ship support for generators. However it is far from ideal, and
-      // if we don't have a good solution, we should just make `babel-runtime`
-      // a dependency in generated projects.
-      // See https://github.com/facebookincubator/create-react-app/issues/255
-      'babel-runtime/regenerator': require.resolve('babel-runtime/regenerator'),
+      // Support React Native Web
+      // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web'
     }
   },
