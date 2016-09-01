6f5594a15ba2e5d4e9ac6e4de2ae0c48349ec5bc
Dan Abramov <dan.abramov@gmail.com>
2016-09-01 17:51:28 +0100

    Disable react-constant-elements because of bugs (#534)
    Disabled because of issues:
     * https://github.com/facebookincubator/create-react-app/issues/525
     * https://phabricator.babeljs.io/search/query/pCNlnC2xzwzx/
    
    TODO: Enable again when these issues are resolved.

--- config/babel.prod.js
+++ config/babel.prod.js
@@ -33,6 +33,10 @@ module.exports = {
       regenerator: true
     }],
     // Optimization: hoist JSX that never changes out of render()
-    require.resolve('babel-plugin-transform-react-constant-elements')
+    // Disabled because of issues:
+    // * https://github.com/facebookincubator/create-react-app/issues/525
+    // * https://phabricator.babeljs.io/search/query/pCNlnC2xzwzx/
+    // TODO: Enable again when these issues are resolved.
+    // require.resolve('babel-plugin-transform-react-constant-elements')
   ]
 };
