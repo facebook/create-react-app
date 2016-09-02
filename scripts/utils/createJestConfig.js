a94b2523bd5a785cef1cdbae214363feab4d5801
Gael du Plessix <gael.duplessix@gmail.com>
2016-09-02 21:27:09 +0200

    Add src/setupTests.js to specify environment setup for Jest (#545) (#548)

--- scripts/utils/createJestConfig.js
+++ scripts/utils/createJestConfig.js
@@ -9,14 +9,22 @@
  */
 // @remove-on-eject-end
 
+const pathExists = require('path-exists');
+const paths = require('../../config/paths');
+
 module.exports = (resolve, rootDir) => {
+  const setupFiles = [resolve('config/polyfills.js')];
+  if (pathExists.sync(paths.testsSetup)) {
+    setupFiles.push(paths.testsSetup);
+  }
+
   const config = {
     moduleNameMapper: {
       '^[./a-zA-Z0-9$_-]+\\.(jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm)$': resolve('config/jest/FileStub.js'),
       '^[./a-zA-Z0-9$_-]+\\.css$': resolve('config/jest/CSSStub.js')
     },
     scriptPreprocessor: resolve('config/jest/transform.js'),
-    setupFiles: [resolve('config/polyfills.js')],
+    setupFiles: setupFiles,
     testPathIgnorePatterns: ['<rootDir>/(build|docs|node_modules)/'],
     testEnvironment: 'node'
   };
