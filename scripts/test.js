98f74a8c65fa08717420f642263fbc082cf7d12d
Dan Abramov <dan.abramov@gmail.com>
2016-09-02 20:27:58 +0100

    Remove code that is unused after publish (#550)

--- scripts/test.js
+++ scripts/test.js
@@ -16,12 +16,6 @@ const paths = require('../config/paths');
 
 const argv = process.argv.slice(2);
 
-// Don't pass this option to Jest
-const debugTemplateIndex = argv.indexOf('--debug-template');
-if (debugTemplateIndex !== -1) {
-  argv.splice(debugTemplateIndex, 1);
-}
-
 // Watch unless on CI
 if (!process.env.CI) {
   argv.push('--watch');
