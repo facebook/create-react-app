c71337f63bde164c17de3fd6ce5000d26a00e68d
Dan Abramov <dan.abramov@gmail.com>
2016-09-02 17:52:58 +0100

    Tweak how NODE_PATH is handled

--- config/paths.js
+++ config/paths.js
@@ -11,13 +11,17 @@
 
 var path = require('path');
 
-// We support resolving modules according to NODE_PATH.
+// We support resolving modules according to `NODE_PATH`.
 // This lets you use absolute paths in imports inside large monorepos:
 // https://github.com/facebookincubator/create-react-app/issues/253.
-// It works just like NODE_PATH in Node:
+
+// It works similar to `NODE_PATH` in Node itself:
 // https://nodejs.org/api/modules.html#modules_loading_from_the_global_folders
+
 // We will export `nodePaths` as an array of absolute paths.
-// It will then be used by Webpack (and potentially other tools).
+// It will then be used by Webpack configs.
+// Jest doesn’t need this because it already handles `NODE_PATH` out of the box.
+
 var nodePaths = (process.env.NODE_PATH || '')
   .split(process.platform === 'win32' ? ';' : ':')
   .filter(Boolean)
