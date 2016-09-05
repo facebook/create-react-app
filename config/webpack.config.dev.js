e995fd7a56de42d555fb1dc983909e0eeebc4c96
Jason Kurian <jasonk92@gmail.com>
2016-09-05 16:09:53 -0400

    fix: Use css sourceMaps in development

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -116,7 +116,7 @@ module.exports = {
       // in development "style" loader enables hot editing of CSS.
       {
         test: /\.css$/,
-        loader: 'style!css!postcss'
+        loader: 'style!css?sourceMap!postcss'
       },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
