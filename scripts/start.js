1079a6ef7c41af8dae783b92563a70f441ae2f79
yunjun <yunjun.mu@gmail.com>
2016-09-05 12:30:25 -0400

    Send proper error reponse upon proxy error.

--- scripts/start.js
+++ scripts/start.js
@@ -175,6 +175,8 @@ function openBrowser(port, protocol) {
 function onProxyError(proxy) {
   return function(err, req, res){
     var host = req.headers && req.headers.host;
+
+    // Log the error to the deveopment console.
     console.log(
       chalk.red('Proxy error:') + ' Could not proxy request ' + chalk.cyan(req.url) +
       ' from ' + chalk.cyan(host) + ' to ' + chalk.cyan(proxy) + '.'
@@ -184,6 +186,15 @@ function onProxyError(proxy) {
       chalk.cyan(err.code) + ').'
     );
     console.log();
+
+    // And immediately send the proper error response to the client.
+    // Otherwise, the request will eventually timeout with ERR_EMPTY_RESPONSE on the client side.
+    if (res.writeHead && !res.headersSent) {
+        res.writeHead(500);
+    }
+    res.end('Proxy error: Could not proxy request ' + req.url + ' from ' + 
+      host + ' to ' + proxy + ' (' + err.code + ').'
+    );
   }
 }
 
