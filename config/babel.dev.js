8a1070ba436dc8bd69af5d544f61054452bb60ca
Khoa Nguyen <hi@khoanguyen.me>
2016-09-10 21:09:33 +0700

    Specify cache directory for babel loader

--- config/babel.dev.js
+++ config/babel.dev.js
@@ -10,13 +10,15 @@
 // @remove-on-eject-end
 
 var path = require('path');
+var findCacheDir = require('find-cache-dir');
 
 module.exports = {
   // Don't try to find .babelrc because we want to force this configuration.
   babelrc: false,
   // This is a feature of `babel-loader` for webpack (not Babel itself).
-  // It enables caching results in OS temporary directory for faster rebuilds.
-  cacheDirectory: true,
+  // It enables caching results in ./node_modules/.cache/create-react-app/
+  // directory for faster rebuilds.
+  cacheDirectory: findCacheDir({ name: 'create-react-app' }),
   presets: [
     // Latest stable ECMAScript features
     require.resolve('babel-preset-latest'),
