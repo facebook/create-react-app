1e1bed91f002487e239fee290eac070ded6ec67f
Vesa Laakso <vesa.laakso@venuu.fi>
2016-09-13 22:16:40 +0300

    Split no-unused-vars ESLint config to multiple lines

--- config/eslint.js
+++ config/eslint.js
@@ -131,7 +131,10 @@ module.exports = {
     'no-unreachable': 'warn',
     'no-unused-expressions': 'warn',
     'no-unused-labels': 'warn',
-    'no-unused-vars': ['warn', { vars: 'local', args: 'none' }],
+    'no-unused-vars': ['warn', {
+      vars: 'local',
+      args: 'none'
+    }],
     'no-use-before-define': ['warn', 'nofunc'],
     'no-useless-computed-key': 'warn',
     'no-useless-concat': 'warn',
