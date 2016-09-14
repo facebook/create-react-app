f2e3490f17c2ff3aee8c9711c6875b82a31c23e4
Chet Corcos <chet.corcos@affirm.com>
2016-09-14 13:05:37 -0700

    generate template app with absolute import paths

--- template/src/index.js
+++ template/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from '__ROOT__/src/App';
+import '__ROOT__/src/index.css';
 
 ReactDOM.render(
   <App />,
