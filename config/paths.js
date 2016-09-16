181bef56a097cb0e34d0c66699427dc511b96a37
Dan Abramov <dan.abramov@gmail.com>
2016-09-17 00:00:21 +0300

    Use new resolveApp() for NODE_PATH too

--- config/paths.js
+++ config/paths.js
@@ -12,6 +12,13 @@
 var path = require('path');
 var fs = require('fs');
 
+// Make sure any symlinks in the project folder are resolved:
+// https://github.com/facebookincubator/create-react-app/issues/637
+var appDirectory = fs.realpathSync(process.cwd());
+function resolveApp(relativePath) {
+  return path.resolve(appDirectory, relativePath);
+}
+
 // We support resolving modules according to `NODE_PATH`.
 // This lets you use absolute paths in imports inside large monorepos:
 // https://github.com/facebookincubator/create-react-app/issues/253.
@@ -26,11 +33,7 @@ var fs = require('fs');
 var nodePaths = (process.env.NODE_PATH || '')
   .split(process.platform === 'win32' ? ';' : ':')
   .filter(Boolean)
-  .map(p => path.resolve(p));
-
-function resolveApp(relativePath) {
-  return path.resolve(fs.realpathSync(process.cwd()), relativePath);
-}
+  .map(resolveApp);
 
 // config after eject: we're in ./config/
 module.exports = {
