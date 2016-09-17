cb3f4a68f3770bd68da04d6ca286db0cb90471bb
Vincent TAING <vnctaing@vnctaingggg.local>
2016-09-17 23:30:23 +0200

    uses appIndexJs variable for specifying entry path in webpack configs

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -49,7 +49,7 @@ module.exports = {
     // We ship a few polyfills by default.
     require.resolve('./polyfills'),
     // Finally, this is your app's code:
-    path.join(paths.appSrc, 'index')
+    paths.appIndexJs
     // We include the app code last so that if there is a runtime error during
     // initialization, it doesn't blow up the WebpackDevServer client, and
     // changing JS code would still trigger a refresh.
