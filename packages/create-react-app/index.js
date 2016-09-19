e96e9ac2a59c015be6e55d864face282a270a47e
Simen Bekkhus <sbekkhus91@gmail.com>
2016-09-20 01:00:51 +0200

    Use pre node@4 compatible `path-exists` (#685)
    * Revert "Add ES5 version of `path-exists` to CLI"
    
    This reverts commit fc3ab46d2a54f142f9287ce7de9ab2fc2514487d.
    
    * Use pre node@4 compatible `path-exists`
    
    Ref #617

--- packages/create-react-app/index.js
+++ packages/create-react-app/index.js
@@ -44,6 +44,7 @@ var spawn = require('cross-spawn');
 var chalk = require('chalk');
 var semver = require('semver');
 var argv = require('minimist')(process.argv.slice(2));
+var pathExists = require('path-exists');
 
 /**
  * Arguments:
@@ -75,7 +76,7 @@ function createApp(name, verbose, version) {
 
   checkAppName(appName);
 
-  if (!pathExistsSync(name)) {
+  if (!pathExists.sync(name)) {
     fs.mkdirSync(root);
   } else if (!isSafeToCreateProjectIn(root)) {
     console.log('The directory `' + name + '` contains file(s) that could conflict. Aborting.');
@@ -218,17 +219,4 @@ function isSafeToCreateProjectIn(root) {
     .every(function(file) {
       return validFiles.indexOf(file) >= 0;
     });
-}
-
-// This is an ES5 version of https://github.com/sindresorhus/path-exists.
-// The reason it exists is so that the CLI doesn't break before being able to
-// warn the user they're using an unsupported version of Node.
-// See https://github.com/facebookincubator/create-react-app/issues/570
-function pathExistsSync(fp) {
-  try {
-    fs.accessSync(fp);
-    return true;
-  } catch (err) {
-    return false;
-  }
 }
