5b85a3624f2c18713988aa4fcc7dab200a1f78b2
Ville Immonen <ville.immonen@iki.fi>
2016-09-22 22:17:54 +0300

    Extract Babel configuration to babel-preset-react-app (#701)

--- packages/react-scripts/config/babel.dev.js
+++ packages/react-scripts/config/babel.dev.js
@@ -9,7 +9,6 @@
  */
 // @remove-on-eject-end
 
-var path = require('path');
 var findCacheDir = require('find-cache-dir');
 
 module.exports = {
@@ -23,29 +22,6 @@ module.exports = {
     name: 'react-scripts'
   }),
   presets: [
-    // Latest stable ECMAScript features
-    require.resolve('babel-preset-latest'),
-    // JSX, Flow
-    require.resolve('babel-preset-react')
-  ],
-  plugins: [
-    // class { handleClick = () => { } }
-    require.resolve('babel-plugin-transform-class-properties'),
-    // { ...todo, completed: true }
-    require.resolve('babel-plugin-transform-object-rest-spread'),
-    // function* () { yield 42; yield 43; }
-    [require.resolve('babel-plugin-transform-regenerator'), {
-      // Async functions are converted to generators by babel-preset-latest
-      async: false
-    }],
-    // Polyfills the runtime needed for async/await and generators
-    [require.resolve('babel-plugin-transform-runtime'), {
-      helpers: false,
-      polyfill: false,
-      regenerator: true,
-      // Resolve the Babel runtime relative to the config.
-      // You can safely remove this after ejecting:
-      moduleName: path.dirname(require.resolve('babel-runtime/package'))
-    }]
+    require.resolve('babel-preset-react-app')
   ]
 };
