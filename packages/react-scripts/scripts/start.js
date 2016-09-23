743c8352f4c25d676016920dfcb9dd1fa956cead
Dan Abramov <dan.abramov@gmail.com>
2016-09-23 15:01:19 +0100

    Style nit

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -320,7 +320,7 @@ function runDevServer(host, port, protocol) {
 
 function run(port) {
   var protocol = process.env.HTTPS === 'true' ? "https" : "http";
-  var host = process.env.HOST ? process.env.HOST : 'localhost';
+  var host = process.env.HOST || 'localhost';
   checkRequiredFiles();
   setupCompiler(host, port, protocol);
   runDevServer(host, port, protocol);
