177b5ecd63119d8b8a69656663d848a22bb72e03
Aeneas <arekkas@users.noreply.github.com>
2016-09-23 12:33:23 +0200

    openBrowser() causes docker-compose set ups to fail because of spawn EACCESS  (#711)
    * openBrowser() causes docker-compose set ups to fail because of spawn EACCESS - closes #710
    
    Signed-off-by: Aeneas Rekkas (arekkas) <aeneas@ory.am>
    
    * Ignore errors

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -170,7 +170,11 @@ function openBrowser(port, protocol) {
   }
   // Fallback to opn
   // (It will always open new tab)
-  opn(protocol + '://localhost:' + port + '/');
+  try {
+    opn(protocol + '://localhost:' + port + '/');
+  } catch (err) {
+    // Ignore errors.
+  }
 }
 
 // We need to provide a custom onError function for httpProxyMiddleware.
