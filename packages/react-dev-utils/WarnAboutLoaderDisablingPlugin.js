a8cd88f201e09c33eb2f95706db311cd084b223b
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-09-24 08:38:06 -0300

    Change matcher to the right one and fix typos

--- packages/react-dev-utils/WarnAboutLoaderDisablingPlugin.js
+++ packages/react-dev-utils/WarnAboutLoaderDisablingPlugin.js
@@ -8,7 +8,7 @@
  */
 
 // This Webpack plugin warns the user when using special webpack syntax
-// to distable loaders (https://webpack.github.io/docs/loaders.html#loader-order)
+// to disable loaders (https://webpack.github.io/docs/loaders.html#loader-order)
 // This makes it very coupled to webpack and might break in the future.
 // See https://github.com/facebookincubator/create-react-app/issues/733.
 
@@ -16,11 +16,11 @@
 
 var chalk = require('chalk');
 
-class WatchAboutLoaderDisablingPlugin {
+class WarnAboutLoaderDisablingPlugin {
   apply(compiler) {
     compiler.plugin('normal-module-factory', (normalModuleFactory) => {
       normalModuleFactory.plugin('before-resolve', (data, callback) => {
-        if (data.request.match('!')) {
+        if (data.request.match(/^-?!!?/)) {
           chalk.yellow(`Warning: You are requesting '${data.request}'. Special Webpack Loaders syntax is not officially supported and makes you code very coupled to Webpack, which might break in the future, we recommend that you remove it. See `https://github.com/facebookincubator/create-react-app/issues/733`);
         }
         callback(null, data);
@@ -29,4 +29,4 @@ class WatchAboutLoaderDisablingPlugin {
   }
 }
 
-module.exports = WatchMissingNodeModulesPlugin;
+module.exports = WarnAboutLoaderDisablingPlugin;
