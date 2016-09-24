99f36a2d6948ea2a81742638b3e576d553174c69
kitze <kristijan.mkd@gmail.com>
2016-09-24 22:47:49 +0200

    removes mobx stores

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import store from './stores/store';
 
 ReactDOM.render(
-  <App store={store}/>,
+  <App />,
   document.getElementById('root')
 );
