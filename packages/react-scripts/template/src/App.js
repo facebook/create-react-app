07e8764f7cead772c2838a09b1667828e4185101
kitze <kristijan.mkd@gmail.com>
2016-09-24 22:46:38 +0200

    remove mobx dependencies, change version to 0.0.1, add config options to landing page

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -6,29 +6,70 @@ import './App.less';
 import './App.scss';
 import styles from './Modules.css';
 
-//mobx
-import {observer} from 'mobx-react';
-
-@observer
 class App extends Component {
   render() {
-
-    const {app} = this.props.store;
-
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
-          <h2 className="App-title">{app.title} </h2>
-          <button className="App-button" onClick={::app.changeRandomTitle}>
-            change me
-          </button>
+          <h2 className="App-title"> ☢ Customizable create-react-app ☢ </h2>
         </div>
-        <p className={styles.appIntro}>
-          To get started, edit <code className={styles.code}>src/App.js</code> and save to reload.
-        </p>
+
+        <div className={styles.description}>
+          <p> If you want to enable certain features just modify the .env file in the root directory of this
+              project.
+          </p>
+
+          <b> Styling </b>
+          <ul className="configs style-configs">
+            <li>
+              <code>REACT_APP_SASS=true</code>
+              <span>- Enable SASS</span>
+            </li>
+            <li>
+              <code>REACT_APP_LESS=true</code>
+              <span>- Enable LESS</span>
+            </li>
+            <li>
+              <code>REACT_APP_CSS_MODULES=true</code>
+              <span>- Enable CSS modules </span>
+            </li>
+          </ul>
+
+          <b> ⚠️ Babel </b>
+
+          <div className={styles.warning}>
+            (Please note that these features are highly experimental (especially stage-0) and still not a part of the ES
+            specification. <br/>
+            Use them at
+            your own risk of breaking backwards compatibility if they don't make the final version of the spec.)
+          </div>
+
+          <ul className="configs babel-configs">
+            <li>
+              <code>REACT_APP_BABEL_STAGE_0=true</code>
+              <span>- Enable stage-0 preset</span>
+            </li>
+            <li>
+              <code>REACT_APP_DECORATORS=true</code>
+              <span>- Enable usage of decorators</span>
+            </li>
+          </ul>
+
+          <b> Webpack </b>
+          <ul className="configs babel-configs">
+            <li>
+              <code>REACT_APP_WEBPACK_DASHBOARD=true</code>
+              <span>- Enable webpack-dashboard ⚠️ (this will turn off the original create-react-app message logs)</span>
+            </li>
+            <li>
+              <code>REACT_APP_DECORATORS=true</code>
+              <span>- Enable usage of decorators</span>
+            </li>
+          </ul>
+        </div>
       </div>
-    );
+    )
   }
 }
 
