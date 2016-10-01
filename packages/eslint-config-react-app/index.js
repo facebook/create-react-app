909d9381661e75052be13c4db5b66a4e8442dac9
Brian Ng <bng412@gmail.com>
2016-10-01 04:13:42 -0500

    Fix some typos (#809)

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -9,7 +9,7 @@
 
 // Inspired by https://github.com/airbnb/javascript but less opinionated.
 
-// We use eslint-loader so even warnings are very visibile.
+// We use eslint-loader so even warnings are very visible.
 // This is why we only use "WARNING" level for potential errors,
 // and we don't use "ERROR" level at all.
 
