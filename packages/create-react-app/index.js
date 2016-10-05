e176df25458fde2ebbce672a2d3a970e135a4d44
Dan Poineau <dpoineau@gmail.com>
2016-10-04 21:44:46 -0700

    Return correct package name for scoped packages

--- packages/create-react-app/index.js
+++ packages/create-react-app/index.js
@@ -154,7 +154,7 @@ function getInstallPackage(version) {
 function getPackageName(installPackage) {
   if (~installPackage.indexOf('.tgz')) {
     return installPackage.match(/^.+\/(.+)-.+\.tgz$/)[1];
-  } else if (~installPackage.indexOf('@')) {
+  } else if (installPackage.indexOf('@') > 0) {
     return installPackage.split('@')[0];
   }
   return installPackage;
