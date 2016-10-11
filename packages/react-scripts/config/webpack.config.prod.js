43d4239e869fc81c930147156c08910a0622dc0a
Kanitkorn S <k.sujautra@gmail.com>
2016-10-11 20:35:20 +0700

    Add an explanation for ManifestPlugin

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -252,6 +252,9 @@ module.exports = {
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin('static/css/[name].[contenthash:8].css'),
+    // Generate a manifest file which contains a mapping of all asset filenames
+    // to their corresponding output file so that tools can pick it up without
+    // having to parse `index.html`.
     new ManifestPlugin({
       fileName: 'asset-manifest.json'
     })
