db31d4a2e69d71d2d2af49f9f4fb134a95ee210c
Zach Perrault <zach.perrault@gmail.com>
2016-10-14 10:41:35 -0500

    Add react-hot-loader@3

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'App'
+import { AppContainer } from 'react-hot-loader'
 import 'index.css'
 
 ReactDOM.render(
-  <App />,
+  <AppContainer>
+    <App/>
+  </AppContainer>,
   document.getElementById('root')
 )
+
+if (module.hot) {
+  module.hot.accept('App', () => {
+    ReactDOM.render(
+      <AppContainer>
+        {React.createElement(require('App').default)}
+      </AppContainer>,
+      document.getElementById('root')
+    )
+  })
+}
