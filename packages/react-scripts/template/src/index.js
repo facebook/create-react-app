d4ea2e908daba281d761e3711da585819a1a5e34
Zach Perrault <zach.perrault@gmail.com>
2016-10-15 09:41:51 -0500

    Add comments to `index.js` explaining `react-hot-loader`

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'App'
+// react-hot-loader is bundled with `@trunkclub/react-scripts`
 import { AppContainer } from '@trunkclub/react-scripts/node_modules/react-hot-loader'
 import 'index.css'
 
+// AppContainer is used by react-hot-loader, in production it is
+// a straight pass through so everything can stay just like this.
+//
+// Put the root of the app as children to `AppContainer` here and
+// also below in the `if (module.hot) { ... }` section.
 ReactDOM.render(
   <AppContainer>
     <App/>
@@ -11,6 +17,8 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
+// With `react-hot-loader@3` we have to setup the hot module
+// replacement manually.
 if (module.hot) {
   module.hot.accept('App', () => {
     ReactDOM.render(
