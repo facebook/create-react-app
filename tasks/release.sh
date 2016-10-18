126f69378d10b7df0162a94da3b8a8d83883f798
Rupert Redington <rupert@neontribe.co.uk>
2016-10-18 11:05:55 +0100

    Use the release script provided to avoid confusing errors

--- tasks/release.sh
+++ tasks/release.sh
@@ -75,10 +75,13 @@ rm -rf node_modules/fsevents
 # This modifies $clean_path/package.json to copy all dependencies to bundledDependencies
 node ./node_modules/.bin/bundle-deps
 
+# Don't publish with lerna because nexus doesn't support npm dist-tag...
+npm publish
+
 cd $clean_path
 
 # Go!
-./node_modules/.bin/lerna publish --independent "$@"
+#./node_modules/.bin/lerna publish --independent "$@"
 
 # cleanup
 cd ..
