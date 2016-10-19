4ad58dbbee6f65ae4a57416bdce7aa0ef35af330
Nathan Hunzaker <nate.hunzaker@gmail.com>
2016-10-19 05:19:12 -0400

    PostCSS should run on @import statements. (#929)

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -137,7 +137,7 @@ module.exports = {
       // in development "style" loader enables hot editing of CSS.
       {
         test: /\.css$/,
-        loader: 'style!css!postcss'
+        loader: 'style!css?importLoaders=1!postcss'
       },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
