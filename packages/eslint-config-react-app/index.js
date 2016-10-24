db4241068627026d7d33d35787a2e9e6e205df31
vadzim <v@vadzim.info>
2016-10-24 15:01:49 +0300

    Fixes https://github.com/facebookincubator/create-react-app/issues/952

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -44,7 +44,7 @@ module.exports = {
   settings: {
     'import/ignore': [
       'node_modules',
-      '\\.(json|css|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$',
+      '\\.(json|css|ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$',
     ],
     'import/extensions': ['.js'],
     'import/resolver': {
