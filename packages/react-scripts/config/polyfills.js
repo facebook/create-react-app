759cbeccb9a467bf9741278c1e529107304aad73
Zach Perrault <zach.perrault@gmail.com>
2016-11-08 11:13:36 -0600

    Remove polyfills from babel or unused

--- packages/react-scripts/config/polyfills.js
+++ packages/react-scripts/config/polyfills.js
@@ -16,10 +16,3 @@ if (typeof Promise === 'undefined') {
   require('promise/lib/rejection-tracking').enable();
   window.Promise = require('promise/lib/es6-extensions.js');
 }
-
-// fetch() polyfill for making API calls.
-require('whatwg-fetch');
-
-// Object.assign() is commonly used with React.
-// It will use the native implementation if it's present and isn't buggy.
-Object.assign = require('object-assign');
