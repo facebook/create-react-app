80fe767bf253227a4633c8ec3242f8ac3d90b566
Sathish <sathishwelcomez@gmail.com>
2016-11-17 18:24:12 +0530

    Clears the usage of react-jsx-source & react-jsx-self (#992)
    Explain the usage of react-jsx-source & react-jsx-self

--- packages/babel-preset-react-app/index.js
+++ packages/babel-preset-react-app/index.js
@@ -54,6 +54,12 @@ if (env !== 'development' && env !== 'test' && env !== 'production') {
 }
 
 if (env === 'development' || env === 'test') {
+  // The following two plugins are currently necessary to make React warnings
+  // include more valuable information. They are included here because they are
+  // currently not enabled in babel-preset-react. See the below threads for more info:
+  // https://github.com/babel/babel/issues/4702
+  // https://github.com/babel/babel/pull/3540#issuecomment-228673661
+  // https://github.com/facebookincubator/create-react-app/issues/989
   plugins.push.apply(plugins, [
     // Adds component stack to warning messages
     require.resolve('babel-plugin-transform-react-jsx-source'),
@@ -99,4 +105,3 @@ if (env === 'test') {
     // ]);
   }
 }
-
