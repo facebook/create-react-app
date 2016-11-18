749aec132490e93a3b2eb5105adf684958df3479
Huy Giang <gnhuy91@users.noreply.github.com>
2016-11-18 10:59:16 +0700

    Add camelCase to css-loader config

--- packages/react-scripts/config/customizers.js
+++ packages/react-scripts/config/customizers.js
@@ -60,8 +60,8 @@ module.exports = {
   },
   'CSS_MODULES': {
     config: {
-      dev: 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss',
-      prod: 'style!css?modules&-autoprefixer&importLoaders=1!postcss'
+      dev: 'style!css?modules&camelCase&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss',
+      prod: 'style!css?modules&camelCase&-autoprefixer&importLoaders=1!postcss'
     }
   }
 }
