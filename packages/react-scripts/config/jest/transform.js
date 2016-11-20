a5c30b945fc4ea1312530b4dfb3f9e18c1b06b85
Nazim Hajidin <nhajidin@users.noreply.github.com>
2016-11-20 09:23:11 -0500

    Don't look for babelrc file during test (#1017)

--- packages/react-scripts/config/jest/transform.js
+++ packages/react-scripts/config/jest/transform.js
@@ -9,5 +9,6 @@
 const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
-  presets: [require.resolve('babel-preset-react-app')]
+  presets: [require.resolve('babel-preset-react-app')],
+  babelrc: false
 });
