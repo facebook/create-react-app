f021d7b2d5e007ca2f6363c6cb8980f1c5f13d85
Ian McNally <ianmcnally@gmail.com>
2016-11-22 15:44:21 -0500

    add logging of existing default port process on start (#816)
    * add logging of existing port process on start
    
    * Move port process wording in start command on to next line
    
    * Color the named processes as cyan in terminal output
    
    * Add handling for multiple processes on a part
    
    - With the currently process filtering, if multiple processes are returned as running on port 3000, this command would fail. This splits apart the process IDing and the process naming, to support multiple processes.
    - One curious thing about the bash command to get processes, is that it'll include browsers with a window open on localhost:3000. May want to reconsider that.
    
    * Add process directory to existing port warning
    
    - also moved terminal coloring up, when getting the process, to be able to distinguish the process command from the directory
    
    * Change output color to all cyan, except "in"
    
    * Rename getProcessNameOnPort -> getProcessForPort
    
    - better reflects its broadened scope (both command and directory)
    
    * Add checking if process is a CRA instance, to customize port running message
    
    - moved from using package.json to a regex, for reliability
    
    * Move getProcessForPort to react-dev-utils
    
    - also allowed for breakdown of commands into helper methods
    
    * Add documentation for getProcessForPort
    
    * Add getProcessForPort to list of dev-scripts files
    
    * Use app's package name when CRA app is running on another port
    
    * Filter port process by those listening
    
    - Removed the handling of multiple process IDs since you can filtering by listening process (and not have the browser in the list of processes)
    - Trimmed the terminal outputs for better matching (process id) and better terminal output (directory of process)
    
    * Update README on port helpers, to specify only one port returned
    
    * Add ignore of stderr when executing process commands
    
    - Make sure any potential errors don't leak to the user

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -26,6 +26,7 @@ var detect = require('detect-port');
 var clearConsole = require('react-dev-utils/clearConsole');
 var checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 var formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
+var getProcessForPort = require('react-dev-utils/getProcessForPort');
 var openBrowser = require('react-dev-utils/openBrowser');
 var prompt = require('react-dev-utils/prompt');
 var pathExists = require('path-exists');
@@ -273,9 +274,11 @@ detect(DEFAULT_PORT).then(port => {
   }
 
   clearConsole();
+  var existingProcess = getProcessForPort(DEFAULT_PORT);
   var question =
-    chalk.yellow('Something is already running on port ' + DEFAULT_PORT + '.') +
-    '\n\nWould you like to run the app on another port instead?';
+    chalk.yellow('Something is already running on port ' + DEFAULT_PORT + '.' +
+      ((existingProcess) ? ' Probably:\n  ' + existingProcess : '')) +
+      '\n\nWould you like to run the app on another port instead?';
 
   prompt(question, true).then(shouldChangePort => {
     if (shouldChangePort) {
