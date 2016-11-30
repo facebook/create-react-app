4165e2f81cebbc9d42db946be0001907a69b4d78
Zach Perrault <zach.perrault@gmail.com>
2016-11-30 08:28:00 -0600

    Use `eval-source-map`
    `cheap-module-source-map` hasn't been working well and the potential
    slowdown is worth it.
    
    🙌 Props to Kelly for finding that `eval-source-map` works well

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -34,7 +34,7 @@ module.exports = {
   // We don't use source maps here because they can be confusing:
   // https://github.com/facebookincubator/create-react-app/issues/343#issuecomment-237241875
   // You may want 'cheap-module-source-map' instead if you prefer source maps.
-  devtool: 'cheap-module-source-map',
+  devtool: 'eval-source-map',
   // These are the "entry points" to our application.
   // This means they will be the "root" imports that are included in JS bundle.
   // The first two entry points enable "hot" CSS and auto-refreshes for JS.
