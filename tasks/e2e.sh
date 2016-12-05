3a36693a05ed239397142e1571a3b9df506472a4
Dan Abramov <dan.abramov@gmail.com>
2016-12-05 21:58:31 +0000

    Update e2e.sh (#1167)

--- tasks/e2e.sh
+++ tasks/e2e.sh
@@ -56,7 +56,7 @@ root_path=$PWD
 if [ "$USE_YARN" = "yes" ]
 then
   # Install Yarn so that the test can use it to install packages.
-  npm install -g yarn
+  npm install -g yarn@0.17.10 # TODO: remove version when https://github.com/yarnpkg/yarn/issues/2142 is fixed.
   yarn cache clean
 fi
 
