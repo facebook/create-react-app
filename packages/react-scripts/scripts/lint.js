3c10e34ca0ba03183e87dd0a1918320022c87412
Zach Perrault <zach.perrault@gmail.com>
2016-12-06 10:02:29 -0600

    Enable eslint `fix` option and write fixes to disk

--- packages/react-scripts/scripts/lint.js
+++ packages/react-scripts/scripts/lint.js
@@ -4,11 +4,14 @@ var paths = require('../config/paths');
 var CLIEngine = require('eslint').CLIEngine;
 var config = require('@trunkclub/eslint-config');
 
+config.fix = true;
+
 var eslint = new CLIEngine(config);
 
 var report = eslint.executeOnFiles([paths.appSrc]);
 var formatter = eslint.getFormatter();
 
+CLIEngine.outputFixes(report);
 console.log(formatter(report.results));
 
 if (report.errorCount !== 0) process.exit(1);
