5cc329da6ee669f1850d45216e80b774d743f9d7
harunhasdal <harun.hasdal@rma-consulting.com>
2016-12-06 10:53:55 +0000

    Move path module inclusion right after the other imports
    Re: #1174

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -9,10 +9,6 @@
  */
 // @remove-on-eject-end
 
-// @remove-on-eject-begin
-// `path` is not used after eject - see https://github.com/facebookincubator/create-react-app/issues/1174
-var path = require('path');
-// @remove-on-eject-end
 var autoprefixer = require('autoprefixer');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -22,6 +18,11 @@ var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 var url = require('url');
 var paths = require('./paths');
 var getClientEnvironment = require('./env');
+
+// @remove-on-eject-begin
+// `path` is not used after eject - see https://github.com/facebookincubator/create-react-app/issues/1174
+var path = require('path');
+// @remove-on-eject-end
 
 function ensureSlash(path, needsSlash) {
   var hasSlash = path.endsWith('/');
