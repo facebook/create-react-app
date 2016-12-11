31f8622323017cd51dce24de40097ba837d4648d
Will Monk <will.monk@thetrainline.com>
2016-12-11 15:34:44 +0000

    fix copy

--- packages/react-scripts/template/src/App.tsx
+++ packages/react-scripts/template/src/App.tsx
@@ -12,7 +12,7 @@ class App extends React.Component<null, null> {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
     );
