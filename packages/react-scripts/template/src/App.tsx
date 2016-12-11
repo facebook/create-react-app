940033f190c199be3160705bbf292a5ea9f4fa7d
Will Monk <will.monk@thetrainline.com>
2016-12-11 15:25:50 +0000

    fix styling

--- packages/react-scripts/template/src/App.tsx
+++ packages/react-scripts/template/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as Styles from './App.css';
+import './App.css';
 
 const logo = require('./logo.svg');
 
@@ -7,8 +7,8 @@ class App extends React.Component<null, null> {
   render() {
     return (
       <div className="App">
-        <img src={logo} />
         <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
