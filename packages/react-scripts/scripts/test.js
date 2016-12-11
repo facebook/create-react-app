a13a65f1c53db486eb0f5d509a9527be0a3d5656
Dan Abramov <dan.abramov@gmail.com>
2016-12-11 03:33:34 -0800

    Clear scrollback in test mode (#1237)

--- packages/react-scripts/scripts/test.js
+++ packages/react-scripts/scripts/test.js
@@ -26,6 +26,18 @@ if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');
 }
 
+// A temporary hack to clear terminal correctly.
+// You can remove this after updating to Jest 18 when it's out.
+// https://github.com/facebook/jest/pull/2230
+var realWrite = process.stdout.write;
+var CLEAR = process.platform === 'win32' ? '\x1Bc' : '\x1B[2J\x1B[3J\x1B[H';
+process.stdout.write = function(chunk, encoding, callback) {
+  if (chunk === '\x1B[2J\x1B[H') {
+    chunk = CLEAR;
+  }
+  return realWrite.call(this, chunk, encoding, callback);
+};
+
 // @remove-on-eject-begin
 // This is not necessary after eject because we embed config into package.json.
 const createJestConfig = require('../utils/createJestConfig');
