94c912dc60561c931232caf9cf5442082711227c
Cary Landholt <cary@landholt.com>
2016-12-13 10:13:30 -0600

    Remove interactive shell check when opening browser on start (#1264)
    Browser launch can still be suppressed using BROWSER=none

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -286,9 +286,7 @@ function runDevServer(host, port, protocol) {
     console.log(chalk.cyan('Starting the development server...'));
     console.log();
 
-    if (isInteractive) {
-      openBrowser(protocol + '://' + host + ':' + port + '/');
-    }
+    openBrowser(protocol + '://' + host + ':' + port + '/');
   });
 }
 
