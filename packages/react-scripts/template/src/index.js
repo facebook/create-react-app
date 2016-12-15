aa4bfb240b7512b2cdcc049143789267a1d525d6
Zach Perrault <zach.perrault@gmail.com>
2016-12-15 12:47:49 -0600

    5.7.0

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom'
 import App from 'App'
 import 'index.css'
 
+console.log(process.env)
+
+window.env = process.env
+
 ReactDOM.render(<App/>, document.getElementById('root'))
