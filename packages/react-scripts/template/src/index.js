7652b2a70f9904b68a2ab7c5888b7b3aa93e7469
Rory Hunter <roryhunter2@gmail.com>
2016-12-16 11:09:06 +0000

    Build the prod bundle as UMD and export render function
    Derive a library name from the project name in package.json.
    
    In the index.js template, don't render the App unless window is defined,
    and export a render-to-string function.

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { renderToString } from 'react-dom/server';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (typeof window !== 'undefined') {
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+export default function render() {
+  return renderToString(<App />);
+}
