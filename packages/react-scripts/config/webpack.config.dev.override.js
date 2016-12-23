5be5d630b15f701b51f8d3d0a895ffb563e85b1c
Yanglin Zhao <yz1876@gmail.com>
2016-12-23 17:40:19 -0500

    Add override configs

--- packages/react-scripts/config/webpack.config.dev.override.js
+++ packages/react-scripts/config/webpack.config.dev.override.js
@@ -1,5 +1,13 @@
-function devConfigOverride(config) {
+var overrideConfig = function(config) {
   return config;
-}
+};
 
-module.exports = devConfigOverride;
+// @remove-on-eject-begin
+var path = require('path');
+var overrideConfigPath = path.resolve(process.cwd(), 'config/webpack.config.dev.override.js');
+overrideConfig = require(overrideConfigPath);
+// @remove-on-eject-end
+
+module.exports = function devConfigOverride(config) {
+  return overrideConfig(config);
+};
--- packages/react-scripts/config/webpack.config.prod.override.js
+++ packages/react-scripts/config/webpack.config.prod.override.js
@@ -1,5 +1,13 @@
-function prodConfigOverride(config) {
+var overrideConfig = function(config) {
   return config;
-}
+};
 
-module.exports = prodConfigOverride;
+// @remove-on-eject-begin
+var path = require('path');
+var overrideConfigPath = path.resolve(process.cwd(), 'config/webpack.config.prod.override.js');
+overrideConfig = require(overrideConfigPath);
+// @remove-on-eject-end
+
+module.exports = function prodConfigOverride(config) {
+  return overrideConfig(config);
+};
