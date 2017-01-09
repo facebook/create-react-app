24abc5e1542dce86f5f588b81723f262ed757ad6
Ville Immonen <ville.immonen@iki.fi>
2017-01-09 17:25:31 +0200

    Use yarnpkg alias to run Yarn (#1365)
    There’s a common tool included in Hadoop that also has a `yarn` command,
    which created issues for users who had Hadoop installed:
    * #1257
    * #1363
    
    Yarn also installs the command under `yarnpkg` alias (added in
    https://github.com/yarnpkg/yarn/commit/cefa9a368dd3df1a1f1ecd9e50de9c5fabefbc92)
    so we can use `yarnpkg` instead of `yarn` to make it more reliable.
    
    This has no effect on users who don't have Hadoop installed, but those
    who have won't see errors from falsely detecting Hadoop Yarn as Yarn
    the package manager, and they can now also install Yarn to make use of
    our Yarn support without the Hadoop Yarn interfering.

--- packages/react-scripts/scripts/eject.js
+++ packages/react-scripts/scripts/eject.js
@@ -147,7 +147,7 @@ prompt(
   if (pathExists.sync(paths.yarnLockFile)) {
     console.log(cyan('Running yarn...'));
     fs.removeSync(ownPath);
-    spawnSync('yarn', [], {stdio: 'inherit'});
+    spawnSync('yarnpkg', [], {stdio: 'inherit'});
   } else {
     console.log(cyan('Running npm install...'));
     fs.removeSync(ownPath);
