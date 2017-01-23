4ff49f1cae168c92298903a7e0816c67b621f0a4
Joe Haddad <timer150@gmail.com>
2017-01-23 18:55:02 -0500

    noop ProgressPlugin when running CI

--- packages/react-dev-utils/BuildProgressPlugin.js
+++ packages/react-dev-utils/BuildProgressPlugin.js
@@ -7,16 +7,26 @@ const ProgressBar = require('progress');
 const chalk = require('chalk');
 
 function BuildProgressPlugin() {
+  if (process.env.CI) {
+    return new ProgressPlugin(function handler(percentage, msg) {
+      // noop
+    })
+  }
   const bar = new ProgressBar(`  [:bar] ${ chalk.bold(':percent') } ${ chalk.yellow(':etas') } (${ chalk.dim(':msg') })`, {
     total: 100,
     complete: '=',
     incomplete: ' ',
     width: 25
   });
-  return new ProgressPlugin(function (percent, msg) {
-    if (percent === 1) msg = 'completed';
+  return new ProgressPlugin(function handler(percent, msg) {
+    const done = percent === 1;
+    if (done) {
+      msg = 'completed';
+    }
     bar.update(percent, { msg });
-    if (percent === 1) bar.terminate();
+    if (done) {
+      bar.terminate();
+    }
   });
 }
 
