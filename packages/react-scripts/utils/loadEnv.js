e93c432f9e0d9d13e5788155483fe84ed17e1779
Jason Block <jason@jasontheblock.com>
2017-02-01 17:37:08 -0600

    Export `TC_ENV` within apps (#63)
    * Exports TC_ENV
    
    * Bumps package.json

--- packages/react-scripts/utils/loadEnv.js
+++ packages/react-scripts/utils/loadEnv.js
@@ -6,6 +6,11 @@ var dotenv = require('dotenv');
 var env = process.env.NODE_ENV;
 var appDirectory = fs.realpathSync(process.cwd());
 
+// CRA assumes two environments: development or production.
+// There are some scenarios where we need to explicitly know that
+// we are in a staging environment.
+process.env.TC_ENV = env;
+
 // dotenv will not change values that are already set, sourcing the
 // overrides firsts means they will beat anything in the default
 // [process.env.NODE_ENV].env file.
