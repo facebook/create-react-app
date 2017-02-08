b439ab4eae41ca3b4bd0d29ce3e2db7f57a24e08
Zach Perrault <zach.perrault@gmail.com>
2017-02-08 07:03:50 -0600

    Move `TC_CLIENT_` envars into `loadEnv`
    This makes them available in tests

--- packages/react-scripts/utils/loadEnv.js
+++ packages/react-scripts/utils/loadEnv.js
@@ -2,6 +2,9 @@
 var fs = require('fs');
 var path = require('path');
 var dotenv = require('dotenv');
+var paths = require('../config/paths');
+var pkg = require(paths.appPackageJson);
+var git = require('git-rev-sync');
 
 var env = process.env.NODE_ENV;
 var appDirectory = fs.realpathSync(process.cwd());
@@ -10,6 +13,11 @@ var appDirectory = fs.realpathSync(process.cwd());
 // There are some scenarios where we need to explicitly know that
 // we are in a staging environment.
 process.env.TC_ENV = env;
+
+process.env.TC_CLIENT_APP_NAME = pkg.name,
+process.env.TC_CLIENT_BUILD_COMMIT = git.long(),
+process.env.TC_CLIENT_BUILD_TIME = (new Date()).toISOString()
+
 
 // dotenv will not change values that are already set, sourcing the
 // overrides firsts means they will beat anything in the default
