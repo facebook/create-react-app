c7d4897800c7c4e245befd253f06486cac74c745
Michael LaCroix <michael@lacroixdesign.net>
2017-02-09 14:54:17 -0600

    Have the resolver fall back to the app's modules (#66)
    * Have the resolver fall back to the app's modules
    
    * Bump patch version

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -74,7 +74,7 @@ module.exports = {
     // We use `fallback` instead of `root` because we want `node_modules` to "win"
     // if there any conflicts. This matches Node resolution mechanism.
     // https://github.com/facebookincubator/create-react-app/issues/253
-    fallback: paths.nodePaths,
+    fallback: [].concat(paths.appNodeModules, paths.nodePaths),
     root: paths.appSrc,
     // These are the reasonable defaults supported by the Node ecosystem.
     // We also include JSX as a common component filename extension to support
