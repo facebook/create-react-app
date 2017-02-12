6271b875b367c39e52368e40300fb652dced2129
vladimir.filimon <vladimir.filimon@gmail.com>
2017-02-12 01:09:03 +0100

    Disallow event global variable with no-restricted-globals rule (#1530)

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -213,6 +213,7 @@ module.exports = {
     // https://github.com/gajus/eslint-plugin-flowtype
     'flowtype/define-flow-type': 'warn',
     'flowtype/require-valid-file-annotation': 'warn',
-    'flowtype/use-flow-type': 'warn'
+    'flowtype/use-flow-type': 'warn',
+    'no-restricted-globals': ['warn', 'event']
   }
 };
