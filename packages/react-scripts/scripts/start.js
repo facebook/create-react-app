d0f8582221e4e11a40c0449ecde2a5bda183a818
Kees Kluskens <kees@webduck.nl>
2017-02-15 03:41:31 +0100

    Enable `watchContentBase` for webpack-dev-server (#1546)
    Resolves #1508

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -249,6 +249,8 @@ function runDevServer(host, port, protocol) {
     // for some reason broken when imported through Webpack. If you just want to
     // use an image, put it in `src` and `import` it from JavaScript instead.
     contentBase: paths.appPublic,
+    // By default files from `contentBase` will not trigger a page reload.
+    watchContentBase: true,
     // Enable hot reloading server. It will provide /sockjs-node/ endpoint
     // for the WebpackDevServer client so it can learn when the files were
     // updated. The WebpackDevServer client is included as an entry point
