dd801e218c913ed3292f3667d690e6b64b68e015
Vlad <vladfilimon@gmail.com>
2017-02-24 20:29:04 +0100

    Disallow event global variable with no-restricted-globals rule (#1530) (#1532)
    * Disallow event global variable with no-restricted-globals rule (#1530)
    
    * changed event global variable usage liniting from warn to error

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -123,6 +123,7 @@ module.exports = {
     'no-this-before-super': 'warn',
     'no-throw-literal': 'warn',
     'no-undef': 'error',
+    'no-restricted-globals': ['error', 'event'],
     'no-unexpected-multiline': 'warn',
     'no-unreachable': 'warn',
     'no-unused-expressions': ['warn', {
