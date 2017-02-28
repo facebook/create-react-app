fe39eba6be9f6e3cbb20165935c34ede789c6c36
Dan Abramov <gaearon@fb.com>
2017-02-28 15:58:28 +0000

    Work around Uglify bug

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -255,7 +255,10 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         screw_ie8: true, // React doesn't support IE8
-        warnings: false
+        warnings: false,
+        // Work around https://github.com/mishoo/UglifyJS2/issues/1516 until it's fixed:
+        reduce_vars: false,
+        collapse_vars: false
       },
       mangle: {
         screw_ie8: true
