f2d2b4aa03a5968447c1c04c5ef213ab641dcea4
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-03-02 02:50:07 +0700

    break the functionality to different functions

--- packages/react-scripts/utils/bundleVendorIfStale.js
+++ packages/react-scripts/utils/bundleVendorIfStale.js
@@ -10,45 +10,45 @@ var printErrors = require("../utils/printErrors");
 var environment = process.env.NODE_ENV;
 var vendorHash = require("../utils/vendorHash");
 
-module.exports = (callback) => {
+module.exports = initMainCompiler => {
   if (shouldVendorBundleUpdate()) {
+    // Read vendor path for stale files
     return fs.readdir(paths.vendorPath, (err, files) => {
-      try {
-        // delete all stale vendor bundle for this environment
-        files.filter(file => !file.indexOf(environment)).forEach(file => {
-          fs.unlinkSync(path.join(paths.vendorPath, file));
-        });
-      } catch (e) {
-      }
-      var compiler = webpack(config);
+      cleanUpStaleFiles(files);
+
       console.log("Compiling vendor bundle for faster rebuilds...");
-      compiler.run((err, stats) => {
-        if (err) {
-          printErrors("Failed to compile.", [err]);
-          process.exit(1);
-        }
+      webpack(config).run((err, stats) => {
+        checkForErrors(err, stats);
 
-        if (stats.compilation.errors.length) {
-          printErrors("Failed to compile.", stats.compilation.errors);
-          process.exit(1);
-        }
-
-        if (process.env.CI && stats.compilation.warnings.length) {
-          printErrors(
-            "Failed to compile. When process.env.CI = true, warnings are treated as failures. Most CI servers set this automatically.",
-            stats.compilation.warnings
-          );
-          process.exit(1);
-        }
-
-        console.log(chalk.green("Vendor bundle compiled successfully."));
-        console.log();
-        callback();
+        // When the process still run until here, there are no errors :)
+        console.log(chalk.green("Vendor bundle compiled successfully!"));
+        initMainCompiler(); // Let the main compiler do its job
       });
     });
   }
-  return callback();
+  // Just run the main compiler if vendor bundler is up to date
+  return initMainCompiler();
 };
+
+function checkForErrors(err, stats) {
+  if (err) {
+    printErrors("Failed to compile.", [err]);
+    process.exit(1);
+  }
+
+  if (stats.compilation.errors.length) {
+    printErrors("Failed to compile.", stats.compilation.errors);
+    process.exit(1);
+  }
+
+  if (process.env.CI && stats.compilation.warnings.length) {
+    printErrors(
+      "Failed to compile. When process.env.CI = true, warnings are treated as failures. Most CI servers set this automatically.",
+      stats.compilation.warnings
+    );
+    process.exit(1);
+  }
+}
 
 function manifestExists() {
   return fs.existsSync(path.join(paths.vendorPath, vendorHash + ".json"));
@@ -58,9 +58,22 @@ function shouldVendorBundleUpdate() {
   clearConsole();
   console.log("Checking if " + vendorHash + " vendor bundle exists");
   if (manifestExists()) {
-    console.log("Vendor bundle is up to date and safe to use!");
+    clearConsole();
+    console.log(chalk.green("Vendor bundle is up to date and safe to use!"));
     return false;
   }
   console.log("Vendor bundle needs to be compiled...");
   return true;
+}
+
+function cleanUpStaleFiles(files) {
+  try {
+    // delete all stale vendor bundle for this environment
+    files.filter(file => !file.indexOf(environment)).forEach(file => {
+      fs.unlinkSync(path.join(paths.vendorPath, file));
+    });
+  } catch (e) {
+    // Let the user knows that the stale file is not deleted
+    console.log(chalk.red("Failed to delete stale files"));
+  }
 }
