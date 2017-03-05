2f7e3a216804e774c0399a081d318f689e649342
Dan Abramov <dan.abramov@gmail.com>
2017-03-05 22:29:28 +0000

    Lint internal scripts with eslint:recommended (#1729)
    * Lint internal scripts with eslint:recommended
    
    * Warnings r bad

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -14,13 +14,13 @@ case 'test':
     {stdio: 'inherit'}
   );
   if (result.signal) {
-    if (result.signal == 'SIGKILL') {
+    if (result.signal === 'SIGKILL') {
       console.log(
         'The build failed because the process exited too early. ' +
         'This probably means the system ran out of memory or someone called ' +
         '`kill -9` on the process.'
       );
-    } else if (result.signal == 'SIGTERM') {
+    } else if (result.signal === 'SIGTERM') {
       console.log(
         'The build failed because the process exited too early. ' +
         'Someone might have called `kill` or `killall`, or the system could ' +
