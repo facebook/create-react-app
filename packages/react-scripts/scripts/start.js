d209e4c5e6d9a25531dd3699aa93c4a9e300b393
William Monk <will@williammonk.co.uk>
2017-03-07 20:06:57 +0000

    Stop watch process when process.stdin ends

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -47,6 +47,11 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
 
+process.stdin.on('end', function() {
+  process.exit(0);
+});
+process.stdin.resume();
+
 // Tools like Cloud9 rely on this.
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
