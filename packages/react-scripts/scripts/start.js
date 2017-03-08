4053774cbe0a72512642366468f5eecd05899baf
William Monk <will@williammonk.co.uk>
2017-03-08 08:24:34 +0000

    Add process.stdin On End Explanation

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -47,6 +47,11 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
 
+// Issue: https://github.com/facebookincubator/create-react-app/issues/1753
+// The below lines are added to make sure that this process is
+// exited when stdin is ended. The consequence of not doing this means
+// that all watch processes will stay running despite the process that spawned
+// them being closed.
 process.stdin.on('end', function() {
   process.exit(0);
 });
