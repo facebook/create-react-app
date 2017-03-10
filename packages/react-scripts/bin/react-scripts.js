9d1ed095558052348a8905094b61dd497a3efa01
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-03-10 09:38:45 +0700

    add watch to react-scripts case

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -20,6 +20,7 @@ const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
 switch (script) {
   case 'build':
   case 'eject':
+  case 'watch':
   case 'start':
   case 'test': {
     const result = spawn.sync(
