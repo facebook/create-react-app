356603ff6cf651d8f5417730eb77afb13a1b1cd8
Michael Hsu <evenchange4@gmail.com>
2017-03-10 10:06:44 +0800

    docs(babel-preset): Update comment info about babel-preset-env. (#1787)

--- packages/babel-preset-react-app/index.js
+++ packages/babel-preset-react-app/index.js
@@ -98,7 +98,7 @@ if (env === 'test') {
     plugins: plugins.concat([
       // function* () { yield 42; yield 43; }
       [require.resolve('babel-plugin-transform-regenerator'), {
-        // Async functions are converted to generators by babel-preset-latest
+        // Async functions are converted to generators by babel-preset-env
         async: false
       }],
     ])
