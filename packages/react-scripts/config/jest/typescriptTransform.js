dfb348447a70618c2324414141c3c92e7c63d889
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-03-29 19:09:23 +0200

    Use "compilerOptions" from projects tsconfig for jest
    * .. if file is present
    * .. if file is valid json
    * .. if compilerOptions are set in tsconfig.json
    * .. fall back to old value in other cases

--- packages/react-scripts/config/jest/typescriptTransform.js
+++ packages/react-scripts/config/jest/typescriptTransform.js
@@ -1,21 +1,33 @@
 // Copyright 2004-present Facebook. All Rights Reserved.
 
+const fs = require('fs');
 const tsc = require('typescript');
+const tsconfigPath = require('app-root-path').resolve('/tsconfig.json');
+
+let compilerConfig = {
+  module: tsc.ModuleKind.CommonJS,
+  jsx: tsc.JsxEmit.React,
+}
+
+if (fs.existsSync(tsconfigPath)) {
+  try {
+    const tsconfig = require(tsconfigPath);
+
+    if (tsconfig && tsconfig.compilerOptions) {
+      compilerConfig = tsconfig.compilerOptions;
+    }
+  } catch (e) { /* Do nothing - default is set */ }
+}
 
 module.exports = {
   process(src, path) {
     if (path.endsWith('.ts') || path.endsWith('.tsx')) {
       return tsc.transpile(
         src,
-        {
-          module: tsc.ModuleKind.CommonJS,
-          jsx: tsc.JsxEmit.React,
-        },
-        path,
-        []
+        compilerConfig,
+        path, []
       );
     }
     return src;
   },
 };
-
