2be8b1689529d4fc9a1f6efe85932f0232f5fef5
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-04-01 20:24:58 +0700

    add package json as source of vendor hash

--- packages/react-scripts/utils/vendorHash.js
+++ packages/react-scripts/utils/vendorHash.js
@@ -1,13 +1,17 @@
-var crypto = require('crypto');
-var fs = require('fs');
-var path = require('path')
-var paths = require('../config/paths');
-var hash = crypto.createHash('md5');
-var input = fs.readFileSync(path.join(paths.vendorPath, 'index.js'));
+'use strict';
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const paths = require('../config/paths');
+const hash = crypto.createHash('md5');
+const input = fs.readFileSync(path.join(paths.vendorPath, 'index.js'));
+const appPackageJson = fs.readFileSync(paths.appPackageJson);
+
 hash.update(input);
+hash.update(appPackageJson);
 
-var hashed = hash.digest('hex');
-module.exports = 
-process.env.REACT_APP_VENDOR_HASH = 
-[process.env.NODE_ENV, hashed].join('.');
-
+const hashed = hash.digest('hex');
+module.exports = (process.env.REACT_APP_VENDOR_HASH = [
+  process.env.NODE_ENV,
+  hashed,
+].join('.'));
