b2d5d15a4cacf7afef4ee686018b9a7538077480
Paul-Guillaume Déjardin <pgdejardin@xebia.fr>
2017-04-06 16:27:33 +0200

    Override Jest config by package.json Jest config

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -11,6 +11,7 @@
 
 const fs = require('fs');
 const paths = require('../../config/paths');
+const packageConfig = require(paths.appPackageJson).jest;
 
 module.exports = (resolve, rootDir, isEjecting) => {
   // Use this instead of `paths.testsSetup` to avoid putting
@@ -45,5 +46,5 @@ module.exports = (resolve, rootDir, isEjecting) => {
   if (rootDir) {
     config.rootDir = rootDir;
   }
-  return config;
+  return Object.assign({}, config, packageConfig);
 };
