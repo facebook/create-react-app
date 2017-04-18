f0244e9c657ca6f875498a46f91d2761870ce531
Marco Fugaro <marco.fugaro@gmail.com>
2017-04-18 18:26:52 +0200

    removed redundant already default options in UglifyJsPlugin

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -258,15 +258,7 @@ module.exports = {
     // Minify the code.
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        screw_ie8: true, // React doesn't support IE8
         warnings: false,
-      },
-      mangle: {
-        screw_ie8: true,
-      },
-      output: {
-        comments: false,
-        screw_ie8: true,
       },
       sourceMap: true,
     }),
