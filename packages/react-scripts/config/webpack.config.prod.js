a1c54f28d90a10f1edef8dcc4952c3ccd277d6e0
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-04-22 04:43:38 +0700

    turn on sourcemap for production

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -198,6 +198,7 @@ module.exports = {
                   options: {
                     importLoaders: 1,
                     minimize: true,
+                    sourceMap: true,
                   },
                 },
                 {
