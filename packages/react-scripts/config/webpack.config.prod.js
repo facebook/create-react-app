d6173fd2a4f54fd52791037b52b32319a1ba5c4e
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-04-22 08:15:58 +0700

    Minify and map css (#2013)
    Minify and map css

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -197,6 +197,8 @@ module.exports = {
                   loader: 'css-loader',
                   options: {
                     importLoaders: 1,
+                    minimize: true,
+                    sourceMap: true,
                   },
                 },
                 {
