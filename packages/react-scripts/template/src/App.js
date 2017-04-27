6c2a24741e8a907f807e815ec2c55888507fc09a
Robin van der Vleuten <robin@webstronauts.co>
2017-04-27 15:31:45 +0200

    Updated template to contain css modules.

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -5,12 +5,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div styleName="app">
+        <div styleName="header">
+          <img src={logo} styleName="logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p styleName="intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
