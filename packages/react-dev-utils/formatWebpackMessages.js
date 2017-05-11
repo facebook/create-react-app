1ede6a8d123995d13aac87efd3bff22973b2dc5c
Dan Abramov <dan.abramov@gmail.com>
2017-05-11 15:54:31 +0100

    Only show first error (#2125)

--- packages/react-dev-utils/formatWebpackMessages.js
+++ packages/react-dev-utils/formatWebpackMessages.js
@@ -177,6 +177,11 @@ function formatWebpackMessages(json) {
     // preceding a much more useful Babel syntax error.
     result.errors = result.errors.filter(isLikelyASyntaxError);
   }
+  // Only keep the first error. Others are often indicative
+  // of the same problem, but confuse the reader with noise.
+  if (result.errors.length > 1) {
+    result.errors.length = 1;
+  }
   return result;
 }
 
