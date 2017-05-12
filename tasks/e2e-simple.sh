c665ebe4e53738a9aefe25efac322dfe1433779d
Dan Abramov <dan.abramov@gmail.com>
2017-05-12 15:24:17 +0100

    Try 0.24.2

--- tasks/e2e-simple.sh
+++ tasks/e2e-simple.sh
@@ -93,7 +93,7 @@ fi
 if [ "$USE_YARN" = "yes" ]
 then
   # Install Yarn so that the test can use it to install packages.
-  npm install -g yarn
+  npm install -g yarn@0.24.2
   yarn cache clean
 fi
 
