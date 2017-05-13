43a97e3e0d070f94f1a368a69063707032304c4f
Joe Haddad <timer150@gmail.com>
2017-05-12 21:30:32 -0400

    Oops

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -2,28 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Bad from './fold/Bad';
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <div>
-            <div>
-              <div>
-                <div>
-                  <div>
-                    <div>
-                      <img src={logo} className="App-logo" alt="logo" />
-                      <Bad>Welcome to React</Bad>
-                      {[1, 2, 3].map(s => <span>{s}</span>)}
-                    </div>{' '}
-                  </div>{' '}
-                </div>{' '}
-              </div>{' '}
-            </div>{' '}
-          </div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- packages/react-scripts/template/src/fold/Bad.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-let Lol = null;
-export default function Bad() {
-  return <div><h2><Lol /></h2></div>;
-}
