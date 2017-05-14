226088b265a538a8bd356f887d63152b30779b20
Joe Haddad <timer150@gmail.com>
2017-05-14 16:10:46 -0400

    Switch to correct default host

--- packages/react-scripts/config/webpackDevServer.config.js
+++ packages/react-scripts/config/webpackDevServer.config.js
@@ -15,7 +15,7 @@ const config = require('./webpack.config.dev');
 const paths = require('./paths');
 
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || '0.0.0.0';
 
 module.exports = function(proxy) {
   return {
