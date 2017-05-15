7bff7154d931384e407d72a607c8a482a989a3fd
Almero Steyn <almero.steyn@gmail.com>
2017-05-16 00:25:40 +0200

    ESLint Config: Upgrade eslint-plugin-jsx-a11y and activate more rules. (#2163)
    * ESLint Config: Upgrade eslint-plugin-jsx-a11y and activate more rules.
    
    * Try to fix Lerna
    
    * Update README.md
    
    * Remove html-has-lang
    
    * Update README.md

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -294,10 +294,24 @@ module.exports = {
     'react/style-prop-object': 'warn',
 
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/tree/master/docs/rules
+    'jsx-a11y/accessible-emoji': 'warn',
+    'jsx-a11y/alt-text': 'warn',
+    'jsx-a11y/anchor-has-content': 'warn',
+    'jsx-a11y/aria-activedescendant-has-tabindex': 'warn',
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
     'jsx-a11y/aria-role': 'warn',
-    'jsx-a11y/img-has-alt': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/heading-has-content': 'warn',
+    'jsx-a11y/href-no-hash': 'warn',
+    'jsx-a11y/iframe-has-title': 'warn',
     'jsx-a11y/img-redundant-alt': 'warn',
     'jsx-a11y/no-access-key': 'warn',
+    'jsx-a11y/no-distracting-elements': 'warn',
+    'jsx-a11y/no-redundant-roles': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'jsx-a11y/scope': 'warn',
 
     // https://github.com/gajus/eslint-plugin-flowtype
     'flowtype/define-flow-type': 'warn',
