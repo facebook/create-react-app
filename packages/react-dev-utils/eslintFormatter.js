5e35645b95af92b8b45cbb5e703e0bd8f38fd605
Dan Abramov <dan.abramov@gmail.com>
2017-05-15 01:15:58 +0100

    Focus on line in eslint messages

--- packages/react-dev-utils/eslintFormatter.js
+++ packages/react-dev-utils/eslintFormatter.js
@@ -31,7 +31,7 @@ function formatter(results) {
 
       let line = message.line || 0;
       let column = message.column || 0;
-      let position = chalk.dim(`${line}:${column}`);
+      let position = chalk.dim(`Line ${line}:`);
       return [
         '',
         position,
