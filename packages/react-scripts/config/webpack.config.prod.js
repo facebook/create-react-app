871bf4ef82a186c7384d58af89fec5592f15e182
Joe Haddad <timer150@gmail.com>
2017-05-17 00:33:30 -0500

    Oops

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -103,8 +103,8 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
-      plugins: [new ModuleScopePlugin(paths.appSrc)],
     },
+    plugins: [new ModuleScopePlugin(paths.appSrc)],
   },
   module: {
     strictExportPresence: true,
