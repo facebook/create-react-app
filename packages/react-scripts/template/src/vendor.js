ff12a19ecfd52bdc69d9592aec4481f358f74b15
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-05-19 02:30:09 +0700

    tweak vendor entry point

--- packages/react-scripts/template/src/vendor.js
+++ packages/react-scripts/template/src/vendor.js
@@ -1,2 +1,5 @@
-require('react');
-require('react-dom');
+// Import your dependencies here and we'll generate vendor
+// bundle for faster rebuilds and long term caching.
+// Delete this file if you want to disable it.
+import 'react';
+import 'react-dom';
