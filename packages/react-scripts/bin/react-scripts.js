76cec85c3b0c0c89d482d98956d9b71abbdd9573
Danil Shashkov <shashkovdanil@gmail.com>
2017-05-18 22:05:22 +0500

    ES6 syntax

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -10,16 +10,16 @@
 
 'use strict';
 
-var spawn = require('cross-spawn');
-var script = process.argv[2];
-var args = process.argv.slice(3);
+const spawn = require('cross-spawn');
+const script = process.argv[2];
+const args = process.argv.slice(3);
 
 switch (script) {
   case 'build':
   case 'eject':
   case 'start':
-  case 'test':
-    var result = spawn.sync(
+  case 'test': {
+    const result = spawn.sync(
       'node',
       [require.resolve('../scripts/' + script)].concat(args),
       { stdio: 'inherit' }
@@ -42,6 +42,7 @@ switch (script) {
     }
     process.exit(result.status);
     break;
+  }
   default:
     console.log('Unknown script "' + script + '".');
     console.log('Perhaps you need to update react-scripts?');
