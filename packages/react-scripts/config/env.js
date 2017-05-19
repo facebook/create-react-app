4a32171331a17c097cc25c4caab5756130efcfb4
Oleg Kuzava <olegkuzava@gmail.com>
2017-05-19 16:47:21 +0300

    Fix read of .env variables (#2242)

--- packages/react-scripts/config/env.js
+++ packages/react-scripts/config/env.js
@@ -13,6 +13,9 @@
 const fs = require('fs');
 const paths = require('./paths');
 
+// Make sure that including paths.js after env.js will read .env variables.
+delete require.cache[require.resolve('./paths')];
+
 const NODE_ENV = process.env.NODE_ENV;
 if (!NODE_ENV) {
   throw new Error(
