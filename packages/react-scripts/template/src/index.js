b98ba3d7ea21a7c0cb4e250559dbe0e44b89698c
Dan Abramov <dan.abramov@gmail.com>
2017-05-19 01:46:47 +0100

    Better document PWA stuff

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './service-worker-registration';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
 registerServiceWorker();
