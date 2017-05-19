5d894090e90c26993afc1a51c0274bb0ad4af339
Dan Abramov <dan.abramov@gmail.com>
2017-05-19 19:55:17 +0100

    Update env.js

--- packages/react-scripts/config/env.js
+++ packages/react-scripts/config/env.js
@@ -59,7 +59,7 @@ dotenvFiles.forEach(dotenvFile => {
 const appDirectory = fs.realpathSync(process.cwd());
 process.env.NODE_PATH = (process.env.NODE_PATH || '')
   .split(path.delimiter)
-  .filter(folder => !path.isAbsolute(folder))
+  .filter(folder => folder && !path.isAbsolute(folder))
   .map(folder => path.resolve(appDirectory, folder))
   .join(path.delimiter);
 
