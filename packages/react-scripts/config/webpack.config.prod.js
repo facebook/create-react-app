c8ab840c4859c4af0ca7bc2b0b990ee8af77d1b6
fezhengjin <fezhengjin@gmail.com>
2017-05-19 15:20:30 +0800

    Added webpack alias used by Wizard.

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -104,6 +104,10 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      // Useful to import modules from the src folder.
+      '~': paths.appSrc,
+      // This path will be changed in the 'npm run deploy' process.
+      '~customize': path.join(paths.appSrc, 'customize/index'),
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
