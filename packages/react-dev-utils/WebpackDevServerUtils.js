2430b5615f139321cdeac31caaa369ad83d5b321
Joe Haddad <timer150@gmail.com>
2017-05-21 15:44:28 -0400

    Ensure address is private (#2297)
    * Ensure address is private
    Resolves #2296
    
    * adjust rule
    
    * Update WebpackDevServerUtils.js

--- packages/react-dev-utils/WebpackDevServerUtils.js
+++ packages/react-dev-utils/WebpackDevServerUtils.js
@@ -52,9 +52,22 @@ function prepareUrls(protocol, host, port) {
   if (isUnspecifiedHost) {
     prettyHost = 'localhost';
     try {
+      // This can only return an IPv4 address
       lanUrlForConfig = address.ip();
       if (lanUrlForConfig) {
-        lanUrlForTerminal = prettyPrintUrl(lanUrlForConfig);
+        // Check if the address is a private ip
+        // https://en.wikipedia.org/wiki/Private_network#Private_IPv4_address_spaces
+        if (
+          /^10[.]|^172[.](1[6-9]|2[0-9]|3[0-1])[.]|^192[.]168[.]/.test(
+            lanUrlForConfig
+          )
+        ) {
+          // Address is private, format it for later use
+          lanUrlForTerminal = prettyPrintUrl(lanUrlForConfig);
+        } else {
+          // Address is not private, so we will discard it
+          lanUrlForConfig = undefined;
+        }
       }
     } catch (_e) {
       // ignored
