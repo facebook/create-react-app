73f311e58860e6da41a2b555506d260222362c84
Dan Abramov <dan.abramov@gmail.com>
2017-05-27 12:36:01 +0100

    Add a comment

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -282,6 +282,10 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
+        // Disabled because of an issue with Uglify breaking seemingly valid code:
+        // https://github.com/facebookincubator/create-react-app/issues/2376
+        // Pending further investigation:
+        // https://github.com/mishoo/UglifyJS2/issues/2011
         comparisons: false,
         // This feature has been reported as buggy a few times, such as:
         // https://github.com/mishoo/UglifyJS2/issues/1964
