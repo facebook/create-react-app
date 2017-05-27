d49ba5467e6409f461b0b27b083a8be904a89127
David Ascher <david.ascher@gmail.com>
2017-05-26 20:02:14 -0700

    wrong plugin

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -259,9 +259,6 @@ module.exports = {
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       inject: true,
-      compress: {
-        comparisons: false
-      },
       template: paths.appHtml,
       minify: {
         removeComments: true,
@@ -285,6 +282,7 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
+        comparisons: false,
         // This feature has been reported as buggy a few times, such as:
         // https://github.com/mishoo/UglifyJS2/issues/1964
         // We'll wait with enabling it by default until it is more solid.
