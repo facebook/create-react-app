c1ba38dbf525029c637a89b9b33fd0e744b3fd2e
Zac Kwan <Zaccc123@users.noreply.github.com>
2017-05-27 19:33:13 +0800

    Removed the overriding of reduce_vars to false since webpack v2.6.0 included the fixed for Uglify bug (#2351)

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -282,10 +282,6 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
-        // This feature has been reported as buggy a few times, such as:
-        // https://github.com/mishoo/UglifyJS2/issues/1964
-        // We'll wait with enabling it by default until it is more solid.
-        reduce_vars: false,
       },
       output: {
         comments: false,
