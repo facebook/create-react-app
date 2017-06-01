d99c14f82725a14917e998e59e3eefeee78baa82
Forbes Lindesay <forbes@lindesay.co.uk>
2017-06-01 12:03:11 +0100

    Move polyfills.js into react-dev-utils
    [see #2442]

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -64,7 +64,7 @@ module.exports = {
   // You can exclude the *.map files from the build during deployment.
   devtool: 'source-map',
   // In production, we only want to load the polyfills and the app code.
-  entry: [require.resolve('./polyfills'), paths.appIndexJs],
+  entry: [require.resolve('react-dev-utils/polyfills'), paths.appIndexJs],
   output: {
     // The build folder.
     path: paths.appBuild,
