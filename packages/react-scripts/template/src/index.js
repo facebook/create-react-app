b264b8de33cb3a76d30cfc228a7a3a4218603157
bryankang <bryankang@users.noreply.github.com>
2017-06-05 18:36:13 -0500

    Prioritize index.css over the implicit App.css (#2470)

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
