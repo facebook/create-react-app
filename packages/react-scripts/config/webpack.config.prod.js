ec0f811501277c51278495595b8f016a119af262
Mico Piira <mico.piira@gmail.com>
2017-06-26 18:48:21 +0300

    Add empty mock for dgram (#2600)

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -325,6 +325,7 @@ module.exports = {
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
+    dgram: 'empty',
     fs: 'empty',
     net: 'empty',
     tls: 'empty',
