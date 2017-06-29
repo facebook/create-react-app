533f38278768ed60742510959e016b44fa6214ea
Dan Abramov <dan.abramov@gmail.com>
2017-06-29 11:39:10 +0100

    Fix external CSS imports

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -228,6 +228,9 @@ module.exports = {
                 {
                   loader: require.resolve('postcss-loader'),
                   options: {
+                    // Necessary for external CSS imports to work
+                    // https://github.com/facebookincubator/create-react-app/issues/2677
+                    ident: 'postcss',
                     plugins: () => [
                       require('postcss-flexbugs-fixes'),
                       autoprefixer({
