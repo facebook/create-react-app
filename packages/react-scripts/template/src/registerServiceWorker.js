08ff70dbb4751bb425095e4d7adf4130063ac338
Mahmoud Saada <mahmoudsaada@gmail.com>
2017-07-09 20:00:39 -0400

    Update registerServiceWorker.js

--- packages/react-scripts/template/src/registerServiceWorker.js
+++ packages/react-scripts/template/src/registerServiceWorker.js
@@ -7,14 +7,13 @@
 
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
-const {URL, fetch, location, addEventListener} = window;
 
 const isLocalhost = Boolean(
-  location.hostname === 'localhost' ||
+  window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
-    location.hostname === '[::1]' ||
+    window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
-    location.hostname.match(
+    window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
@@ -22,15 +21,15 @@ const isLocalhost = Boolean(
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, location);
-    if (publicUrl.origin !== location.origin) {
+    const publicUrl = new window.URL(process.env.PUBLIC_URL, window.location);
+    if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
       return;
     }
 
-    addEventListener('load', () => {
+    window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (!isLocalhost) {
@@ -75,7 +74,7 @@ function registerValidSW(swUrl) {
 
 function checkValidServiceWorker(swUrl) {
   // Check if the service worker can be found. If it can't reload the page.
-  fetch(swUrl)
+  window.fetch(swUrl)
     .then(response => {
       // Ensure service worker exists, and that we really are getting a JS file.
       if (
@@ -85,7 +84,7 @@ function checkValidServiceWorker(swUrl) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
           registration.unregister().then(() => {
-            location.reload();
+            window.location.reload();
           });
         });
       } else {
@@ -101,8 +100,8 @@ function checkValidServiceWorker(swUrl) {
 }
 
 export function unregister() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(registration => {
+  if ('serviceWorker' in window.navigator) {
+    window.navigator.serviceWorker.ready.then(registration => {
       registration.unregister();
     });
   }
