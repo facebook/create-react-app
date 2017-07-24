26ed6ab744bbbf31d809bc77b309d2625610a46e
einarlove <post@einarlove.com>
2017-07-25 00:27:21 +0200

    Upgrade chalk and use strip-ansi when needed

--- packages/react-dev-utils/eslintFormatter.js
+++ packages/react-dev-utils/eslintFormatter.js
@@ -10,6 +10,7 @@
 'use strict';
 
 const chalk = require('chalk');
+const stripAnsi = require('strip-ansi');
 const table = require('text-table');
 
 function isError(message) {
@@ -67,7 +68,7 @@ function formatter(results) {
     let outputTable = table(messages, {
       align: ['l', 'l', 'l'],
       stringLength(str) {
-        return chalk.stripColor(str).length;
+        return stripAnsi(str).length;
       },
     });
 
