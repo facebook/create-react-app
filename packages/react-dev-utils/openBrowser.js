22894ba7d3c26c5fe69f945fc4f7397a6df09948
Bryan Clark <clarkbw@gmail.com>
2017-07-28 10:47:49 -0700

    Use default-browser to use the users default browser even if chrome is running

--- packages/react-dev-utils/openBrowser.js
+++ packages/react-dev-utils/openBrowser.js
@@ -12,6 +12,7 @@
 var chalk = require('chalk');
 var execSync = require('child_process').execSync;
 var spawn = require('cross-spawn');
+var defaultBrowser = require('default-browser');
 var opn = require('opn');
 
 // https://github.com/sindresorhus/opn#app
@@ -63,47 +64,52 @@ function executeNodeScript(scriptPath, url) {
   return true;
 }
 
-function startBrowserProcess(browser, url) {
-  // If we're on OS X, the user hasn't specifically
-  // requested a different browser, we can try opening
-  // Chrome with AppleScript. This lets us reuse an
-  // existing tab when possible instead of creating a new one.
-  const shouldTryOpenChromeWithAppleScript =
-    process.platform === 'darwin' &&
-    (typeof browser !== 'string' || browser === OSX_CHROME);
+async function startBrowserProcess(browser, url) {
+  return await defaultBrowser().then(dBrowser => {
+    // { name: 'Chrome', id: 'com.google.chrome' }
+    if (dBrowser.id === 'com.google.chrome') {
+      // If we're on OS X, the user hasn't specifically
+      // requested a different browser, we can try opening
+      // Chrome with AppleScript. This lets us reuse an
+      // existing tab when possible instead of creating a new one.
+      const shouldTryOpenChromeWithAppleScript =
+        process.platform === 'darwin' &&
+        (typeof browser !== 'string' || browser === OSX_CHROME);
 
-  if (shouldTryOpenChromeWithAppleScript) {
-    try {
-      // Try our best to reuse existing tab
-      // on OS X Google Chrome with AppleScript
-      execSync('ps cax | grep "Google Chrome"');
-      execSync('osascript openChrome.applescript "' + encodeURI(url) + '"', {
-        cwd: __dirname,
-        stdio: 'ignore',
-      });
-      return true;
-    } catch (err) {
-      // Ignore errors.
+      if (shouldTryOpenChromeWithAppleScript) {
+        try {
+          // Try our best to reuse existing tab
+          // on OS X Google Chrome with AppleScript
+          execSync('ps cax | grep "Google Chrome"');
+          execSync('osascript openChrome.applescript "' + encodeURI(url) + '"', {
+            cwd: __dirname,
+            stdio: 'ignore',
+          });
+          return true;
+        } catch (err) {
+          // Ignore errors.
+        }
+      }
+    } else {
+      // Another special case: on OS X, check if BROWSER has been set to "open".
+      // In this case, instead of passing `open` to `opn` (which won't work),
+      // just ignore it (thus ensuring the intended behavior, i.e. opening the system browser):
+      // https://github.com/facebookincubator/create-react-app/pull/1690#issuecomment-283518768
+      if (process.platform === 'darwin' && browser === 'open') {
+        browser = undefined;
+      }
+
+      // Fallback to opn
+      // (It will always open new tab)
+      try {
+        var options = { app: browser };
+        opn(url, options).catch(() => {}); // Prevent `unhandledRejection` error.
+        return true;
+      } catch (err) {
+        return false;
+      }
     }
-  }
-
-  // Another special case: on OS X, check if BROWSER has been set to "open".
-  // In this case, instead of passing `open` to `opn` (which won't work),
-  // just ignore it (thus ensuring the intended behavior, i.e. opening the system browser):
-  // https://github.com/facebookincubator/create-react-app/pull/1690#issuecomment-283518768
-  if (process.platform === 'darwin' && browser === 'open') {
-    browser = undefined;
-  }
-
-  // Fallback to opn
-  // (It will always open new tab)
-  try {
-    var options = { app: browser };
-    opn(url, options).catch(() => {}); // Prevent `unhandledRejection` error.
-    return true;
-  } catch (err) {
-    return false;
-  }
+  });
 }
 
 /**
