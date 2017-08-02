83b3a6f787efb5c19e3aac83c4b1292820d3f8b9
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-08-02 20:34:52 +0700

    Rename to use SOURCE_MAP with explicit value

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -30,7 +30,7 @@ const publicPath = paths.servedPath;
 // For these, "homepage" can be set to "." to enable relative asset paths.
 const shouldUseRelativeAssetPaths = publicPath === './';
 // Source maps are resource heavy and can cause out of memory issue for large source files.
-const shouldUseSourceMap = !process.env.REACT_APP_DISABLE_SOURCE_MAP;
+const shouldUseSourceMap = process.env.SOURCE_MAP !== 'false';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
