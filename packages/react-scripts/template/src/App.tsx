62cfd49bae07b3d6ef3d058f1f51e3452093e584
Tom Crockett <pelotom@gmail.com>
2017-08-02 00:07:37 -0700

    Remove unneeded React.Component type parameters
    TypeScript's new type parameter defaulting makes these unnecessary.

--- packages/react-scripts/template/src/App.tsx
+++ packages/react-scripts/template/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component<{}, {}> {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
