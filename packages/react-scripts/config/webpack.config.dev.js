9b7901241f66df60ac53edadecd8cb9b082bce79
iamdoron <iamdoron@users.noreply.github.com>
2017-08-02 05:50:58 +0300

    Allow importing package.json (#2468)
    * Allow importing package.json
    
    * Remove package.json import from App.js template
    
    * fix importing package.json
    
    * Rename variable to reflect path is relative to root
    
    * Check for both package & package.json in ModuleScopePlugin
    
    * Use regex to check relative path to package.json
    
    * Strictly enforce package.json extension on scope plugin
    
    * Add allowedPaths to ModuleScopePlugin ctor and use it to allow app package.json
    
    * Remove package.json import from App.js template
    
    * Add package.json to react-scripts/template, show package version and name in the template
    
    * Remove import package.json from template
    
    * Remove template/package.json and its references in code
    
    * Update ModuleScopePlugin.js
    
    * Update README.md

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -126,7 +126,7 @@ module.exports = {
       // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
-      new ModuleScopePlugin(paths.appSrc),
+      new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
     ],
   },
   module: {
