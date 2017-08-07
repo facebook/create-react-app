3afca81e0336b61c09fac13cfa13557d8324b5a7
William Monk <will@williammonk.co.uk>
2017-08-07 17:27:41 +0200

    ! PUBLIC_URL

--- packages/react-scripts/template/src/registerServiceWorker.ts
+++ packages/react-scripts/template/src/registerServiceWorker.ts
@@ -23,7 +23,7 @@ export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(
-      process.env.PUBLIC_URL || '',
+      process.env.PUBLIC_URL!,
       window.location.toString()
     );
     if (publicUrl.origin !== window.location.origin) {
