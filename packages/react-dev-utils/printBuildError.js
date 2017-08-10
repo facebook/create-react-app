84fed63949db3c7e70fb1f5a1a62fb883eb65030
Joe Haddad <timer150@gmail.com>
2017-08-10 15:59:23 -0700

    Remove superfluous lodash usage

--- packages/react-dev-utils/printBuildError.js
+++ packages/react-dev-utils/printBuildError.js
@@ -9,12 +9,11 @@
 
 'use strict';
 
-const get = require('lodash/get');
 const chalk = require('chalk');
 
 module.exports = function printBuildError(err) {
-  const message = get(err, 'message');
-  const stack = get(err, 'stack');
+  const message = err != null && err.message;
+  const stack = err != null && err.stack;
 
   // Add more helpful message for UglifyJs error
   if (
@@ -23,24 +22,22 @@ module.exports = function printBuildError(err) {
     message.indexOf('from UglifyJs') !== -1
   ) {
     try {
-      const matched = /Unexpected token:(.+)\[(.+):(.+),(.+)\]\[.+\]/.exec(
-        stack
-      );
+      const matched = /(.+)\[(.+):(.+),(.+)\]\[.+\]/.exec(stack);
       if (!matched) {
-        throw new Error(
-          "The regex pattern is not matched. Maybe UglifyJs changed it's message?"
-        );
+        throw new Error('Using errors for control flow is bad.');
       }
       const problemPath = matched[2];
       const line = matched[3];
       const column = matched[4];
       console.log(
         'Failed to minify the code from this file: \n\n',
-        chalk.yellow(`${problemPath} line ${line}:${column}`),
+        chalk.yellow(
+          `\t${problemPath}:${line}${column !== '0' ? ':' + column : ''}`
+        ),
         '\n'
       );
     } catch (ignored) {
-      console.log('Failed to minify the code.', err);
+      console.log('Failed to minify the bundle.', err);
     }
     console.log('Read more here: http://bit.ly/2tRViJ9');
   } else {
