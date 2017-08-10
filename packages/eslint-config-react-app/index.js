75d380b1e2d9bce42ad5b05159cfd2ea37d8911d
Filippo Sarzana <filippo@sarzana.me>
2017-08-10 09:38:05 +0200

    Remove deprecated rule `jsx-a11y/href-no-hash`
    Remove this rule we can avoid having a lot of warnings during build of CRA
    
    `Line 1:  Definition for rule 'jsx-a11y/href-no-hash' was not found  jsx-a11y/href-no-hash`

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -279,7 +279,6 @@ module.exports = {
     'jsx-a11y/aria-role': 'warn',
     'jsx-a11y/aria-unsupported-elements': 'warn',
     'jsx-a11y/heading-has-content': 'warn',
-    'jsx-a11y/href-no-hash': 'warn',
     'jsx-a11y/iframe-has-title': 'warn',
     'jsx-a11y/img-redundant-alt': 'warn',
     'jsx-a11y/no-access-key': 'warn',
