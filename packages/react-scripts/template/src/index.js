d922d563e9caa1292566567bbc4cbaa92615a726
Serge R. Kamel <serge.kamel@postlight.com>
2017-08-16 13:13:57 +0300

    fix: remove duplicates, add global css to showcase non-css-modules processing.

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.global.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
