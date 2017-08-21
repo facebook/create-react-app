00811350189b9fe032be9a9fe95876a1b6493d1d
Leger, David <DLeger@cvent.com>
2017-08-21 20:08:08 -0300

    Added header tag to template
    Corrected templete to use semantically correct header tag for header instead of div

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -6,10 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Welcome to React</h1>
-        </div>
+        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
