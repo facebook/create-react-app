630cf1e14c0cee0f20e9e83fbf3cd070fa08fa26
Yuval Greenfield <ubershmekel@gmail.com>
2017-08-27 01:29:01 -0500

    Avoid build issue with unused variable

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // Consider enabling service workers for caching and offline pages by uncommenting the following line
