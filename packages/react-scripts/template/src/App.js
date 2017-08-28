2603e02c48c1bc5c12dffb59f422a72eb46efe2c
Leger, David <DLeger@cvent.com>
2017-08-28 16:45:07 -0300

    Added className for App-title
    - Added className="App-title" to h1 tag
    - Changed CSS selector to target class instead of element

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to React</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
