51236e97f4203d82b587e01bf429094ce16c7720
Tharaka Wijebandara <tharaka.nw@gmail.com>
2017-09-09 23:24:48 +0530

    Add click-to-open support for lint errors and refactor parser

--- packages/react-error-overlay/src/utils/parseCompileError.js
+++ packages/react-error-overlay/src/utils/parseCompileError.js
@@ -1,4 +1,5 @@
 // @flow
+import Anser from 'anser';
 
 export type ErrorLocation = {|
   filePath: string,
@@ -7,9 +8,16 @@ export type ErrorLocation = {|
 
 const filePathRegex = /^\.(\/[^\/\n ]+)+\.[^\/\n ]+$/;
 
-// Based on syntax error formating of babylon parser
-// https://github.com/babel/babylon/blob/v7.0.0-beta.22/src/parser/location.js#L19
-const lineNumberRegex = /^.*\((\d+):(\d+)\)$/;
+const lineNumberRegexes = [
+  // Babel syntax errors
+  // Based on syntax error formating of babylon parser
+  // https://github.com/babel/babylon/blob/v7.0.0-beta.22/src/parser/location.js#L19
+  /^.*\((\d+):(\d+)\)$/,
+
+  // ESLint errors
+  // Based on eslintFormatter in react-dev-utils
+  /^Line (\d+):.+$/,
+];
 
 // Based on error formatting of webpack
 // https://github.com/webpack/webpack/blob/v3.5.5/lib/Stats.js#L183-L217
@@ -19,18 +27,21 @@ function parseCompileError(message: string): ?ErrorLocation {
   let lineNumber: number = 0;
 
   for (let i = 0; i < lines.length; i++) {
-    const line: string = lines[i];
+    const line: string = Anser.ansiToText(lines[i]).trim();
     if (!line) continue;
 
     if (!filePath && line.match(filePathRegex)) {
       filePath = line;
     }
 
-    if (!lineNumber) {
-      const match: ?Array<string> = line.match(lineNumberRegex);
+    let k = 0;
+    while (k < lineNumberRegexes.length) {
+      const match: ?Array<string> = line.match(lineNumberRegexes[k]);
       if (match) {
         lineNumber = parseInt(match[1]);
+        break;
       }
+      k++;
     }
 
     if (filePath && lineNumber) break;
