e1c916d526c226c84b305093d6773cccdb3c2585
David Leger <davidleger95@me.com>
2017-09-12 23:32:57 -0300

    Resolved issue #2971 (#2989)
    * Resolved issue #2971
    
    - changed h2 to h1 in template to adhere to accessibility guidelines.
    - Added h1 styles to maintain same font size.
    
    * Added header tag to template
    
    Corrected templete to use semantically correct header tag for header instead of div
    
    * Added className for App-title
    
    - Added className="App-title" to h1 tag
    - Changed CSS selector to target class instead of element

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -6,10 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
