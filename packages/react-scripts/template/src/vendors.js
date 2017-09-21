3ef36177bc4d92f570a2a226913878cca40b2d6b
Ayc0 <benjamin.koltes@gmail.com>
2017-09-21 10:58:17 -0400

    Add comments in vendors.js

--- packages/react-scripts/template/src/vendors.js
+++ packages/react-scripts/template/src/vendors.js
@@ -1,1 +1,11 @@
+// This file controls which npm packages are extracted into a vendor bundle.
+// A vendor bundle is very useful for reducing your transmitted bundle size,
+// as dependencies which rarely change can be cached while you iterate on your
+// application.
+
+// If you want to add another vendor, just add in this array the name of the
+// npm package.
+
 module.exports = ['react', 'react-dom'];
+
+// You can delete this file if you'd prefer to not make a vendor bundle.
