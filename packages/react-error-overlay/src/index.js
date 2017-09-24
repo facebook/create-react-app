7796f54d8a12ee4231b4f339a17c65212b68e927
Tharaka Wijebandara <tharaka.nw@gmail.com>
2017-09-24 11:55:44 +0530

    Suppress flowtype error on importing iframe-bundle

--- packages/react-error-overlay/src/index.js
+++ packages/react-error-overlay/src/index.js
@@ -12,6 +12,7 @@ import { applyStyles } from './utils/dom/css';
 
 // Importing iframe-bundle generated in the pre build step as
 // a text using webpack raw-loader. See webpack.config.js file.
+// $FlowFixMe
 import iframeScript from 'iframeScript';
 
 import type { ErrorRecord } from './listenToRuntimeErrors';
