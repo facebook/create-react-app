480612fe2ce978dad50057c94604f2257abbcc32
Neek Sandhu <neek.sandhu@outlook.com>
2017-09-26 22:26:54 -0700

    Turn on sourcemaps

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -192,6 +192,7 @@ module.exports = {
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 1,
+                  sourceMap: true
                 },
               },
               {
