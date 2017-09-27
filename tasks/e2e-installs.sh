fedc539246a155f11db8a1b64f6928914672d9db
Joe Haddad <timer150@gmail.com>
2017-09-26 20:15:14 -0400

    Fix license comments

--- tasks/e2e-installs.sh
+++ tasks/e2e-installs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Copyright (c) 2015-present, Facebook, Inc.
-
-This source code is licensed under the MIT license found in the
-LICENSE file in the root directory of this source tree.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 # ******************************************************************************
 # This is an end-to-end test intended to run on CI.
