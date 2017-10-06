a0030fcf2df5387577ced165198f1f0264022fbd
Jonny Buchanan <jonathan.buchanan@gmail.com>
2017-10-06 10:02:32 +1000

    Update style-loader and disable inclusion of its HMR code in builds (#3236)

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -197,7 +197,12 @@ module.exports = {
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
-                  fallback: require.resolve('style-loader'),
+                  fallback: {
+                    loader: require.resolve('style-loader'),
+                    options: {
+                      hmr: false,
+                    },
+                  },
                   use: [
                     {
                       loader: require.resolve('css-loader'),
