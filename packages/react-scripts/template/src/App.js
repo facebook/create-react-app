44044c1fa40bf7d2ea0f6b7d12e380a8b41fdfd5
Pablo Ferro <pablo.pyp@gmail.com>
2017-10-09 11:46:01 -0300

    Update template to use modules

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={style.app}>
+        <header className={style.appHeader}>
+          <img src={logo} className={style.appLogo} alt="logo" />
+          <h1 className={style.appTitle}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={style.appIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
