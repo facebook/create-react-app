85109c39d9f04ef2062882781986db80f791ff90
Joe Haddad <timer150@gmail.com>
2017-10-11 13:30:52 -0400

    Update index.js

--- packages/react-error-overlay/src/index.js
+++ packages/react-error-overlay/src/index.js
@@ -168,23 +168,9 @@ window.__REACT_ERROR_OVERLAY_GLOBAL_HOOK__.iframeReady = function iframeReady() 
   updateIframeContent();
 };
 
-const warningText =
-  'When deploying an application, `react-error-overlay` should be excluded ' +
-  'as it is a heavy dependency meant for development.\n\n' +
-  'Consider adding an error boundary to your tree to customize error ' +
-  'handling behavior. See https://fb.me/react-error-boundaries for more ' +
-  'information.';
-
-if (process.env.NODE_ENV !== 'production') {
-  var testFunc = function testFn() {};
-  if ((testFunc.name || testFunc.toString()).indexOf('testFn') === -1) {
-    console.warn(
-      'It looks like you are using `react-error-overlay` in production. ' +
-        warningText
-    );
-  }
-} else {
+if (process.env.NODE_ENV === 'production') {
   console.warn(
-    'You are using `react-error-overlay` in production. ' + warningText
+    'react-error-overlay is not meant for use in production. You should ' +
+      'ensure it is not included in your build to reduce bundle size.'
   );
 }
