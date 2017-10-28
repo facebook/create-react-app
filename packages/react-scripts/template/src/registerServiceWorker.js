15d8e53daf3d7fee077efe3bb83a19a55fe363a5
Kévin Dunglas <dunglas@gmail.com>
2017-10-28 23:01:24 +0200

    Remove an useless negation in registerServiceWorker.js (#3150)

--- packages/react-scripts/template/src/registerServiceWorker.js
+++ packages/react-scripts/template/src/registerServiceWorker.js
@@ -32,12 +32,12 @@ export default function register() {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
-      if (!isLocalhost) {
-        // Is not local host. Just register service worker
-        registerValidSW(swUrl);
-      } else {
+      if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
         checkValidServiceWorker(swUrl);
+      } else {
+        // Is not local host. Just register service worker
+        registerValidSW(swUrl);
       }
     });
   }
