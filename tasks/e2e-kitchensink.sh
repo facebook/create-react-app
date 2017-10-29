3a2130eac70836b66fbc5b28b045824df818aad2
Joe Haddad <timer150@gmail.com>
2017-10-29 13:54:57 -0400

    re-enable cleanup

--- tasks/e2e-kitchensink.sh
+++ tasks/e2e-kitchensink.sh
@@ -23,7 +23,7 @@ function cleanup {
   ps -ef | grep 'react-scripts' | grep -v grep | awk '{print $2}' | xargs kill -9
   cd "$root_path"
   # TODO: fix "Device or resource busy" and remove ``|| $CI`
-  # rm -rf "$temp_cli_path" "$temp_app_path" "$temp_module_path" || $CI
+  rm -rf "$temp_cli_path" "$temp_app_path" "$temp_module_path" || $CI
 }
 
 # Error messages are redirected to stderr
