33dc56027a80a434585bea7e8a19ced6d706a2f1
Thomas Marek <ttmarek@outlook.com>
2017-10-29 12:03:28 -0400

    Remove comment that could be confusing

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -10,8 +10,6 @@
 // Inspired by https://github.com/airbnb/javascript but less opinionated.
 
 // We use eslint-loader so even warnings are very visible.
-// This is why we only use "WARNING" level for potential errors,
-// and we don't use "ERROR" level at all.
 
 // In the future, we might create a separate list of rules for production.
 // It would probably be more strict.
