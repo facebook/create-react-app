6724a964f7d0d304cde77d785a76f778a90ef2cf
mcallegari10 <martin.callegari@wolox.com.ar>
2017-10-31 16:59:30 -0300

    change regex for css loaders to avoid using modules in generics

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -184,7 +184,7 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.scss$/,
+            test: /\/src\/app\/(components|screens)\/\*\*\/\*.scss$/,
             use: [
               require.resolve('style-loader'),
               {
@@ -192,6 +192,46 @@ module.exports = {
                 options: {
                   importLoaders: 1,
                   modules: true,
+                  camelCase: true,
+                  localIdentName: '[name]__[local]___[hash:base64:5]',
+                },
+              },
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  includePaths: [path.resolve(__dirname, '../src/app/scss')],
+                },
+              },
+            ],
+          },
+          {
+            test: /\/src\/app\/scss\/\*\*\/\*.scss$/,
+            use: [
+              require.resolve('style-loader'),
+              {
+                loader: require.resolve('css-loader'),
+                options: {
+                  importLoaders: 1,
                   camelCase: true,
                   localIdentName: '[name]__[local]___[hash:base64:5]',
                 },
