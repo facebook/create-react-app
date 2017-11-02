c39eebc6658bbf6c68fd46c3fa348cb75fe7bf02
mcallegari10 <martin.callegari@wolox.com.ar>
2017-11-02 12:03:37 -0300

    fix template to match wolox standars

--- packages/react-scripts/template/src/app/components/App/index.js
+++ packages/react-scripts/template/src/app/components/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import style from './App.css';
+
+import logo from '../../../logo.svg';
+import style from './styles.scss';
 
 class App extends Component {
   render() {
@@ -11,7 +12,8 @@ class App extends Component {
           <h1 className={style.appTitle}>Welcome to React</h1>
         </header>
         <p className={style.appIntro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/app/components/App/index.js</code> and
+          save to reload.
         </p>
       </div>
     );
