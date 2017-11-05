1332a190edac8adc94733bda2f42bc4a12382f6f
Joe Haddad <timer150@gmail.com>
2017-11-05 14:06:44 -0500

    try yarn.cmd explicitly

--- packages/react-scripts/scripts/eject.js
+++ packages/react-scripts/scripts/eject.js
@@ -48,7 +48,7 @@ function adjustPackages(cwd, packages, append, dev) {
   let status, output;
   if (fs.existsSync(paths.yarnLockFile)) {
     ({ status, output } = spawnSync(
-      process.platform === 'win32' ? 'yarnpkg.cmd' : 'yarnpkg',
+      process.platform === 'win32' ? 'yarn.cmd' : 'yarnpkg',
       [append ? 'add' : 'remove', ...packages],
       {
         stdio: 'pipe',
