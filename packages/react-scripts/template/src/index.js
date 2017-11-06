03c92e8f85e778153920086e990c4356b4b53fee
mcallegari10 <martin.callegari@wolox.com.ar>
2017-11-06 11:43:13 -0300

    fix app component import in template

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from 'components/App';
 
-import App from './app/components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './scss/index.scss';
 
