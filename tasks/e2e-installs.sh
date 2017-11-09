b6ff52c0ba5d85e87ff14c7a383869a8519c77b2
Trevor Brindle <tabrindle@gmail.com>
2017-11-09 14:03:40 -0500

    add —use-npm flag to bypass yarn (#3409)
    * add —use-npm flag to bypass yarn
    
    * add e2e test for —use-npm flag

--- tasks/e2e-installs.sh
+++ tasks/e2e-installs.sh
@@ -146,6 +146,20 @@ grep '"version": "0.4.0"' node_modules/react-scripts/package.json
 checkDependencies
 
 # ******************************************************************************
+# Test --use-npm flag
+# ******************************************************************************
+
+cd "$temp_app_path"
+create_react_app --use-npm --scripts-version=0.4.0 test-use-npm-flag
+cd test-use-npm-flag
+
+# Check corresponding scripts version is installed.
+exists node_modules/react-scripts
+[ ! -e "yarn.lock" ] && echo "yarn.lock correctly does not exist"
+grep '"version": "0.4.0"' node_modules/react-scripts/package.json
+checkDependencies
+
+# ******************************************************************************
 # Test --scripts-version with a tarball url
 # ******************************************************************************
 
