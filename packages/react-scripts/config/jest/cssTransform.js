fd47ba0bfd3dff2372caa29149759e1a57ea21be
vannio <vannio@users.noreply.github.com>
2017-11-15 11:41:07 +0000

    Fix dead link (#3453)
    Fix another dead link

--- packages/react-scripts/config/jest/cssTransform.js
+++ packages/react-scripts/config/jest/cssTransform.js
@@ -9,7 +9,7 @@
 'use strict';
 
 // This is a custom Jest transformer turning style imports into empty objects.
-// http://facebook.github.io/jest/docs/tutorial-webpack.html
+// http://facebook.github.io/jest/docs/en/webpack.html
 
 module.exports = {
   process() {
