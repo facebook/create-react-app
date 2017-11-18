b3fe68bbd5229c9436f553dcf619c65beff4ebc6
Andy Kenward <hello@andykenward.com>
2017-11-18 22:09:54 +0000

    revert react-dev-utils changes that arent in current release

--- packages/react-scripts/config/webpackDevServer.config.js
+++ packages/react-scripts/config/webpackDevServer.config.js
@@ -10,7 +10,7 @@
 
 const errorOverlayMiddleware = require('react-dev-utils/errorOverlayMiddleware');
 const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
-const ignoredFiles = require('react-dev-utils/ignoredFiles');
+const path = require('path');
 const config = require('./webpack.config.dev');
 const paths = require('./paths');
 
@@ -76,7 +76,12 @@ module.exports = function(proxy, allowedHost) {
     // src/node_modules is not ignored to support absolute imports
     // https://github.com/facebookincubator/create-react-app/issues/1065
     watchOptions: {
-      ignored: ignoredFiles(paths.appSrc),
+      ignored: new RegExp(
+        `^(?!${path
+          .normalize(paths.appSrc + '/')
+          .replace(/[\\]+/g, '\\\\')}).+[\\\\/]node_modules[\\\\/]`,
+        'g'
+      ),
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === 'https',
