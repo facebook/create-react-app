da3e1002da2ce37f9e89e180e9f3a37f36bc2d61
Andy Kenward <hello@andykenward.com>
2017-11-19 16:37:53 +0000

    revert react-scripts eslint config

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -57,7 +57,7 @@ module.exports = function(
 
   // Add ESlint config
   appPackage.eslintConfig = {
-    extends: '@stinkstudios/eslint-config-react-app',
+    extends: 'eslint-config-react-app',
   };
 
   // Add EslintIgnore
