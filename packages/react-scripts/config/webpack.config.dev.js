fdbaa71f78c5d4f0fe1a8561e05ff2f196e119ea
Adam Mockor <adam.mockor@gmail.com>
2017-11-22 12:53:49 +0100

    exclude css from file-loader

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -235,7 +235,14 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.js$/, /\.html$/, /\.json$/, /\.scss$/, /\.svg$/],
+            exclude: [
+              /\.js$/,
+              /\.html$/,
+              /\.json$/,
+              /\.css/,
+              /\.scss$/,
+              /\.svg$/,
+            ],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
