7cbeb833fcf1e69f61724761b4bbe6923104474d
Clement Hoang <clemmy@fb.com>
2017-11-29 15:23:35 -0800

    Add first pass of working dependencies for babel-preset-react-app and react-scripts

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <>
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </>
         </p>
       </div>
     );
