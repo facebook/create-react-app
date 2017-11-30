ae2b569b77e29d2ef6c54f49b2f25729a368de1f
Clement Hoang <clemmy@fb.com>
2017-11-30 14:51:01 -0800

    Restore functionality of old preset

--- packages/babel-preset-react-app/index.js
+++ packages/babel-preset-react-app/index.js
@@ -1,58 +1,49 @@
-"use strict";
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+'use strict';
 
 module.exports = function(api, opts) {
   if (!opts) {
     opts = {};
   }
 
+  // This is similar to how `env` works in Babel:
+  // https://babeljs.io/docs/usage/babelrc/#env-option
+  // We are not using `env` because it’s ignored in versions > babel-core@6.10.4:
+  // https://github.com/babel/babel/issues/4539
+  // https://github.com/facebookincubator/create-react-app/issues/720
+  // It’s also nice that we can enforce `NODE_ENV` being specified.
   var env = process.env.BABEL_ENV || process.env.NODE_ENV;
-
-  // if (env !== "development" && env !== "test" && env !== "production") {
-  //   throw new Error(
-  //     "Using `babel-preset-react-app` requires that you specify `NODE_ENV` or " +
-  //       '`BABEL_ENV` environment variables. Valid values are "development", ' +
-  //       '"test", and "production". Instead, received: ' +
-  //       JSON.stringify(env) +
-  //       "."
-  //   );
-  // }
-  console.log("babel-preset-react-app says hi");
-
-//   return {
-//     presets: [
-//       [
-//         require.resolve('babel-preset-env'),
-//         {
-//           targets: {
-//             node: 'current',
-//           },
-//         },
-//       ],
-//       [require.resolve("@babel/preset-react"), { development: true }]
-//     ],
-//     plugins: [
-//       require.resolve("@babel/plugin-proposal-class-properties"),
-//       [
-//         require.resolve("@babel/plugin-transform-react-jsx"),
-//         {
-//           useBuiltIns: true
-//         }
-//       ],
-//       [
-//         require.resolve("@babel/plugin-transform-runtime"),
-//         {
-//           helpers: false,
-//           polyfill: false,
-//           regenerator: true
-//         }
-//       ]
-//     ].filter(Boolean)
-//   };
-// };
+  var isEnvDevelopment = env === 'development';
+  var isEnvProduction = env === 'production';
+  var isEnvTest = env === 'test';
+  if (!isEnvDevelopment && !isEnvProduction && !isEnvTest) {
+    throw new Error(
+      'Using `babel-preset-react-app` requires that you specify `NODE_ENV` or ' +
+        '`BABEL_ENV` environment variables. Valid values are "development", ' +
+        '"test", and "production". Instead, received: ' +
+        JSON.stringify(env) +
+        '.'
+    );
+  }
 
   return {
     presets: [
-      [
+      isEnvTest && [
+        // ES features necessary for user's Node version
+        require('@babel/preset-env').default,
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+      (isEnvProduction || isEnvDevelopment) && [
+        // Latest stable ECMAScript features
         require('@babel/preset-env').default,
         {
           targets: {
@@ -62,26 +53,46 @@ module.exports = function(api, opts) {
             // Remove after https://github.com/mishoo/UglifyJS2/issues/448
             forceAllTransforms: true,
           },
+          // Disable polyfill transforms
+          useBuiltIns: false,
+          // Do not transform modules to CJS
+          modules: false,
         },
       ],
-      [require("@babel/preset-react").default, { development: true }],
-      [require("@babel/preset-flow").default]
-    ],
+      [
+        require('@babel/preset-react').default,
+        { development: isEnvDevelopment || isEnvTest },
+      ],
+      [require('@babel/preset-flow').default],
+    ].filter(Boolean),
     plugins: [
+      // class { handleClick = () => { } }
+      require('@babel/plugin-proposal-class-properties').default,
+      // The following two plugins use Object.assign directly, instead of Babel's
+      // extends helper. Note that this assumes `Object.assign` is available.
+      // { ...todo, completed: true }
       [
-        require("@babel/plugin-proposal-object-rest-spread").default
+        require('@babel/plugin-proposal-object-rest-spread').default,
+        {
+          useBuiltIns: true,
+        },
       ],
+      // Transforms JSX
       [
-        require("@babel/plugin-proposal-class-properties").default
+        require('@babel/plugin-transform-react-jsx').default,
+        {
+          useBuiltIns: true,
+        },
       ],
+      // Polyfills the runtime needed for async/await and generators
       [
-        require("@babel/plugin-transform-runtime").default,
+        require('@babel/plugin-transform-runtime').default,
         {
           helpers: false,
           polyfill: false,
-          regenerator: true
-        }
-      ]
-    ]
+          regenerator: true,
+        },
+      ],
+    ].filter(Boolean),
   };
 };
