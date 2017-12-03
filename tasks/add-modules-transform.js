67ca211923c649421fb24e3f896ac37adb1ab336
Joe Haddad <timer150@gmail.com>
2017-12-03 17:35:38 -0500

    Attempt fix for ejected tests

---
+++ tasks/add-modules-transform.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+'use strict';
+
+const fs = require('fs');
+
+const file = fs.readFileSync('./package.json');
+const data = JSON.parse(file);
+
+if (data.babel == null) {
+  data.babel = { plugins: ['@babel/plugin-transform-modules-commonjs'] };
+} else if (data.babel.plugins == null) {
+  data.babel.plugins = ['@babel/plugin-transform-modules-commonjs'];
+} else {
+  data.babel.plugins.push('@babel/plugin-transform-modules-commonjs');
+}
+
+fs.writeFile('./package.json', JSON.stringify(data, null, 2), 'utf8', err => {
+  if (err) {
+    throw err;
+  }
+  console.log('Added module transform');
+});
