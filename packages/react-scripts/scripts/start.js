a5f0f9b08227aa03635fc4354f64b24efea6c933
Diana Suvorova <diana.suvorova@uber.com>
2017-12-02 17:50:25 -0800

    keep node_env if provided

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -10,7 +10,7 @@
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = process.env.NODE_ENV ||'development';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
