411ddf780bc8ddb48984e8917e914f9778f96650
Harald Rudell <harald@attiresoft.com>
2017-12-04 17:21:26 -0800

    App component using CSS Modules local class names

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={styles.root}>
+        <header className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <h1 className={styles.title}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={styles.intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
