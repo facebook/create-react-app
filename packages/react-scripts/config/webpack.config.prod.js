af291f3cb64ce472002ff98688bccc2d22d2efb7
Ian Sutherland <ian@iansutherland.ca>
2017-12-07 10:20:45 -0800

    Add mjs and jsx filename extensions to file-loader exclude pattern (#3537)

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -249,7 +249,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.js$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
