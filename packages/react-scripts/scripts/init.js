b37e721f7cdda489afcc81a43373fa03d0202bc2
Demian Dekoninck <demian@live.be>
2017-12-15 20:17:57 +0100

    create tailwind config on init

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -56,6 +56,20 @@ module.exports = function(
     );
   }
 
+  // Create a Tailwind config file
+  const procTailwind = spawn.sync(
+    `${appPath}/node_modules/.bin/tailwind`,
+    ["init", path.join(appPath, "tailwind.js")],
+    { stdio: "inherit" }
+  );
+
+  if (procTailwind.status !== 0) {
+    console.error(`Failed creating Tailwind config file`);
+    return;
+  }
+
+  console.log("Created a Tailwind config file");
+
   // Copy the files for the user
   const templatePath = template
     ? path.resolve(originalDirectory, template)
@@ -180,10 +194,6 @@ module.exports = function(
       )
     );
   }
-
-  // Create a Tailwind config file
-  spawn.sync('./node_modules/.bin/tailwind', 'init', { stdio: 'inherit' });
-  console.log('Created a Tailwind config file');
 
   console.log();
   console.log('Happy hacking!');
