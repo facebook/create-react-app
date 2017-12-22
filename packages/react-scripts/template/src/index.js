66ab8547ae618ce5a136b92ccfa19fcddfd77fa5
mcallegari10 <martin.callegari@wolox.com.ar>
2017-12-22 15:21:24 -0300

    add correct imports order

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'components/App';
 
-import registerServiceWorker from './registerServiceWorker';
 import './scss/index.scss';
+import registerServiceWorker from './registerServiceWorker';
+
+import App from '~components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
