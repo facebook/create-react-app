dc44cc8a54d3e390c2f9f18d1bde5b1bc9d7e3ef
Guilherme Heynemann Bruzzi <guibruzzi@gmail.com>
2017-12-27 14:30:33 -0200

    Allow other envs on e2e tests

--- packages/react-scripts/scripts/e2e.js
+++ packages/react-scripts/scripts/e2e.js
@@ -9,8 +9,8 @@
 'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
+process.env.BABEL_ENV = process.env.BABEL_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
