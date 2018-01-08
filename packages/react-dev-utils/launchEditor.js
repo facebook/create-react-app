1b067c5659abc7ef478f074761570758bd091f4d
Ernesto García <gnapse@gmail.com>
2018-01-08 18:08:29 -0300

    Autodetect MacVim editor (#3545)
    Adds support for the error overlay to detect and open the file with the error in MacVim.
    
    Relates to #2636.

--- packages/react-dev-utils/launchEditor.js
+++ packages/react-dev-utils/launchEditor.js
@@ -52,6 +52,8 @@ const COMMON_EDITORS_OSX = {
     '/Applications/RubyMine.app/Contents/MacOS/rubymine',
   '/Applications/WebStorm.app/Contents/MacOS/webstorm':
     '/Applications/WebStorm.app/Contents/MacOS/webstorm',
+  '/Applications/MacVim.app/Contents/MacOS/MacVim':
+    'mvim',
 };
 
 const COMMON_EDITORS_LINUX = {
