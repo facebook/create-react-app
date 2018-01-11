9ca199111d76edd2f4891ffaa6996742c9847ba8
aisensiy <aisensiy@163.com>
2018-01-11 15:49:30 +0800

    Update jest to 22 and support watchPathIgnorePatterns configuration (#3124)
    * update jest to 21.0.2 to support watchPathIgnorePatterns configuration
    
    * update jest to 21.1.0
    
    * Try bumping Jest
    
    * Bump babel-jest
    
    * Try to debug weird CI failure
    
    * Remove debug code
    
    * Bump other Jest packages
    
    * ffs
    
    * temp
    
    * Revert "temp"
    
    This reverts commit 62aec9ac1ae70a995a89548feb7ac7870e5324c0.

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -62,6 +62,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
     'coverageReporters',
     'coverageThreshold',
     'snapshotSerializers',
+    'watchPathIgnorePatterns',
   ];
   if (overrides) {
     supportedKeys.forEach(key => {
