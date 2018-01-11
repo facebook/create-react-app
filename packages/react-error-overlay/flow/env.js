0a6d95e17737697391b265000193d58c33d9df67
Dan Abramov <dan.abramov@gmail.com>
2018-01-11 20:31:18 +0000

    Fix Flow without perf penalty

---
+++ packages/react-error-overlay/flow/env.js
@@ -0,0 +1,19 @@
+declare module 'anser' {
+  declare var exports: any;
+}
+
+declare module 'babel-code-frame' {
+  declare var exports: any;
+}
+
+declare module 'html-entities' {
+  declare var exports: any;
+}
+
+declare module 'settle-promise' {
+  declare var exports: any;
+}
+
+declare module 'source-map' {
+  declare var exports: any;
+}
