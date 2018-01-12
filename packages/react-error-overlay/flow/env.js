f5a17bb0f8400568e3480285a842daad37e2d00d
Dan <dan.abramov@gmail.com>
2018-01-12 00:18:30 +0000

    lol flow COME ON

--- packages/react-error-overlay/flow/env.js
+++ packages/react-error-overlay/flow/env.js
@@ -1,19 +1,19 @@
 declare module 'anser' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'babel-code-frame' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'html-entities' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'settle-promise' {
-  declare var exports: any;
+  declare module.exports: any;
 }
 
 declare module 'source-map' {
-  declare var exports: any;
+  declare module.exports: any;
 }
