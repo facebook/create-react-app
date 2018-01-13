11db9af4df4caa7d0664e732b807115c3b05b7a3
Dan Abramov <dan.abramov@gmail.com>
2018-01-13 15:55:48 +0000

    Add preflight check to guard against wrong versions of webpack/eslint/jest higher up the tree (#3771)
    * Run real scripts in local development
    
    * Add preflight check warning
    
    * I know what I am doing
    
    * Move preflight check into individual scripts
    
    This ensures we don't try to filter NODE_PATH twice, accidentally removing the now-absolute path.
    
    * Slightly tweak the wording
    
    * Fix lint

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -8,6 +8,13 @@
 
 'use strict';
 
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
 const spawn = require('react-dev-utils/crossSpawn');
 const args = process.argv.slice(2);
 
