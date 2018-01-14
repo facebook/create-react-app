b86fe056a36c2da948e9c3ad363217712b4a37bc
Ian Sutherland <ian@iansutherland.ca>
2018-01-14 12:14:37 -0700

    Add warning when HOST environment variable is set (#3730)
    * Add warning when HOST environment variable is set (#3719)
    
    * Improve HOST environment variable warning message
    
    * Adjust text and message
    
    Closes #3719

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -51,6 +51,21 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
+if (process.env.HOST) {
+  console.log(
+    chalk.cyan(
+      `Attempting to bind to HOST environment variable: ${chalk.yellow(
+        chalk.bold(process.env.HOST)
+      )}`
+    )
+  );
+  console.log(
+    `If this was unintentional, check that you haven't mistakenly set it in your shell.`
+  );
+  console.log(`Learn more here: ${chalk.yellow('http://bit.ly/2mwWSwH')}`);
+  console.log();
+}
+
 // We attempt to use the default port but if it is busy, we offer the user to
 // run on a different port. `choosePort()` Promise resolves to the next free port.
 choosePort(HOST, DEFAULT_PORT)
