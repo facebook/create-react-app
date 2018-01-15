09068d527337b0faaf8184de5fa761f6ccf7c585
Dan Abramov <dan.abramov@gmail.com>
2018-01-15 17:45:41 +0000

    Use clearer condition

--- packages/react-scripts/scripts/test.js
+++ packages/react-scripts/scripts/test.js
@@ -36,8 +36,8 @@ const argv = process.argv.slice(2);
 // Watch unless on CI in coverage mode, or explicitly running all tests
 if (
   !process.env.CI &&
-  argv.indexOf('--coverage') < 0 &&
-  argv.indexOf('--watchAll') < 0
+  argv.indexOf('--coverage') === -1 &&
+  argv.indexOf('--watchAll') === -1
 ) {
   argv.push('--watch');
 }
