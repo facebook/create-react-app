acdd3487eda889e984bcf6d8909bee1cb9bb90de
Ian Sutherland <ian@iansutherland.ca>
2018-01-16 10:57:00 -0700

    WIP disable service worker by default (#2554)

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker, {
+  unregister as unregisterServiceWorker,
+} from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// By default we make sure that no service worker is registered. If you would
+// like to enable service worker uncomment the call to registerServiceWorker()
+// and comment out the call to unregisterServiceWorker().
+// See http://bit.ly/2vJdu84 for more information.
+
+// registerServiceWorker();
+unregisterServiceWorker();
