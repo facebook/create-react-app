5e15920cafc0d5d30d205f05c00438374442c949
Ian Sutherland <ian@iansutherland.ca>
2018-01-16 12:32:09 -0700

    Updated service worker registration

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker, {
-  unregister as unregisterServiceWorker,
-} from './registerServiceWorker';
+import * as serviceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // By default we make sure that no service worker is registered. If you would
-// like to enable service worker uncomment the call to registerServiceWorker()
-// and comment out the call to unregisterServiceWorker().
+// like to enable service worker uncomment the call to serviceWorker.register()
+// and comment out the call to serviceWorker.unregister().
 // See http://bit.ly/2vJdu84 for more information.
 
-// registerServiceWorker();
-unregisterServiceWorker();
+// serviceWorker.register();
+serviceWorker.unregister();
--- packages/react-scripts/template/src/serviceWorker.js
+++ packages/react-scripts/template/src/serviceWorker.js
@@ -18,7 +18,7 @@ const isLocalhost = Boolean(
     )
 );
 
-export default function register() {
+export function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
