d400d198dd2e1518d84bd42a4b0586f8de0d36e6
Dan Abramov <dan.abramov@gmail.com>
2018-01-17 19:40:08 +0000

    Add a comment

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -235,6 +235,9 @@ module.exports = {
               },
             ],
           },
+          // Allows you to use two kinds of imports for SVG:
+          // import logoUrl from './logo.svg'; gives you the URL.
+          // import { ReactComponent as Logo } from './logo.svg'; gives you a component.
           {
             test: /\.svg$/,
             use: [
