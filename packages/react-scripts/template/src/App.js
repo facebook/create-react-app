bc0554bfe295ab9142601c2c9144c17cd9fe40af
Dan Abramov <dan.abramov@gmail.com>
2018-01-17 19:30:51 +0000

    Tweaks

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p className="App-intro">
+          <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
