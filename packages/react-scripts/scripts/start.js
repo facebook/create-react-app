9d67605ab631e504bf13e5e963b1a38e36be0346
Bond <bondz@users.noreply.github.com>
2018-01-18 15:17:02 +0100

    Use custom bit.ly links (#3836)
    * change link to advanced deployment
    
    * Use custom CRA link for deployment
    
    * use custom link for minification failure
    
    * update link for deployment
    
    * feedback

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -69,7 +69,7 @@ if (process.env.HOST) {
   console.log(
     `If this was unintentional, check that you haven't mistakenly set it in your shell.`
   );
-  console.log(`Learn more here: ${chalk.yellow('http://bit.ly/2mwWSwH')}`);
+  console.log(`Learn more here: ${chalk.yellow('http://bit.ly/CRA-advanced-config')}`);
   console.log();
 }
 
