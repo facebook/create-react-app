3ad86ae280640face28f3504022872ffc5e81f2e
Jeppe Reinhold <jeppereinhold@gmail.com>
2018-01-18 20:47:28 +0100

    allow cacheDirectory in createJestConfig.js

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -69,6 +69,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
     'resetModules',
     'snapshotSerializers',
     'watchPathIgnorePatterns',
+    'cacheDirectory',
   ];
   if (overrides) {
     supportedKeys.forEach(key => {
