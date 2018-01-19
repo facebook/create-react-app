184cec480cbcf4764f7915faf91985883d8ab7d0
Dan Abramov <dan.abramov@gmail.com>
2018-01-19 00:13:04 +0000

    Call it serviceWorker

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
