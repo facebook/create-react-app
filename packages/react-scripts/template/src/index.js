09e99ec57f573a4d4f9b2fb5114e0d985e68d53b
Bond <bondz@users.noreply.github.com>
2018-01-20 13:35:21 +0100

    Update short link for PWA

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -8,5 +8,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/2vJdu84
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
