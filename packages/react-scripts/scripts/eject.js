79ac8151b3816d039c376f55d970affd90bc1214
Bradford Lemley <bradfordlemley@gmail.com>
2018-01-21 01:45:30 -0700

    Don't write babel or lint config to package.json when ejecting.

--- packages/react-scripts/scripts/eject.js
+++ packages/react-scripts/scripts/eject.js
@@ -205,18 +205,6 @@ inquirer
     console.log(`  Adding ${cyan('Jest')} configuration`);
     appPackage.jest = jestConfig;
 
-    // Add Babel config
-    console.log(`  Adding ${cyan('Babel')} preset`);
-    appPackage.babel = {
-      presets: ['react-app'],
-    };
-
-    // Add ESlint config
-    console.log(`  Adding ${cyan('ESLint')} configuration`);
-    appPackage.eslintConfig = {
-      extends: 'react-app',
-    };
-
     fs.writeFileSync(
       path.join(appPath, 'package.json'),
       JSON.stringify(appPackage, null, 2) + os.EOL
