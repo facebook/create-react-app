d49ffde4e627eea1f935ebc4f8fb2ea61010577d
Dan Abramov <dan.abramov@gmail.com>
2018-01-21 23:31:03 +0000

    Fix the E2E script (#3888)
    * Fix the E2E script
    
    * Delete .git if committing failed

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -22,7 +22,7 @@ const spawn = require('react-dev-utils/crossSpawn');
 const { defaultBrowsers } = require('react-dev-utils/browsersHelper');
 const os = require('os');
 
-function insideGitRepository() {
+function isInGitRepository() {
   try {
     execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' });
     return true;
@@ -31,7 +31,7 @@ function insideGitRepository() {
   }
 }
 
-function insideMercurialRepository() {
+function isInMercurialRepository() {
   try {
     execSync('hg --cwd . root', { stdio: 'ignore' });
     return true;
@@ -40,22 +40,36 @@ function insideMercurialRepository() {
   }
 }
 
-function tryGitInit() {
+function tryGitInit(appPath) {
+  let didInit = false;
   try {
     execSync('git --version', { stdio: 'ignore' });
-
-    if (insideGitRepository() || insideMercurialRepository()) {
+    if (isInGitRepository() || isInMercurialRepository()) {
       return false;
     }
 
     execSync('git init', { stdio: 'ignore' });
+    didInit = true;
+
     execSync('git add -A', { stdio: 'ignore' });
     execSync('git commit -m "Initial commit from Create React App"', {
       stdio: 'ignore',
     });
-
     return true;
   } catch (e) {
+    if (didInit) {
+      // If we successfully initialized but couldn't commit,
+      // maybe the commit author config is not set.
+      // In the future, we might supply our own committer
+      // like Ember CLI does, but for now, let's just
+      // remove the Git files to avoid a half-done state.
+      try {
+        // unlinkSync() doesn't work on directories.
+        fs.removeSync(path.join(appPath, '.git'));
+      } catch (removeErr) {
+        // Ignore.
+      }
+    }
     return false;
   }
 }
@@ -172,7 +186,7 @@ module.exports = function(
     }
   }
 
-  if (tryGitInit()) {
+  if (tryGitInit(appPath)) {
     console.log();
     console.log('Initialized a git repository.');
   }
