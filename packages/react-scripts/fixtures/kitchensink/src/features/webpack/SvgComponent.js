3a1e13f332b9b4616f09f86ceca26c8c94b67f86
Ian Sutherland <ian@iansutherland.ca>
2018-01-23 12:45:10 -0700

    Fix failing e2e test

--- packages/react-scripts/fixtures/kitchensink/src/features/webpack/SvgComponent.js
+++ packages/react-scripts/fixtures/kitchensink/src/features/webpack/SvgComponent.js
@@ -6,6 +6,6 @@
  */
 
 import React from 'react';
-import { ReactComponent as Logo } from './assets/logo.svg';
+import { default as Logo } from './assets/logo.svg';
 
 export default () => <Logo />;
