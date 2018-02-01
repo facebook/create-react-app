1e2d8a5bce40aee9201d16fb4971651fde3da9f0
Ian Sutherland <ian@iansutherland.ca>
2018-02-01 13:46:52 -0700

    Added tests for including SVG in CSS

--- packages/react-scripts/fixtures/kitchensink/src/App.js
+++ packages/react-scripts/fixtures/kitchensink/src/App.js
@@ -82,9 +82,9 @@ class App extends Component {
         );
         break;
       case 'css-modules-inclusion':
-        import(
-          './features/webpack/CssModulesInclusion'
-        ).then(f => this.setFeature(f.default));
+        import('./features/webpack/CssModulesInclusion').then(f =>
+          this.setFeature(f.default)
+        );
         break;
       case 'custom-interpolation':
         import('./features/syntax/CustomInterpolation').then(f =>
@@ -171,6 +171,16 @@ class App extends Component {
         break;
       case 'svg-inclusion':
         import('./features/webpack/SvgInclusion').then(f =>
+          this.setFeature(f.default)
+        );
+        break;
+      case 'svg-component':
+        import('./features/webpack/SvgComponent').then(f =>
+          this.setFeature(f.default)
+        );
+        break;
+      case 'svg-in-css':
+        import('./features/webpack/SvgInCss').then(f =>
           this.setFeature(f.default)
         );
         break;
