39d39bcc57650c3db27394785e0c45e2c8be877d
petetnt <pete.a.nykanen@gmail.com>
2018-02-03 17:08:12 +0200

    Update copyright header
    Signed-off-by: petetnt <pete.a.nykanen@gmail.com>

--- packages/react-scripts/config/jest/graphqlTransform.js
+++ packages/react-scripts/config/jest/graphqlTransform.js
@@ -1,6 +1,7 @@
 // @remove-on-eject-begin
 /**
  * Copyright (c) 2018-present, Facebook, Inc.
+ * Copyright (c) 2016 Remind
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
