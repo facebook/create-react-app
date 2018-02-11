7a96d899e510476028c0feaa96e91c73b362b4e0
Alexander Roberts <detrohutt@gmail.com>
2018-02-11 03:42:06 -0600

    Allow extensions besides .js for entry file

--- packages/react-dev-utils/checkRequiredFiles.js
+++ packages/react-dev-utils/checkRequiredFiles.js
@@ -7,24 +7,51 @@
 
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-var chalk = require('chalk');
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+// Currently, files passed without an extension are assumed to be js-like.
+// If other types of files need to be passed later without extensions the
+// checkRequiredFiles function will need to be modified.
+const fallbackExts = [
+  '.web.js',
+  '.mjs',
+  '.js',
+  '.web.jsx',
+  '.jsx',
+  '.ts',
+  '.tsx',
+];
+const withExt = (ext, file) => file.dir + path.sep + file.name + ext;
 
 function checkRequiredFiles(files) {
-  var currentFilePath;
+  let didFallbackExts = false;
   try {
-    files.forEach(filePath => {
-      currentFilePath = filePath;
-      fs.accessSync(filePath, fs.F_OK);
-    });
+    for (const filePath of files) {
+      const file = path.parse(filePath);
+
+      if (file.ext === '') {
+        didFallbackExts = true;
+        if (fallbackExts.some(e => fs.existsSync(withExt(e, file)))) {
+          continue;
+        } else {
+          throw file;
+        }
+      }
+
+      if (!fs.existsSync(filePath)) throw file;
+    }
     return true;
-  } catch (err) {
-    var dirName = path.dirname(currentFilePath);
-    var fileName = path.basename(currentFilePath);
+  } catch (file) {
     console.log(chalk.red('Could not find a required file.'));
-    console.log(chalk.red('  Name: ') + chalk.cyan(fileName));
-    console.log(chalk.red('  Searched in: ') + chalk.cyan(dirName));
+    console.log(chalk.red('  Name: ') + chalk.cyan(file.base));
+    if (didFallbackExts) {
+      // Typescript files will pass the check but are not officially supported.
+      const extStr = fallbackExts.filter(e => /\.(?!tsx?)/.test(e)).join(', ');
+      console.log(chalk.red('  Supported extensions: ') + chalk.cyan(extStr));
+    }
+    console.log(chalk.red('  Searched in: ') + chalk.cyan(file.dir));
     return false;
   }
 }
