7906136a14b2fcd462c7cd242a2a34a499f580af
Adam Mockor <adam.mockor@gmail.com>
2018-02-26 14:39:58 +0100

    rename `public/index.html` => `public/styleguide.html`

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -43,7 +43,7 @@ const useYarn = fs.existsSync(paths.yarnLockFile);
 const isInteractive = process.stdout.isTTY;
 
 // Warn and crash if required files are missing
-if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
+if (!checkRequiredFiles([paths.styleguideHtml, paths.appIndexJs])) {
   process.exit(1);
 }
 
