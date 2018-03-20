7f1725700de75eed0b1bf30a30ce9416d61f75d0
David Moodie <davidmoodie12@gmail.com>
2018-03-20 15:46:03 +0000

    Remove workbox configuration options as c-r-a isn't planning on supporting optional configuration

--- packages/react-scripts/config/workbox.js
+++ packages/react-scripts/config/workbox.js
@@ -8,40 +8,13 @@
 // @remove-on-eject-end
 'use strict';
 
-const path = require('path');
-const paths = require('./paths');
-const fs = require('fs');
-const { GenerateSW, InjectManifest } = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
-const defaultGenerateConfig = {
+const workboxConfig = {
   exclude: [/\.map$/, /^(?:asset-)manifest.*\.js(?:on)?$/],
   navigateFallback: '/index.html',
   navigateFallbackWhitelist: [/^(?!\/__).*/], // fix for Firebase
 };
 
-const defaultInjectConfig = {
-  exclude: defaultGenerateConfig.exclude,
-  swSrc: path.join(paths.appSrc, 'sw.js'),
-};
-
-// Default method is generate
-let method = 'generate';
-let workboxConfig = defaultGenerateConfig;
-
-const craConfigPath = paths.craConfig;
-if (fs.existsSync(craConfigPath)) {
-  const craConfig = require(craConfigPath);
-  if ('workbox' in craConfig) {
-    // 'method' should be either 'generate' or 'inject'
-    method = craConfig.workbox.method || method;
-    workboxConfig =
-      method === 'generate' ? defaultGenerateConfig : defaultInjectConfig;
-    workboxConfig = craConfig.workbox.config || workboxConfig;
-  }
-}
-
-const plugin =
-  method === 'generate'
-    ? new GenerateSW(workboxConfig)
-    : new InjectManifest(workboxConfig);
+const plugin = new GenerateSW(workboxConfig);
 module.exports = plugin;
