a0d5c091c6802306746b61dccb2fbb6333fad5ba
Kevin Busby <earthrid@users.noreply.github.com>
2018-03-27 00:24:13 +0100

    Tiny typo in comment
    This is just to correct the typo "Lets check" in a comment to "Let's check".

--- packages/react-scripts/template/src/registerServiceWorker.js
+++ packages/react-scripts/template/src/registerServiceWorker.js
@@ -33,7 +33,7 @@ export default function register() {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Lets check if a service worker still exists or not.
+        // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl);
 
         // Add some additional logging to localhost, pointing developers to the
