986543946001996c21c08b1eff8b26197193c24a
Fabiano Brito <fabianopbrito@gmail.com>
2018-03-26 20:51:01 +0300

    Creates mock components and unit tests for SASS and SCSS with and without modules.

---
+++ packages/react-scripts/fixtures/kitchensink/src/features/webpack/ScssModulesInclusion.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import styles from './assets/scss-styles.module.scss';
+
+export default () => (
+  <p className={styles.scssModulesInclusion}>SCSS Modules are working!</p>
+);
