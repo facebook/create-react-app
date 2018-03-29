12c8fc9cdffa524b27eadd11d9eef9a4a8f33d20
Tankred Hase <mail@tankredhase.de>
2018-03-29 10:46:56 +0200

    Add support for react-native-svg via svgs

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -104,6 +104,7 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      'react-native-svg': 'svgs',
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
