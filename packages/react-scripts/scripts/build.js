8e9c36e97bbc9f63a7a918e71c78e985742ced0f
Adam Mockor <adam.mockor@gmail.com>
2018-04-02 23:01:50 +0200

    do not check if styleguide.html exists

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -52,7 +52,7 @@ const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
 const isInteractive = process.stdout.isTTY;
 
 // Warn and crash if required files are missing
-if (!checkRequiredFiles([paths.styleguideHtml, paths.appIndexJs])) {
+if (!checkRequiredFiles([paths.appIndexJs])) {
   process.exit(1);
 }
 
