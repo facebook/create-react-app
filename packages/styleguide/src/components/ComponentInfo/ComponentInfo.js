04f5a9832fc7c2a9317edaa112f2a4a9aced2b46
Adam Mockor <adam.mockor@gmail.com>
2018-04-17 08:45:22 +0200

    <ComponentInfo /> is more flexible

--- packages/styleguide/src/components/ComponentInfo/ComponentInfo.js
+++ packages/styleguide/src/components/ComponentInfo/ComponentInfo.js
@@ -1,32 +1,47 @@
 import React from 'react';
-import { array } from 'prop-types';
+import { arrayOf, array, shape, string } from 'prop-types';
 import styled from 'styled-components';
 
 import { Bar, BarItem } from '../Bar';
 
 import Info from './Info';
+import { propTypes as BadgePropTypes } from './../Badge/InfoBadge';
 
 const propTypes = {
-  implementation: array,
-  status: array
+  infoTypes: arrayOf(
+    shape({
+      label: string.isRequired,
+      values: array
+    })
+  ),
+  typeToColorMap: BadgePropTypes.typeToColorMap
 };
 
-const ComponentInfo = ({ status, implementation }) => (
-  <StyledComponentInfo>
-    <Bar>
-      {status && (
-        <BarItem>
-          <Info what="Status:" values={status} />
+const ComponentInfo = ({ infoTypes, typeToColorMap, ...other }) => {
+  const infoItems = infoTypes.reduce((types, type) => {
+    if (type.values) {
+      types = [
+        ...types,
+        <BarItem key={type.label}>
+          <Info
+            what={type.label}
+            values={type.values}
+            typeToColorMap={typeToColorMap}
+            {...other}
+          />
         </BarItem>
-      )}
-      {implementation && (
-        <BarItem>
-          <Info what="Implementation:" values={implementation} />
-        </BarItem>
-      )}
-    </Bar>
-  </StyledComponentInfo>
-);
+      ];
+    }
+
+    return types;
+  }, []);
+
+  return (
+    <StyledComponentInfo>
+      {infoItems && <Bar>{infoItems}</Bar>}
+    </StyledComponentInfo>
+  );
+};
 
 const StyledComponentInfo = styled.div`
   h1 + &,
