4e42a8b1557cb7d2f989819efb449461e6856fe0
Juraj Kiss <kiss@lbstudio.sk>
2018-05-02 10:53:19 +0200

    Refactor layout styles

--- packages/styleguide/src/App.js
+++ packages/styleguide/src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isActive: false
+      isNavActive: false
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleNavLinkClick = this.handleNavLinkClick.bind(this);
@@ -29,12 +29,12 @@ class App extends React.Component {
 
   // eslint-disable-next-line class-methods-use-this
   handleClick() {
-    this.setState({ isActive: !this.state.isActive });
+    this.setState({ isNavActive: !this.state.isNavActive });
   }
 
   handleNavLinkClick() {
-    if (this.state.isActive) {
-      this.setState({ isActive: false });
+    if (this.state.isNavActive) {
+      this.setState({ isNavActive: false });
     }
   }
 
@@ -49,7 +49,7 @@ class App extends React.Component {
       styleguideBasePath = '/styleguide/'
     } = config;
 
-    const activeClass = this.state.isActive ? 'is-active' : '';
+    const activeClass = this.state.isNavActive ? 'is-active' : '';
 
     // merge styleguide theme and project theme
     const localTheme = Object.keys(theme).reduce((acc, prop) => {
@@ -79,22 +79,17 @@ class App extends React.Component {
             >
               <NavigationButton
                 onClick={() => this.handleClick()}
-                isActive={this.state.isActive}
+                isActive={this.state.isNavActive}
               />
             </PageHeader>
-            <PageBody>
-              <PageContent className={activeClass}>
+            <PageBody className={activeClass}>
+              <PageContent>
                 <Sitemap routes={routes} />
               </PageContent>
-              <PageSidebar className={activeClass}>
+              <PageSidebar>
                 <Navigation
                   routes={routes}
                   onNavLinkClick={() => this.handleNavLinkClick()}
-                />
-                <NavigationButton
-                  onClick={() => this.handleClick()}
-                  isActive={this.state.isActive}
-                  isMobileButton
                 />
               </PageSidebar>
             </PageBody>
@@ -126,92 +121,71 @@ injectGlobal`
 /* eslint-enable */
 
 const PageLayout = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  min-height: calc(100vh - 6rem);
-  margin-top: ${props => rem(props.theme.sizes.headerHeight)};
 `;
 
 const PageHeader = styled(Header)`
+  width: 100%;
   height: ${props => rem(props.theme.sizes.headerHeight)};
   display: flex;
+  flex: 0 0 auto;
   justify-content: space-between;
   align-items: center;
   padding: 0 ${props => rem(props.theme.spaces.large)};
   background-color: ${props => props.theme.colors.main};
   color: ${props => props.theme.colors.black};
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
   z-index: ${props => props.theme.zIndex.header};
 `;
 
 const PageBody = styled.div`
   display: flex;
-  flex-direction: column;
-  overflow: hidden;
-
-  @media (min-width: ${props => props.theme.breakpoints.m}) {
-    flex-direction: row;
-    flex: 1 0 auto;
-  }
+  flex: 1 1 auto;
 `;
 
 const PageSidebar = styled(Sidebar)`
+  position: absolute;
+  height: 100%;
   display: flex;
+  flex: 0 0 ${props => rem(props.theme.sizes.sidebarWidth)};
   order: -1;
-  overflow: hidden;
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 0;
-  transition: width 0.3s ease-in-out 0s;
+  overflow: auto;
+  transform: translateX(-${props => rem(props.theme.sizes.sidebarWidth)});
+  transition: transform 0.3s ease-in-out 0s;
   z-index: ${props => props.theme.zIndex.sidebar};
 
-  &.is-active {
-    width: 100vw;
-    overflow: auto;
-
-    @media (min-width: ${props => props.theme.breakpoints.s}) {
-      width: ${props => rem(props.theme.sizes.sidebarWidth)};
-    }
-  }
-
-  @media (min-width: ${props => props.theme.breakpoints.s}) {
-    top: ${props => rem(props.theme.sizes.headerHeight)};
-    left: initial;
-    height: calc(100vh - 6rem);
-  }
-
   @media (min-width: ${props => props.theme.breakpoints.l}) {
-    width: ${props => rem(props.theme.sizes.sidebarWidth)};
-    overflow: auto;
+    margin-left: 0;
+    position: relative;
+    transform: translateX(0);
+  }
+
+  .is-active & {
+    transform: translateX(0);
   }
 `;
 
 const PageContent = styled.main`
-  max-width: 100%;
-  padding: ${props => rem(props.theme.spaces.default)}
-    ${props => rem(props.theme.spaces.medium)}
-    ${props => rem(props.theme.spaces.medium)};
   position: relative;
-  left: 0;
-  transition: left 0.3s ease-in-out 0s;
+  flex: 1 1 auto;
+  overflow-x: hidden;
+  overflow-y: auto;
+  padding: ${props => rem(props.theme.spaces.default)} 0;
+  transition: transform 0.3s ease-in-out 0s, opacity 0.3s ease-in-out 0s;
 
-  &.is-active {
-    left: ${props => rem(props.theme.sizes.sidebarWidth)};
+  .is-active & {
+    transform: translateX(${props => rem(props.theme.sizes.sidebarWidth)});
+    opacity: 0.5;
   }
 
   @media (min-width: ${props => props.theme.breakpoints.l}) {
-    width: calc(100vw - ${props => rem(props.theme.sizes.sidebarWidth)});
-    margin-left: ${props => rem(props.theme.sizes.sidebarWidth)};
     padding-left: 0;
-
-    &.is-active {
-      left: 0;
+    .is-active & {
+      transform: translateX(0);
+      opacity: 1;
     }
   }
 `;
