31d5eca066ab1bb4c506d467ee36f5ba3071e2a4
Dan <dan.abramov@gmail.com>
2018-05-11 20:04:03 +0100

    Fix bad merge

--- packages/babel-preset-react-app/index.js
+++ packages/babel-preset-react-app/index.js
@@ -23,6 +23,18 @@ module.exports = function(api, opts) {
     opts = {};
   }
 
+  // This is similar to how `env` works in Babel:
+  // https://babeljs.io/docs/usage/babelrc/#env-option
+  // We are not using `env` because it’s ignored in versions > babel-core@6.10.4:
+  // https://github.com/babel/babel/issues/4539
+  // https://github.com/facebook/create-react-app/issues/720
+  // It’s also nice that we can enforce `NODE_ENV` being specified.
+  var env = process.env.BABEL_ENV || process.env.NODE_ENV;
+  var isEnvDevelopment = env === 'development';
+  var isEnvProduction = env === 'production';
+  var isEnvTest = env === 'test';
+  var isFlowEnabled = validateBoolOption('flow', opts.flow, true);
+
   if (!isEnvDevelopment && !isEnvProduction && !isEnvTest) {
     throw new Error(
       'Using `babel-preset-react-app` requires that you specify `NODE_ENV` or ' +
