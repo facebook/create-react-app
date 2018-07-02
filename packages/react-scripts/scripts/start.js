1e5c0487bfbc7dbba44eb9f4bacbb9caf73d5355
Valerii Gusev <valerii.gusev@cognite.com>
2018-07-02 08:26:33 +0200

    Run start script on localhost with https on by default
    You can enable chrome://flags/#allow-insecure-localhost flag in order to prevent certificate warning message

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -12,6 +12,11 @@
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
+// We want to run this script on localhost with https on by default
+if (process.env.HTTPS === undefined) {
+  process.env.HTTPS = 'true';
+}
+
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
