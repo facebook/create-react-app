39ed9d1f4caf1cced32b3526e5614d7b572e9da8
radu <radu.helstern@deskpro.com>
2018-08-20 17:14:57 +0100

    use apps sdk version ^0.7.1

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -51,7 +51,7 @@ module.exports = function(
       'react-scripts prettier --list-different "{src,test}/**/*.js"',
   };
 
-  const sdkVersion = '^0.7.0';
+  const sdkVersion = '^0.7.1';
   Object.assign(appPackage.dependencies, {
     '@deskpro/apps-sdk': sdkVersion,
     '@deskpro/apps-components': sdkVersion,
