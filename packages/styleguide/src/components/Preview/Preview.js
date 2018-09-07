e6d68b8e5184a54398c7542c553d4d9af70114b4
Benjamín Starý <stary.benjamin@gmail.com>
2018-09-07 15:41:02 +0200

    removed unnecessary code

--- packages/styleguide/src/components/Preview/Preview.js
+++ packages/styleguide/src/components/Preview/Preview.js
@@ -40,6 +40,8 @@ export default class Preview extends Component {
     super(props);
 
     this.handleToggleCode = this.handleToggleCode.bind(this);
+    this.handleBackroundsToArray = this.handleBackroundsToArray.bind(this);
+    this.handlePreviewBackground = this.handlePreviewBackground.bind(this);
   }
 
   state = {
@@ -47,18 +49,8 @@ export default class Preview extends Component {
   };
 
   componentDidMount() {
-    const previewBackgroundsArray = [];
-
-    Object.keys(previewBackgrounds).map(key => {
-      return previewBackgroundsArray.push({
-        value: previewBackgrounds[key],
-        label: key
-      });
-    });
-
     this.setState({
-      previewBackgroundsList: previewBackgroundsArray,
-      previewBackground: previewBackgroundsArray[0]
+      previewBackground: this.handleBackroundsToArray()
     });
   }
 
@@ -67,6 +59,22 @@ export default class Preview extends Component {
       isCodeShown: !this.state.isCodeShown
     });
   }
+
+  handleBackroundsToArray = () => {
+    const previewBackgroundsArray = Object.keys(previewBackgrounds).map(key => {
+      return {
+        value: previewBackgrounds[key],
+        label: key
+      };
+    });
+    return previewBackgroundsArray;
+  };
+
+  handlePreviewBackground = previewBackground => {
+    this.setState({ previewBackground });
+  };
+
+  return;
 
   render() {
     const {
@@ -83,6 +91,8 @@ export default class Preview extends Component {
       html,
       ...other
     } = this.props;
+
+    const { previewBackground } = this.state;
 
     const classes = cx(CLASS_ROOT, className);
 
@@ -116,7 +126,7 @@ export default class Preview extends Component {
         value={previewBackground}
         placeholder={previewBackground.value}
         onChange={this.handlePreviewBackground}
-        options={previewBackgroundsList}
+        options={this.handleBackroundsToArray()}
         styles={colourStyles}
       />
     );
