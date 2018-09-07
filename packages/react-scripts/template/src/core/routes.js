8d0846fdf2280f7fb6bb90dc70361db03e8f46de
thanawat <tha_chookes@hotmail.com>
2018-09-07 11:54:13 +0700

    code style follow eslint

--- packages/react-scripts/template/src/core/routes.js
+++ packages/react-scripts/template/src/core/routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Main from '../containers/main'
 
 const Routes = () => (
   <Router>
     <div>
-      <Route path='/test' component={Main} />
+      <Route path="/test" component={Main} />
     </div>
   </Router>
 )
