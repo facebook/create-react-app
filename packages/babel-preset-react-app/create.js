56b4ae5fb3532111673ab498ad68a0e2587609e7
vikr01 <vik120@icloud.com>
2018-09-07 21:24:19 -0700

    use the correct dependency for babel-plugin-dynamic-import-node

--- packages/babel-preset-react-app/create.js
+++ packages/babel-preset-react-app/create.js
@@ -158,7 +158,7 @@ module.exports = function(api, opts, env) {
       require('@babel/plugin-syntax-dynamic-import').default,
       isEnvTest &&
         // Transform dynamic import to require
-        require('babel-plugin-transform-dynamic-import').default,
+        require('babel-plugin-dynamic-import-node'),
     ].filter(Boolean),
   };
 };
