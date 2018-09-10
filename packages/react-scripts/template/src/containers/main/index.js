8124c2fd2dca804686320e0dbc5611eeb5b993f3
Chotipat Poowongthanarat <topz.classic@gmail.com>
2018-09-10 11:09:12 +0700

    sample style

--- packages/react-scripts/template/src/containers/main/index.js
+++ packages/react-scripts/template/src/containers/main/index.js
@@ -1,19 +1,38 @@
 import React from 'react'
 import { compose } from 'recompose'
 import { inject, observer } from 'mobx-react'
+import styled from 'styled-components'
 
-type Props = {}
+type Props = {
+  title?: string
+}
+
+const Container = styled.div`
+  display: flex
+  flex-direction: column
+  flex: 1
+  align-items: center
+`
+
+const Header = styled.h1.attrs({
+  isbig: props => props.isbig ? '3em' : '2em'
+})`
+  font-size: ${props => props.isbig}
+`
 
 const Page1 = (props: Props) => {
-  const { ExampleStore } = props
+  const { ExampleStore, title } = props
   return (
-    <div>
-      <h1>{ExampleStore.exampleData.text}</h1>
-      <h1>Page1 </h1>
-    </div>
+    <Container >
+      <Header isbig >{ExampleStore.exampleData.text}</Header>
+      <Header>{title} </Header>
+    </Container>
   )
 }
 
+Page1.defaultProps = {
+  title: 'Page1'
+}
 
 export default compose(
   inject('ExampleStore'),
