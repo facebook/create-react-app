92a362801f6fc63e391c9864f34606c6fda6cc39
Matthew Davidson <matthewdavidson@users.noreply.github.com>
2018-09-10 09:38:19 +0100

    [BPK-1884] More cleanup (#34)
    [BPK-1884] More cleanup
    
    - Downgrade stylelint to get rid of peer dep warnings
    - Reintroduce sugarss and postcss-less to get rid of peer dep warnigns
    - Remove usage of backpack grid component
    - Remove backpack-react-scripts from template deps
    - This was added to "move" the dep from "dependencies" -> "devDependencies"
    - However it is confusing behaviour during development and pre-releases
    - E.g. during local testing, `npm create-react-app my-app` actually installs that latest version from the registry due to `"*"`

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,7 +1,12 @@
-import 'bpk-stylesheets';
+import 'bpk-stylesheets/base';
+import 'bpk-stylesheets/base.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById('root'),
+);
