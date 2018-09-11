3b9358263d8f25eeae1547c90dc36008e12e5f06
Clifton <clifton.craig@gmail.com>
2018-09-11 16:20:44 -0700

    Enable setting various types of sourcemaps.
    On certain platforms (for eg. Tizen TVs) we need the ability to enable a specific type of sourcemap (eval-source-map). True/false is too limiting.

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -28,7 +28,8 @@ const getClientEnvironment = require('./env');
 // It requires a trailing slash, or the file assets will get an incorrect path.
 const publicPath = paths.servedPath;
 // Source maps are resource heavy and can cause out of memory issue for large source files.
-const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
+// We want to enable different types of sourcemaps since certain platforms only support certain types of sourcemaps.
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP;
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
