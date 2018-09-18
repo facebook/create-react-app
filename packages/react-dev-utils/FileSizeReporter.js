ea92b9c8981683367bcc92b58ac3a39344772b81
Fredrik Palmquist <fredrik.palmquist@cybercom.com>
2018-09-18 08:54:21 +0200

    fix FileSizeReporter in Windows enviroment

--- packages/react-dev-utils/FileSizeReporter.js
+++ packages/react-dev-utils/FileSizeReporter.js
@@ -43,7 +43,7 @@ function printFileSizesAfterBuild(
             name: path.basename(asset.name),
             size: size,
             sizeLabel:
-              filesize(size) + (difference ? ' (' + difference + ')' : '')
+              filesize(size) + (difference ? ' (' + difference + ')' : ''),
           };
         })
     )
@@ -98,6 +98,7 @@ function printFileSizesAfterBuild(
 function removeFileNameHash(buildFolder, fileName) {
   return fileName
     .replace(buildFolder, '')
+    .replace(/\\/g, '/')
     .replace(
       /\/?(.*)(\.[0-9a-f]+)(\.chunk)?(\.js|\.css)/,
       (match, p1, p2, p3, p4) => p1 + p4
