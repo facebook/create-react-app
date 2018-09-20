920f5f2600f172c1894ef4b65f458d5d9a457535
Joe Haddad <timer150@gmail.com>
2018-09-20 15:59:32 -0400

    Correct babel dependency compile target

--- packages/babel-preset-react-app/dependencies.js
+++ packages/babel-preset-react-app/dependencies.js
@@ -48,6 +48,17 @@ module.exports = function(api, opts) {
         // Latest stable ECMAScript features
         require('@babel/preset-env').default,
         {
+          // We want Create React App to be IE 9 compatible until React itself
+          // no longer works with IE 9
+          targets: {
+            ie: 9,
+          },
+          // Users cannot override this behavior because this Babel
+          // configuration is highly tuned for ES5 support
+          ignoreBrowserslistConfig: true,
+          // If users import all core-js they're probably not concerned with
+          // bundle size. We shouldn't rely on magic to try and shrink it.
+          useBuiltIns: false,
           // Do not transform modules to CJS
           modules: false,
         },
