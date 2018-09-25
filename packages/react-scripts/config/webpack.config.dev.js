35f0d0eda0844c6aa2ab6d922e9aab68401c61e3
Joe Haddad <timer150@gmail.com>
2018-09-25 14:54:57 -0400

    Switch to stage 3 css

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -60,7 +60,7 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
             autoprefixer: {
               flexbox: 'no-2009',
             },
-            stage: 4,
+            stage: 3,
           }),
         ],
       },
