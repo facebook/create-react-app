82c82f85b55526716714362c51f5249afdc3b2e4
Joe Haddad <timer150@gmail.com>
2018-09-25 18:20:44 -0400

    Use object assign not object spread

--- packages/babel-preset-react-app/dev.js
+++ packages/babel-preset-react-app/dev.js
@@ -9,5 +9,5 @@
 const create = require('./create');
 
 module.exports = function(api, opts) {
-  return create(api, { helpers: false, ...opts }, 'development');
+  return create(api, Object.assign({ helpers: false }, opts), 'development');
 };
