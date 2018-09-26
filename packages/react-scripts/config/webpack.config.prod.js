4e34d8678ec2e3e37cedde423349515a2a13394f
Dominic Chambers <dominic.chambers@babylonhealth.com>
2018-09-26 22:45:18 +0100

    Make CSS Module classes test friendly
    It's impossible to write CSS selectors against class names that
    contain random hashses, so use a friendlier naming scheme that's still
    able to minimize the risk of a name clash occuring.
    
    For example:
    
    ```
    .babylon-component-library__Textarea__label {
      color: red;
    }
    ```

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -234,7 +234,8 @@ module.exports = {
                         minimize: true,
                         sourceMap: true,
                         modules: true,
-                        localIdentName: '[name]__[local]___[hash:base64:5]',
+                        localIdentRegExp: /.*\/([^/]+)\/src/,
+                        localIdentName: '[1]__[folder]__[local]',
                       },
                     },
                     {
