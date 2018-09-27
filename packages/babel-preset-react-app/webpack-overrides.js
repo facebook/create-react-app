95dc22403a81bb88a91142262babf64c2defe0e6
Ian Sutherland <ian@iansutherland.ca>
2018-09-27 15:29:09 -0600

    Update comment about cache identifier

--- packages/babel-preset-react-app/webpack-overrides.js
+++ packages/babel-preset-react-app/webpack-overrides.js
@@ -14,8 +14,8 @@ module.exports = function() {
     config(config, { source }) {
       // Babel Macros are notoriously hard to cache, so they shouldn't be
       // https://github.com/babel/babel/issues/8497
-      // We naively detect macros using their package suffix and insert a random
-      // caller name, a valid option accepted by Babel, to compose a one-time
+      // We naively detect macros using their package suffix and add a random token
+      // to the caller, a valid option accepted by Babel, to compose a one-time
       // cacheIdentifier for the file. We cannot tune the loader options on a per
       // file basis.
       if (source.indexOf('.macro') !== -1 || source.indexOf('/macro') !== -1) {
