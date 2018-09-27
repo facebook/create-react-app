a44f0b3181c939a2a793a8659c6cc4ffbe76f9bd
Joe Haddad <timer150@gmail.com>
2018-09-27 09:12:46 -0400

    explain options

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -160,7 +160,14 @@ module.exports = {
       new OptimizeCSSAssetsPlugin({
         cssProcessorOptions: {
           parser: safePostCssParser,
-          map: { inline: false, annotation: true },
+          map: {
+            // `inline: false` forces the sourcemap to be output into a
+            // separate file
+            inline: false,
+            // `annotation: true` appends the sourceMappingURL to the end of
+            // the css file, helping the browser find the sourcemap
+            annotation: true,
+          },
         },
       }),
     ],
