328c3123e889d7e46fd8ce0b65e275e02aa9eb27
Joe Haddad <timer150@gmail.com>
2018-09-27 10:23:02 -0400

    Don't polyfill fetch for Node (#5132)

--- packages/react-app-polyfill/jsdom.js
+++ packages/react-app-polyfill/jsdom.js
@@ -6,5 +6,9 @@
  */
 'use strict';
 
-// fetch() polyfill for making API calls.
-require('whatwg-fetch');
+// Make sure we're in a Browser-like environment before importing polyfills
+// This prevents `fetch()` from being imported in a Node test environment
+if (typeof window !== 'undefined') {
+  // fetch() polyfill for making API calls.
+  require('whatwg-fetch');
+}
