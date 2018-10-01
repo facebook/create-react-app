9e074bbf08a8760579e7345fe1391a76f9c41f96
Maël Nison <nison.mael@gmail.com>
2018-10-01 15:53:07 +0100

    Plug'n'Play support (#5136)
    * Adds the PnP plugin for Webpack to find dependencies when working under PnP
    
    * Adds configuration for jest
    
    * Adds an e2e test for when using PnP
    
    * Avoids cra from crashing at the engine check
    
    * Avoids cra from crashing when initializing react-scripts
    
    * Makes the ownPath portable
    
    * Fixes linting
    
    * Bumps to pnp-webpack-plugin@1.1.0, removes symlinks: false
    
    * Adds a --use-pnp option
    
    * Pin version

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -10,6 +10,7 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const PnpWebpackPlugin = require('pnp-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 const TerserPlugin = require('terser-webpack-plugin');
@@ -214,12 +215,22 @@ module.exports = {
       'react-native': 'react-native-web',
     },
     plugins: [
+      // Adds support for installing with Plug'n'Play, leading to faster installs and adding
+      // guards against forgotten dependencies and such.
+      PnpWebpackPlugin,
       // Prevents users from importing files from outside of src/ (or node_modules/).
       // This often causes confusion because we only process files within src/ with babel.
       // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
       new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+    ],
+  },
+  resolveLoader: {
+    plugins: [
+      // Also related to Plug'n'Play, but this time it tells Webpack to load its loaders
+      // from the current package.
+      PnpWebpackPlugin.moduleLoader(module),
     ],
   },
   module: {
