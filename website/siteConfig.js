2a2e8bf1b8c33a0e0897c6f1af48de148ad4bbd1
Kristofer Selbekk <selbekk@users.noreply.github.com>
2018-10-02 15:22:18 +0200

    Set the project name and owner correctly (#5243)
    This commit sets the page name to "Create React App", the tagline to
    "Create React apps with no build configuration." and the copyright
    owner to "Facebook"

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,16 +21,16 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  title: 'Create React App', // Title for your website.
+  tagline: 'Create React apps with no build configuration.',
+  url: 'https://facebook.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'create-react-app',
   organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
@@ -38,9 +38,9 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
+    { doc: 'doc1', label: 'Docs' },
+    { doc: 'doc4', label: 'API' },
+    { page: 'help', label: 'Help' },
   ],
 
   // If you have users set above, you add it here:
@@ -72,7 +72,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Facebook`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
