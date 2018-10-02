3c04d5224fa335200be09e8a565c8047993c8afd
Kristofer Selbekk <kristofer.selbekk@sparebank1.no>
2018-10-02 14:56:24 +0200

    Remove users and help pages

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,18 +8,6 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
@@ -37,14 +25,7 @@ const siteConfig = {
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-  ],
-
-  // If you have users set above, you add it here:
-  users,
+  headerLinks: [{ doc: 'doc1', label: 'Docs' }, { doc: 'doc4', label: 'API' }],
 
   /* path to images for header/footer */
   headerIcon: 'img/docusaurus.svg',
