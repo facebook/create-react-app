4bda8f735032d0c1c09b806a9e4936a7219a8b56
Kristofer Selbekk <selbekk@users.noreply.github.com>
2018-10-04 00:09:14 +0200

    5238 set up basic docs structure (#5245)
    * Add a temporary README file
    
    This commit adds a temporary readme file that points users to the
    main README file or the User Guide.
    
    * Add a "getting started" doc
    
    This commit adds a page for getting started, copying the
    "Get started now" from the current `README.md`.
    
    * Add link to the get started page from the index page
    
    * Add the getting started doc to the site header
    
    * Add footer link to getting started info
    
    * Add getting started to the sidebar
    
    * Add placeholder for the user guide page
    
    * Add links to the user-guide page as well
    
    * Add auto-generated i18n file
    
    * Move all docusaurus related files into own folder

--- docusaurus/website/core/Footer.js
+++ docusaurus/website/core/Footer.js
@@ -34,11 +34,11 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('doc1.html', this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl('getting-started', this.props.language)}>
+              Getting Started
             </a>
-            <a href={this.docUrl('doc2.html', this.props.language)}>
-              Guides (or other categories)
+            <a href={this.docUrl('user-guide', this.props.language)}>
+              User Guide
             </a>
             <a href={this.docUrl('doc3.html', this.props.language)}>
               API Reference (or other categories)
--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -26,8 +26,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: 'doc1', label: 'Docs' },
-    { doc: 'doc4', label: 'API' },
+    { doc: 'getting-started', label: 'Getting started' },
     { href: 'https://reactjs.org/community/support.html', label: 'Help' },
   ],
 
