169b57f8aef046b582ae9527d85f7d4a957a0efa
Hieu Pham <phthhieu@gmail.com>
2018-10-03 20:57:33 +0700

    Temporary disable split chunk optimization
    [No Trello]

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -197,13 +197,13 @@ module.exports = {
     // Automatically split vendor and commons
     // https://twitter.com/wSokra/status/969633336732905474
     // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-    splitChunks: {
-      chunks: 'all',
-      name: false,
-    },
-    // Keep the runtime chunk seperated to enable long term caching
-    // https://twitter.com/wSokra/status/969679223278505985
-    runtimeChunk: true,
+    // splitChunks: {
+    //   chunks: 'all',
+    //   name: false,
+    // },
+    // // Keep the runtime chunk seperated to enable long term caching
+    // // https://twitter.com/wSokra/status/969679223278505985
+    // runtimeChunk: true,
   },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
