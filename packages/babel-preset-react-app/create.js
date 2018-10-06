af0a7fb0201f14c9bc38ee5d72c6e35e89ba1515
Charles Assuncao <junioassuncaocharles@gmail.com>
2018-10-06 16:32:11 +0200

    Removing unnecessary spread plugin proposal since it's in @babel@7

--- packages/babel-preset-react-app/create.js
+++ packages/babel-preset-react-app/create.js
@@ -120,15 +120,6 @@ module.exports = function(api, opts, env) {
           loose: true,
         },
       ],
-      // The following two plugins use Object.assign directly, instead of Babel's
-      // extends helper. Note that this assumes `Object.assign` is available.
-      // { ...todo, completed: true }
-      [
-        require('@babel/plugin-proposal-object-rest-spread').default,
-        {
-          useBuiltIns: true,
-        },
-      ],
       // Polyfills the runtime needed for async/await, generators, and friends
       // https://babeljs.io/docs/en/babel-plugin-transform-runtime
       [
