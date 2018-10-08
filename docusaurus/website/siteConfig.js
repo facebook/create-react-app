ffc63d55976f9cbcce7f33dc7c45b3c2190a5924
Kristofer Selbekk <selbeezy@gmail.com>
2018-10-08 18:40:09 +0200

    Add Inc to copyright notice

--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -60,7 +60,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Facebook`,
+  copyright: `Copyright © ${new Date().getFullYear()} Facebook Inc.`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
