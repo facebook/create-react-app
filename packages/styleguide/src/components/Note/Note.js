4aefa8b263b18385e3c8e265d307f9f1f99919f5
Dusan Tuzinsky <brooklynPD@gmail.com>
2018-10-16 10:57:25 +0200

    NoteWarning fixes (#34)
    * NoteWarning fixes
    new NoteInfo variant
    Note default font
    
    * variant prop back to type prop

--- packages/styleguide/src/components/Note/Note.js
+++ packages/styleguide/src/components/Note/Note.js
@@ -6,22 +6,24 @@ import cx from 'classnames';
 const propTypes = {
   title: string,
   /* type influences set styles */
-  type: oneOf(['warning', 'success', 'error'])
+  type: oneOf(['warning', 'success', 'error', 'info']),
 };
 
 const CLASS_ROOT = 'note';
 
 /* Styles have to be on the top because of Tag and TagTitle */
 const StyledNote = styled.div`
+  font-family: ${props => props.theme.fontFamily};
+  font-weight: ${props => props.theme.fontWeights.normal};
+  font-size: ${props => props.theme.fontSizes.base};
+  color: ${props => props.theme.colors.black};
   width: 100%;
   margin: 0 0 ${props => props.theme.spaces.medium};
   padding: ${props => props.theme.spaces.small}
     ${props => props.theme.spaces.medium};
   border-left-width: 2px;
   border-left-style: solid;
-  border-left-color: ${props =>
-    (props.type === 'warning' && props.theme.colors.orange) ||
-    props.theme.colors.greyDarker};
+  border-left-color: ${props => props.theme.colors.greyDarker};
 `;
 
 const StyledTitle = styled.div`
@@ -30,6 +32,7 @@ const StyledTitle = styled.div`
     (props.type === 'success' && props.theme.colors.success) ||
     (props.type === 'warning' && props.theme.colors.warning) ||
     (props.type === 'error' && props.theme.colors.error) ||
+    (props.type === 'info' && props.theme.colors.info) ||
     props.theme.colors.greyText};
   text-transform: uppercase;
   font-weight: 700;
--- packages/styleguide/src/components/Note/index.js
+++ packages/styleguide/src/components/Note/index.js
@@ -6,7 +6,10 @@ export const NoteWarning = props => <Note type="warning" {...props} />;
 NoteWarning.displayName = 'NoteWarning';
 
 export const NoteError = props => <Note type="error" {...props} />;
-NoteWarning.displayName = 'NoteWarning';
+NoteError.displayName = 'NoteError';
+
+export const NoteInfo = props => <Note type="info" {...props} />;
+NoteInfo.displayName = 'NoteInfo';
 
 export const Dos = props => <Note type="success" title="Do's" {...props} />;
 Dos.displayName = 'Dos';
