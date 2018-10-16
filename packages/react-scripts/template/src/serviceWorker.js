e912cf860a427472b079f6d6b78b11f18e07bce2
Ran Yitzhaki <ranyitz@gmail.com>
2018-10-16 09:42:12 +0300

    remove dot from the end of the link to fix it when using from github preview

--- packages/react-scripts/template/src/serviceWorker.js
+++ packages/react-scripts/template/src/serviceWorker.js
@@ -8,7 +8,7 @@
 // resources are updated in the background.
 
 // To learn more about the benefits of this model and instructions on how to
-// opt-in, read http://bit.ly/CRA-PWA.
+// opt-in, read http://bit.ly/CRA-PWA
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
