be52b24cfba3ca11d5e6ab318a7094830589ed67
Joe Haddad <timer150@gmail.com>
2018-10-21 18:38:32 -0400

    Check for TypeScript install in preflight

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -22,11 +22,13 @@ process.on('unhandledRejection', err => {
 // Ensure environment variables are read.
 require('../config/env');
 // @remove-on-eject-begin
-// Do the preflight check (only happens before eject).
+// Do the preflight checks (only happens before eject).
 const verifyPackageTree = require('./utils/verifyPackageTree');
 if (process.env.SKIP_PREFLIGHT_CHECK !== 'true') {
   verifyPackageTree();
 }
+const verifyTypeScriptSetup = require('./utils/verifyTypeScriptSetup');
+verifyTypeScriptSetup();
 // @remove-on-eject-end
 
 const path = require('path');
