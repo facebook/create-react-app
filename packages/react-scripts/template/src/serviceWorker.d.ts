9b4009d7185ce403ab760c7c6ccd58505d4e1fe6
Joe Haddad <timer150@gmail.com>
2018-10-23 20:31:57 -0400

    Remove service worker types from the main install

--- packages/react-scripts/template/src/serviceWorker.d.ts
+++
@@ -1,8 +0,0 @@
-export type Config = {
-  onSuccess?: (registration: ServiceWorkerRegistration) => void
-  onUpdate?: (registration: ServiceWorkerRegistration) => void
-}
-
-export function register(config: Config): void
-
-export function unregister(): void
