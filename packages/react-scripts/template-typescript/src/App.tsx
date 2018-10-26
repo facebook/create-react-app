952e7b13c49ce14b88fe4ed4fca8dc8326c24fcf
Bruno Lemos <brunolemos@users.noreply.github.com>
2018-10-26 15:57:21 -0300

    Update App.tsx (#5588)

--- packages/react-scripts/template-typescript/src/App.tsx
+++ packages/react-scripts/template-typescript/src/App.tsx
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
             className="App-link"
