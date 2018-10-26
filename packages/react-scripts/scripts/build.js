5dbb3c362d26c369016d1fdb17273f3473f10290
Andreas Sandberg <andreas.a.sandberg@gmail.com>
2018-10-25 22:40:19 -0700

    Update build.js
    Allow NODE_ENV to determine which .env files are loaded for the build.

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -8,10 +8,6 @@
 // @remove-on-eject-end
 'use strict';
 
-// Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
-
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
@@ -21,6 +17,11 @@ process.on('unhandledRejection', err => {
 
 // Ensure environment variables are read.
 require('../config/env');
+
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+
 // @remove-on-eject-begin
 // Do the preflight checks (only happens before eject).
 const verifyPackageTree = require('./utils/verifyPackageTree');
