cf05acebfd81b659333fb1d08926b7b2d6d7844b
Dominic Chambers <dominic.chambers@babylonhealth.com>
2018-10-27 20:48:49 +0100

    Make CSS Module classes test friendly
    It's impossible to write CSS selectors against class names that contain
    random hashses, so use a friendlier naming scheme that's still able to
    minimize the risk of a name clash occuring.
    
    For example:
    
    ```
    .babylon-component-library__Textarea__label {
          color: red;
    }
    ```
    
    Co-authored-by: MentalAtom <matt.malone@babylonhealth.com>

---
+++ packages/react-scripts/config/getCSSModuleLocalIdent.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const getCSSModuleLocalIdent = (context, localIdentName, localName) => {
+  const projectNameMatches =
+    /.*\/([^/]+)\/src|lib\/([^/]+)/.exec(context.resourcePath) || [];
+
+  const projectName =
+    projectNameMatches[2] || projectNameMatches[1] || 'babylon';
+
+  const pathInsideSrc =
+    context.resourcePath.split(
+      new RegExp(`${projectName}/(?:src/)?`, 'g')
+    )[1] || context.resourcePath;
+
+  const parentFolderName = pathInsideSrc
+    ? pathInsideSrc.split('/').slice(-2, -1)[0]
+    : context.resourcePath.split('/').slice(-2, -1)[0];
+
+  const fileName =
+    pathInsideSrc
+      .split('/')
+      .slice(-1)[0]
+      .split('.')[0] || '';
+
+  let moduleName = /^[A-Z]/.test(fileName) ? fileName : parentFolderName;
+
+  return `${projectName}__${moduleName}__${localName}`;
+};
+
+module.exports = getCSSModuleLocalIdent;
