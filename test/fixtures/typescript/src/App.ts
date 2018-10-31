5b32018333ea3e29c2ff37e9c2c25e3c0c181620
Joe Haddad <timer150@gmail.com>
2018-10-31 19:40:19 -0400

    Add decorator test

--- test/fixtures/typescript/src/App.ts
+++ test/fixtures/typescript/src/App.ts
@@ -6,9 +6,14 @@ interface MyType {
 
 type MyObject = Pick<MyType, 'bar' | 'baz'>;
 
+@annotation
 class App {
   static foo: MyObject = { bar: true, baz: { n: 123 } };
   n = App.foo.baz!.n;
+}
+
+function annotation(target: any) {
+  target.annotated = true;
 }
 
 export default App;
