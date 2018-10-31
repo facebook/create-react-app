aeb1a83268ebd20132e6cc23d6fd56b8beaa4d66
AmaldevAD <amaldev.ad71868@github.com>
2018-10-31 17:35:28 +0530

    Simple react app using material ui

--- .github/PULL_REQUEST_TEMPLATE.md
+++
@@ -1,7 +0,0 @@
-<!--
-Thank you for sending the PR!
-
-If you changed any code, please provide us with clear instructions on how you verified your changes work. Bonus points for screenshots!
-
-Happy contributing!
--->
--- test/fixtures/typescript/src/App.test.ts
+++
@@ -1,8 +0,0 @@
-import App from './App';
-
-it('reads a typescript file with no syntax error', () => {
-  const app = new App();
-  expect(App.foo.bar).toBe(true);
-  expect(App.foo.baz!.n).toBe(123);
-  expect(app.n).toBe(123);
-});
