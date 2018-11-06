6f5fe2abd9fe9291aaedd4879599b34a8b97813d
Joe Haddad <timer150@gmail.com>
2018-11-05 20:51:07 -0500

    Adjust comment

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -557,7 +557,8 @@ module.exports = function(env) {
       new ModuleNotFoundPlugin(paths.appPath),
       // Makes some environment variables available to the JS code, for example:
       // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
-      // It is absolutely essential that NODE_ENV was set to production here.
+      // It is absolutely essential that NODE_ENV is set to production
+      // during a production build.
       // Otherwise React will be compiled in the very slow development mode.
       new webpack.DefinePlugin(env.stringified),
       new MiniCssExtractPlugin({
