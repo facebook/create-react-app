fa2af426f2bf36a35309a620d72c19c5d374deaa
Joey Cozza <joeycozza@gmail.com>
2018-11-07 14:31:04 -0700

    tweaking the template App.js and App.jsx to give correct instructions

--- packages/react-scripts/template-redux/src/components/App.jsx
+++ packages/react-scripts/template-redux/src/components/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/components/App.jsx</code> and save to reload.
           </p>
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn Frontier
