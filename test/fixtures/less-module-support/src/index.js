6676e047c91c166e6cc05748d26a9b9b995b2e2d
zenghongtu <zenghongtu@qq.com>
2018-11-30 21:09:54 +0800

    Add less module tests

---
+++ test/fixtures/less-module-support/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from './index.module.less';
+
+ReactDOM.render(<div className={style.div} />, document.getElementById('root'));
