8d8f2060bfa968572cf052937a5e170c4d76b5fa
Dan Abramov <dan.abramov@gmail.com>
2018-12-04 00:32:28 +0000

    Just use context

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -4,28 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-let setStates = new Map();
+let HotContext = React.createContext();
+let _invalidate;
+function HotContainer({ children }) {
+  const [inc, setInc] = React.useState(0);
+  _invalidate = () => setInc(c => c + 1);
+  return <HotContext.Provider value={inc}>{children}</HotContext.Provider>;
+}
 
 window.__enqueueForceUpdate = function(type) {
   console.log('force', type.name);
-  const id = type.__hot__id;
-  (setStates.get(id) || []).forEach(setState => setState({}));
+  _invalidate();
 };
 
 window.__renderHook = function(type) {
   console.log('render', type.name);
-  const [, setState] = React.useState();
-  React.useLayoutEffect(() => {
-    const id = type.__hot__id;
-    if (!setStates.has(id)) {
-      setStates.set(id, new Set());
-    }
-    setStates.get(id).add(setState);
-    return () => setStates.get(id).delete(setState);
-  }, []);
+  React.useContext(HotContext);
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HotContainer>
+    <App />
+  </HotContainer>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
