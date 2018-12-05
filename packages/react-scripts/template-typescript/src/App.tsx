8e7984c54331162f0148133e77bef3e5d4d27ff5
Adrian Kremer <a.kremer@reply.de>
2018-12-05 16:42:31 +0100

    disabled ordered-imports, excluded serviceWorker.ts

--- packages/react-scripts/template-typescript/src/App.tsx
+++ packages/react-scripts/template-typescript/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
