af5dd9b47613a0abd451609a984a31c4d06cb89d
dugagjin <lashi.dugagjin@gmail.com>
2018-12-06 11:58:01 +0100

    add webp support for typescript

--- packages/react-scripts/lib/react-app.d.ts
+++ packages/react-scripts/lib/react-app.d.ts
@@ -43,6 +43,11 @@ declare module '*.svg' {
   export default src;
 }
 
+declare module '*.webp' {
+    const src: string;
+    export default src;
+}
+
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
