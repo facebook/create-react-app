1df87e1d3c55311e5b28d1c8df2a2efaf59360f4
Matt Malone <matt.malone@babylonhealth.com>
2018-12-07 11:50:05 +0000

    Break up methods for readability

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -62,26 +62,30 @@ class ExtractEnvVarsPlugin {
     this.vars = []
   }
 
-  apply(compiler) {
-    const vars = this.vars
+  shouldProcessModule(module) {
+    return module._source && module._source._value && /\.js$/.test(module.resource)
+  }
 
+  findMatchesInFile(fileContent) {
+    const vars = this.vars
+    const regExp = /env(?:Flag|Url|Var)\(\s*["']([A-Z0-9_]+)["']/g
+    let match = null
+
+    while ((match = regExp.exec(fileContent)) !== null) {
+      if (vars.indexOf(match[1]) < 0) {
+        vars.push(match[1])
+      }
+    }
+  }
+
+  apply(compiler) {
     compiler.hooks.compilation.tap('ExtractEnvVarsPlugin', (compilation) => {
       compilation.hooks.optimizeChunks.tap('ExtractEnvVarsPlugin', chunks => {
         chunks.forEach(chunk => {
           const modules = Array.from(chunk._modules)
-          modules.forEach(module => {
-            if (module._source && module._source._value && /\.js$/.test(module.resource)) {
-              const content = fs.readFileSync(module.resource).toString()
-              const envVars = []
-              const regExp = /env(?:Flag|Url|Var)\(\s*["']([A-Z0-9_]+)["']/g
-              let match = null
-
-              while ((match = regExp.exec(content)) !== null) {
-                if (vars.indexOf(match[1]) < 0) {
-                  vars.push(match[1])
-                }
-              }
-            }
+          modules.filter(this.shouldProcessModule).forEach(module => {
+            const content = fs.readFileSync(module.resource).toString()
+            this.findMatchesInFile(content)
           })
         })
       })
