6da8e6df304267f67a250914c08b7b980ad1e597
jean-smaug <maximeblanc.dev@gmail.com>
2018-12-10 16:24:58 +0100

    Update InlineChunkHtmlPlugin.js

--- packages/react-dev-utils/InlineChunkHtmlPlugin.js
+++ packages/react-dev-utils/InlineChunkHtmlPlugin.js
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+'use strict';
+
 class InlineChunkHtmlPlugin {
   constructor(htmlWebpackPlugin, tests) {
     this.htmlWebpackPlugin = htmlWebpackPlugin;
