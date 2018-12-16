770a013748a318a9431a878c8188e99392aaad09
Martin Charles <martincharles07@gmail.com>
2018-12-16 00:20:04 -0500

    Fail Test to Test Something

--- packages/react-scripts/fixtures/kitchensink/src/features/webpack/WasmInclusion.js
+++ packages/react-scripts/fixtures/kitchensink/src/features/webpack/WasmInclusion.js
@@ -2,4 +2,4 @@ import React from 'react';
 import * as wasm from './assets/add.wasm';
 console.log(wasm);
 
-export default () => <span id="wasm-inclusion">{wasm.add(1, 10)}</span>;
+export default () => <span id="wasm-inclusion">{wasm.add(1, 9)}</span>;
