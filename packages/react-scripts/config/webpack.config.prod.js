6058ba31c83f1e70fb4619def62f4c1575125a14
Dusan Tuzinsky <brooklynPD@gmail.com>
2018-12-17 17:51:37 +0100

    Feature/add mdx config (#59)
    * fix bad naming of script files constant
    
    * add mdx config

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -202,7 +202,25 @@ module.exports = {
       // TODO: Disable require.ensure as it's not a standard language feature.
       // We are waiting for https://github.com/facebookincubator/create-react-app/issues/2176.
       // { parser: { requireEnsure: false } },
-
+      {
+        test: /\.mdx?$/,
+        use: [
+          { 
+            loader: require.resolve('babel-loader'),
+            options: {
+              // @remove-on-eject-begin
+              babelrc: false,
+              presets: [require.resolve('babel-preset-react-app')],
+              // @remove-on-eject-end
+              // This is a feature of `babel-loader` for webpack (not Babel itself).
+              // It enables caching results in ./node_modules/.cache/babel-loader/
+              // directory for faster rebuilds.
+              cacheDirectory: true,
+            },
+          },
+          { loader: require.resolve('@mdx-js/loader') }
+        ]
+      },
       // First, run the linter.
       // It's important to do this before Babel processes the JS.
       {
