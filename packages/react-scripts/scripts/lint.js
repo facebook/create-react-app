16588561699a78ccda16a501cce4e90a7c800a86
Diego Lara <dyegolara@gmail.com>
2018-12-19 14:26:20 -0600

    Set script for 'lint' command

--- packages/react-scripts/scripts/lint.js
+++ packages/react-scripts/scripts/lint.js
@@ -1,10 +1,23 @@
 const paths = require('../config/paths');
 const { exec } = require('child_process');
 
-const match = paths.appSrc + '/**/*.{js,jsx}';
-const prettierCommand = `prettier '${match}'`;
+const prettierMatch = paths.appSrc + '/**/*.{html,js,jsx,json,css,scss}';
+const prettierCommand = `prettier '${prettierMatch}' --write`;
+const eslintConfigPath = paths.ownPath + '/config/.eslintrc';
+const eslintCommand = `eslint --fix --config ${eslintConfigPath} --ext .jsx,.js src/`;
+
 
 exec(prettierCommand, (error, stdout, stderr) => {
+  if (error) {
+    console.log('Error: ' + error);
+    process.exit(1);
+  }
+  console.log(stdout);
+  console.log(stderr);
+  process.exit(0);
+});
+
+exec(eslintCommand, (error, stdout, stderr) => {
   if (error) {
     console.log('Error: ' + error);
     process.exit(1);
