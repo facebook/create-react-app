9b2444c7b35d22241a660c0c68e0c14294941dae
Adam Močkoř <adam.mockor@gmail.com>
2018-12-28 15:44:09 +0100

    Feat/styleguide docgen babel plugin (#64)
    * add react-docgen babel plugin
    
    * add `component` prop to ComponentDocs
    
    to display information about props from `__docgenInfo` object
    
    * add deprecation warning to `path` prop of ComponentDocs

--- packages/styleguide/config-overrides.js
+++ packages/styleguide/config-overrides.js
@@ -1,23 +1,36 @@
 const { getBabelLoader } = require('react-app-rewired');
 module.exports = (config, env) => {
-    const babelLoader = getBabelLoader(config.module.rules);
-    config.module.rules.map(rule => {
-        if (typeof rule.test !== 'undefined' || typeof rule.oneOf === 'undefined') {
-            return rule
-        }
+  const babelLoader = getBabelLoader(config.module.rules);
 
-        rule.oneOf.unshift({
-            test: /\.mdx?$/,
-            use: [
-                {
-                    loader: babelLoader.loader,
-                    options: babelLoader.options
-                },
-                '@mdx-js/loader'
-            ]
-        });
+  const docgenPlugin = [
+    'babel-plugin-react-docgen',
+    {
+      DOC_GEN_COLLECTION_NAME: 'MY_REACT_DOCS',
+    },
+  ];
 
-        return rule
+  babelLoader.options.plugins = [docgenPlugin].concat(
+    babelLoader.options.plugins || []
+  );
+
+  config.module.rules.map(rule => {
+    if (typeof rule.test !== 'undefined' || typeof rule.oneOf === 'undefined') {
+      return rule;
+    }
+
+    rule.oneOf.unshift({
+      test: /\.mdx?$/,
+      use: [
+        {
+          loader: babelLoader.loader,
+          options: babelLoader.options,
+        },
+        '@mdx-js/loader',
+      ],
     });
-    return config
+
+    return rule;
+  });
+
+  return config;
 };
