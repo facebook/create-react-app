5d67018bdaff4f68da1fff3f40e43e03a6f388a7
Robert van Steen <hello@rovansteen.nl>
2019-01-06 20:01:03 +0100

    Remove unused variable

--- packages/react-scripts/config/env.js
+++ packages/react-scripts/config/env.js
@@ -9,7 +9,6 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 const paths = require('./paths');
 
 // Make sure that including paths.js after env.js will read .env variables.
