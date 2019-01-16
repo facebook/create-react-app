34e5029c548f0fa08e1cd0a9865fc7c90914ff1e
Dotan Simha <dotansimha@gmail.com>
2019-01-16 04:26:24 -0500

    Add empty mock for module (#5798)

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -644,6 +644,7 @@ module.exports = function(webpackEnv) {
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
     node: {
+      module: 'empty',
       dgram: 'empty',
       fs: 'empty',
       net: 'empty',
