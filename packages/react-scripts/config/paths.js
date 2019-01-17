caf9378b327179e8e297df2f288d17ff26d6bf2e
NilsonBF <nilson.bertola@gmail.com>
2019-01-16 23:44:25 -0200

    npm run build Fix
    build/index.html not finding js and css files

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -42,7 +42,7 @@ const getPublicUrl = appPackageJson =>
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);
   const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : './');
   return ensureSlash(servedUrl, true);
 }
 
