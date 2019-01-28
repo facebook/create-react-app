21179b328265dce82c71349a976966c286129846
Charles Pritchard <chuck@jumis.com>
2019-01-28 10:53:54 -0800

    Add Node DNS module to empty mocks
    DNS is required by some popular node.js modules; presently a require statement for DNS will throw an error if it is not wrapped in a try catch block.

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -646,6 +646,7 @@ module.exports = function(webpackEnv) {
     node: {
       module: 'empty',
       dgram: 'empty',
+      dns: 'empty',
       fs: 'empty',
       net: 'empty',
       tls: 'empty',
