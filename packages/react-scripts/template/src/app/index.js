0fb47bf0cffaaf15fcfcf60e5d1b46f6bcec7a01
MihaiRo <mihai.rosca@thinslices.com>
2019-01-31 10:55:37 +0200

    Update template. Update scripts version.

--- packages/react-scripts/template/src/app/index.js
+++ packages/react-scripts/template/src/app/index.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
 
-class App extends Component {
+import 'assets/App.css';
+import icons from 'assets/icons';
+
+class Root extends PureComponent {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={icons.logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -25,4 +26,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Root;
--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import 'assets/index.css'
+import 'assets/index.css';
 
-import App from './app/App'
-import * as serviceWorker from './config/serviceWorker'
+import Root from './app';
+import * as serviceWorker from './config/serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
