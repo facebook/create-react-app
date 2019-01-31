d822111b32c72d7f1ee5e1b8449362e97c59687c
MihaiRo <mihai.rosca@thinslices.com>
2019-01-31 10:47:51 +0200

    Edit template and linting in files.

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -48,7 +48,7 @@ module.exports = {
 
   settings: {
     react: {
-      version: 'detect',
+      version: require('react').version,
     },
   },
 
--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.ivory.io"
             target="_blank"
             rel="noopener noreferrer"
           >
