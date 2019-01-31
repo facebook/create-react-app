9674d9fd1ecab1aa2391217ab642451679ea166f
MihaiRo <mihai.rosca@thinslices.com>
2019-01-31 14:02:44 +0200

    Fix invalid name of app in app entry. Fix missing import.

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,18 +1,20 @@
 import { render } from 'frint-react';
 import Amplify from 'aws-amplify';
 
+import awsConfig from './config/aws.config';
+import serviceWorker from './config/serviceWorker';
+
 import 'assets/index.css';
 import Root from './app';
-import awsConfig from './config/aws.config';
 
 const apps = window.app || [];
 const rootApp = (window.app = new Root());
-rootApp.push = options => app.registerApp(...options);
-apps.forEach(app.push);
+rootApp.push = options => rootApp.registerApp(...options);
+apps.forEach(rootApp.push);
 
 Amplify.configure(awsConfig);
 
-render(app, document.getElementById('root'));
+render(rootApp, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
