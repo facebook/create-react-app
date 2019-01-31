0cdb520e2e80e0378403d03651c7901628ca397f
Diego Lara <dyegolara@gmail.com>
2019-01-31 15:51:28 -0600

    Add yarn ci-lint support

--- packages/react-scripts/scripts/lint.js
+++ packages/react-scripts/scripts/lint.js
@@ -1,24 +1,30 @@
 const paths = require('../config/paths');
 const { exec } = require('child_process');
 
-const prettierMatch = paths.appSrc + '/**/*.{html,js,jsx,json,css,scss,mdx}';
-const prettierCommand = `prettier '${prettierMatch}' --write`;
+const isCI = process.env.CI === 'true'
+
+const prettierMatch = paths.appSrc + '/**/*.{html,json,css,scss,mdx}';
+const prettierCommand = `prettier '${prettierMatch}' ${isCI ? '--check' : '--write'}`;
+
 const eslintConfigPath = paths.ownPath + '/config/.eslintrc';
 const eslintIgnorePath = paths.ownPath + '/config/.eslintignore';
-const eslintCommand = `eslint --fix --config ${eslintConfigPath} --ignore-path ${eslintIgnorePath} --ext .jsx,.js src/`;
+const eslintCommand = `eslint ${isCI ? '' : '--fix'} --config ${eslintConfigPath} --ignore-path ${eslintIgnorePath} --ext .jsx,.js src/`;
 
 exec(prettierCommand, (error, stdout, stderr) => {
   if (error) {
+    console.log(stdout);
+    console.log(stderr);
     console.log('Error: ' + error);
     process.exit(1);
   }
   console.log(stdout);
   console.log(stderr);
-  process.exit(0);
 });
 
 exec(eslintCommand, (error, stdout, stderr) => {
   if (error) {
+    console.log(stdout);
+    console.log(stderr);
     console.log('Error: ' + error);
     process.exit(1);
   }
