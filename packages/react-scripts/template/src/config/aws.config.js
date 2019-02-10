6e06977db4eb15016fcd29404a6ace6a3852fa6f
Bogdan Balan <bogdan.balan@thinslices.com>
2019-02-10 14:43:50 +0200

    Change Amplify placeholder values
    Signed-off-by: Bogdan Balan <bogdan.balan@thinslices.com>

--- packages/react-scripts/template/src/config/aws.config.js
+++ packages/react-scripts/template/src/config/aws.config.js
@@ -2,10 +2,10 @@ import Amplify, { Auth } from 'aws-amplify';
 
 export default Amplify.configure({
   Auth: {
-    identityPoolId: '[AWS_COGNITO_IDENTITY_POOL_ID]', // REQUIRED - Amazon Cognito Identity Pool ID
-    region: '[AWS_COGNITO_REGION]', // REQUIRED - Amazon Cognito Region
-    userPoolId: '[AWS_COGNITO_USER_POOL_ID]', // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolWebClientId: '[AWS_COGNITO_WEB_CLIENT_ID]', // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    identityPoolId: 'AWS_COGNITO_IDENTITY_POOL_ID',
+    region: 'AWS_COGNITO_REGION',
+    userPoolId: 'AWS_COGNITO_USER_POOL_ID',
+    userPoolWebClientId: 'AWS_COGNITO_WEB_CLIENT_ID',
   },
   API: {
     endpoints: [
