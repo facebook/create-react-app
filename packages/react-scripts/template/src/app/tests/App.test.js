11fb11e95d3ea97e69f92ae9115d95e06e2b422b
MihaiRo <mihai.rosca@thinslices.com>
2019-02-12 12:00:31 +0200

    Update init test.

--- packages/react-scripts/template/src/app/tests/App.test.js
+++ packages/react-scripts/template/src/app/tests/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from '../RootEntry';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
+  const div = document.createElement('div');
+  ReactDOM.render(<Root />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
