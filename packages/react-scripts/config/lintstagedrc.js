c2b3dbc9a857100deabae86cf102e766bd6f4392
Diego Lara <dyegolara@gmail.com>
2019-02-20 11:45:59 -0600

    Add configurations for stylelint

--- packages/react-scripts/config/lintstagedrc.js
+++ packages/react-scripts/config/lintstagedrc.js
@@ -2,17 +2,23 @@ const paths = require('../config/paths');
 
 const eslintConfigPath = paths.ownPath + '/config/.eslintrc';
 const eslintIgnorePath = paths.ownPath + '/config/.eslintignore';
+const stylelintConfigPath = paths.ownPath + '/config/.stylelintrc';
 
 module.exports = {
   concurrent: false,
   linters: {
     './**/*.{js,jsx}': [
       `eslint --fix --config ${eslintConfigPath} --ignore-path ${eslintIgnorePath}`,
-      'git add'
+      'git add',
     ],
-    './**/*.{html,json,css,scss,mdx}': [
-      'prettier --write --end-of-line lf',
-      'git add'
+    './**/*.{html,json,mdx}': ['prettier --write --end-of-line lf', 'git add'],
+    './**/*.scss': [
+      `stylelint --config ${stylelintConfigPath} --fix`,
+      'git add',
+    ],
+    './**/*.css': [
+      `stylelint --config ${stylelintConfigPath} --fix`,
+      'git add',
     ],
   },
 };
--- packages/react-scripts/scripts/lint.js
+++ packages/react-scripts/scripts/lint.js
@@ -1,16 +1,53 @@
+/* eslint-disable */
 const paths = require('../config/paths');
 const { exec } = require('child_process');
 
-const isCI = process.env.CI === 'true'
+const isCI = process.env.CI === 'true';
 
-const prettierMatch = paths.appSrc + '/**/*.{html,json,css,scss,mdx}';
-const prettierCommand = `prettier '${prettierMatch}' ${isCI ? '--check' : '--write'} --end-of-line lf`;
+const prettierMatch = paths.appSrc + '/**/*.{html,json,mdx}';
+const prettierCommand = `prettier '${prettierMatch}' ${
+  isCI ? '--check' : '--write'
+} --end-of-line lf`;
 
 const eslintConfigPath = paths.ownPath + '/config/.eslintrc';
 const eslintIgnorePath = paths.ownPath + '/config/.eslintignore';
-const eslintCommand = `eslint ${isCI ? '' : '--fix'} --config ${eslintConfigPath} --ignore-path ${eslintIgnorePath} --ext .jsx,.js src/`;
+const eslintCommand = `eslint ${
+  isCI ? '' : '--fix'
+} --config ${eslintConfigPath} --ignore-path ${eslintIgnorePath} --ext .jsx,.js src/`;
+
+const stylelintMatchSass = paths.appSrc + '/**/*.scss';
+const stylelintMatchCss = paths.appSrc + '/**/*.css';
+const stylelintConfigPath = paths.ownPath + '/config/.stylelintrc';
+const stylelintCommandSass = `stylelint ${stylelintMatchSass} --config ${stylelintConfigPath} ${
+  isCI ? '' : '--fix'
+}`;
+const stylelintCommandCss = `stylelint ${stylelintMatchCss} --config ${stylelintConfigPath} ${
+  isCI ? '' : '--fix'
+}`;
 
 exec(prettierCommand, (error, stdout, stderr) => {
+  if (error) {
+    console.log(stdout);
+    console.log(stderr);
+    console.log('Error: ' + error);
+    process.exit(1);
+  }
+  console.log(stdout);
+  console.log(stderr);
+});
+
+exec(stylelintCommandSass, (error, stdout, stderr) => {
+  if (error) {
+    console.log(stdout);
+    console.log(stderr);
+    console.log('Error: ' + error);
+    process.exit(1);
+  }
+  console.log(stdout);
+  console.log(stderr);
+});
+
+exec(stylelintCommandCss, (error, stdout, stderr) => {
   if (error) {
     console.log(stdout);
     console.log(stderr);
