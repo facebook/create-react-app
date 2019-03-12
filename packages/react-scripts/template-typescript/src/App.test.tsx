1bb8457f85fd4d5c8cc919225cded17d068f367a
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2019-03-12 14:33:18 +0800

    change default import into named import

--- packages/react-scripts/template-typescript/src/App.test.tsx
+++ packages/react-scripts/template-typescript/src/App.test.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
--- packages/react-scripts/template-typescript/src/index.tsx
+++ packages/react-scripts/template-typescript/src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
