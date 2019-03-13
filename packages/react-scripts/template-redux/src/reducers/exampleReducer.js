be7cf28cd14e6c7cecd7b0438744adbb262c4671
Joey Cozza <joeycozza@gmail.com>
2019-03-12 19:46:45 -0600

    removing the redux template

--- packages/react-scripts/template-redux/src/reducers/exampleReducer.js
+++
@@ -1,8 +0,0 @@
-export default function reduceTimestamp(state = {}, action) {
-  switch (action.type) {
-    case 'UPDATE_TIMESTAMP':
-      return { ...state, timestamp: Date.now() };
-    default:
-      return state;
-  }
-}
