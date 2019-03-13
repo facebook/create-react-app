52f21d1b06102ea36d47d28c7c9161effb1b3dd8
Joey Cozza <joeycozza@gmail.com>
2019-03-12 18:23:33 -0600

    fixed eslint issue and turned off lint-staged in the base of the repo

--- packages/react-scripts/template/src/setupTests.js
+++ packages/react-scripts/template/src/setupTests.js
@@ -1,5 +1,5 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { cleanup } from '@fs/testing-library';
-import 'jest-dom/extend-expect';
+/* eslint-disable import/no-extraneous-dependencies */
+import { cleanup } from '@fs/testing-library'
+import 'jest-dom/extend-expect'
 
-afterEach(cleanup);
+afterEach(cleanup)
