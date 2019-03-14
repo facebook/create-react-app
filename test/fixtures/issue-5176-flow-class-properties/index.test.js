e7b0f48d3559ebf2c117416c38f8c1a438f762c0
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2019-03-14 10:28:47 +0100

    Remove console.log

--- test/fixtures/issue-5176-flow-class-properties/index.test.js
+++ test/fixtures/issue-5176-flow-class-properties/index.test.js
@@ -1,9 +1,8 @@
 const testSetup = require('../__shared__/test-setup');
 
 test('passes tests', async () => {
-  const { fulfilled, ...rest } = await testSetup.scripts.test({
+  const { fulfilled } = await testSetup.scripts.test({
     jestEnvironment: 'node',
   });
-  console.log(rest);
   expect(fulfilled).toBe(true);
 });
