99a45bad8f236f4011f7a13df0b93d47b343b493
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2019-03-14 10:07:24 +0100

    Debug failing test

--- test/fixtures/issue-5176-flow-class-properties/index.test.js
+++ test/fixtures/issue-5176-flow-class-properties/index.test.js
@@ -1,8 +1,9 @@
 const testSetup = require('../__shared__/test-setup');
 
 test('passes tests', async () => {
-  const { fulfilled } = await testSetup.scripts.test({
+  const { fulfilled, ...rest } = await testSetup.scripts.test({
     jestEnvironment: 'node',
   });
+  console.log(rest);
   expect(fulfilled).toBe(true);
 });
