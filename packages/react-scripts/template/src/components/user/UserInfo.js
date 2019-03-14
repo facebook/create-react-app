7d9f1e8a5cc15feb487eef8455c7eb8829865939
Joey Cozza <joeycozza@gmail.com>
2019-03-14 10:07:33 -0600

    running prettier on a file

--- packages/react-scripts/template/src/components/user/UserInfo.js
+++ packages/react-scripts/template/src/components/user/UserInfo.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import {useUser} from '@fs/user';
+import React from 'react'
+import { useUser } from '@fs/user'
 
-export default function UserInfo(){
-  const user = useUser();
-  return (<div>Hello, {user.displayName}!</div>)
+export default function UserInfo() {
+  const user = useUser()
+  return <div>Hello, {user.displayName}!</div>
 }
