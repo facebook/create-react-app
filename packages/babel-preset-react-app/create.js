f4f20a20d095c7ea97191292e9d17753ff24596a
Ian Schmitz <ianschmitz@gmail.com>
2019-03-14 12:16:09 -0700

    Support browserslist in @babel/preset-env (#6608)
    * Add browserslist support to @babel/preset-env
    
    * Support @babel/polyfill in entry point

--- packages/babel-preset-react-app/create.js
+++ packages/babel-preset-react-app/create.js
@@ -79,17 +79,8 @@ module.exports = function(api, opts, env) {
         // Latest stable ECMAScript features
         require('@babel/preset-env').default,
         {
-          // We want Create React App to be IE 9 compatible until React itself
-          // no longer works with IE 9
-          targets: {
-            ie: 9,
-          },
-          // Users cannot override this behavior because this Babel
-          // configuration is highly tuned for ES5 support
-          ignoreBrowserslistConfig: true,
-          // If users import all core-js they're probably not concerned with
-          // bundle size. We shouldn't rely on magic to try and shrink it.
-          useBuiltIns: false,
+          // Allow importing @babel/polyfill in entrypoint and use browserlist to select polyfills
+          useBuiltIns: 'entry',
           // Do not transform modules to CJS
           modules: false,
           // Exclude transforms that make all code slower
