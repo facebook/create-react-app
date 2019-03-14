eebfe1d880b797ff88dc634a72957a760520f7b9
Joey Cozza <joeycozza@gmail.com>
2019-03-14 16:44:19 -0600

    adding react and react-dom to the webpack.config.js alias section

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -274,6 +274,8 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
+        react: require.resolve('react'),
+        'react-dom': require.resolve('react-dom'),
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
