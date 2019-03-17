435d13b1c0aea7d9033ed278f3b9e165a7f7c94e
Ian Sutherland <ian@iansutherland.ca>
2019-03-17 11:14:13 -0600

    Change TypeScript template back to an arrow function

--- packages/react-scripts/template-typescript/src/App.tsx
+++ packages/react-scripts/template-typescript/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App(): React.FC {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
