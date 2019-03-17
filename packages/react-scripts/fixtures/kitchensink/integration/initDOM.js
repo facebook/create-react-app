0bb3e3ebd4dca61b6983f93221cd7db3afa3c632
Ian Schmitz <ianschmitz@gmail.com>
2019-03-16 22:29:41 -0700

    WIP

--- packages/react-scripts/fixtures/kitchensink/integration/initDOM.js
+++ packages/react-scripts/fixtures/kitchensink/integration/initDOM.js
@@ -6,43 +6,12 @@
  */
 
 const fs = require('fs');
-const http = require('http');
-const { JSDOM, ResourceLoader, VirtualConsole } = require('jsdom');
+const { JSDOM, ResourceLoader } = require('jsdom');
 const path = require('path');
 
-let getMarkup;
-export let resourceLoader;
+// export let resourceLoader;
 
-if (process.env.E2E_FILE) {
-  const file = path.isAbsolute(process.env.E2E_FILE)
-    ? process.env.E2E_FILE
-    : path.join(process.cwd(), process.env.E2E_FILE);
-
-  const markup = fs.readFileSync(file, 'utf8');
-  getMarkup = () => markup;
-
-  const pathPrefix = process.env.PUBLIC_URL.replace(/^https?:\/\/[^/]+\/?/, '');
-
-  resouceLoader = new class FileResourceLoader extends ResourceLoader {
-    fetch(url, options) {
-      return Promise.resolve(
-        fs.readFileSync(
-          path.join(path.dirname(file), url.pathname.replace(pathPrefix, '')),
-          'utf8'
-        )
-      );
-    }
-  }();
-} else if (process.env.E2E_URL) {
-  getMarkup = () =>
-    new Promise(resolve => {
-      http.get(process.env.E2E_URL, res => {
-        let rawData = '';
-        res.on('data', chunk => (rawData += chunk));
-        res.on('end', () => resolve(rawData));
-      });
-    });
-} else {
+if (!process.env.E2E_FILE && !process.env.E2E_URL) {
   it.only('can run jsdom (at least one of "E2E_FILE" or "E2E_URL" environment variables must be provided)', () => {
     expect(
       new Error("This isn't the error you are looking for.")
@@ -53,29 +22,50 @@ if (process.env.E2E_FILE) {
 export default feature =>
   new Promise(async (resolve, reject) => {
     try {
-      // const markup = await getMarkup();
       const host = process.env.E2E_URL || 'http://www.example.org/spa:3000';
-
       const url = `${host}#${feature}`;
 
-      const { window } = await JSDOM.fromURL(url, {
-        pretendToBeVisual: true,
-        resources: 'usable',
-        runScripts: 'dangerously',
-      });
+      let window;
+
+      if (process.env.E2E_FILE) {
+        const file = path.isAbsolute(process.env.E2E_FILE)
+          ? process.env.E2E_FILE
+          : path.join(process.cwd(), process.env.E2E_FILE);
+
+        const pathPrefix = process.env.PUBLIC_URL.replace(
+          /^https?:\/\/[^/]+\/?/,
+          ''
+        );
+
+        const resourceLoader = new class FileResourceLoader extends ResourceLoader {
+          fetch(url, options) {
+            return Promise.resolve(
+              fs.readFileSync(
+                path.join(
+                  path.dirname(file),
+                  url.pathname.replace(pathPrefix, '')
+                ),
+                'utf8'
+              )
+            );
+          }
+        }();
+
+        window = (await JSDOM.fromFile(file, {
+          pretendToBeVisual: true,
+          resources: resourceLoader,
+          runScripts: 'dangerously',
+          url,
+        })).window;
+      } else {
+        window = (await JSDOM.fromURL(url, {
+          pretendToBeVisual: true,
+          resources: 'usable',
+          runScripts: 'dangerously',
+        })).window;
+      }
 
       const { document } = window;
-
-      // console.log(document);
-      // console.log(window);
-
-      // const { document } = new JSDOM(markup, {
-      //   pretendToBeVisual: true,
-      //   resourceLoader,
-      //   resources: 'usable',
-      //   runScripts: 'dangerously',
-      //   url,
-      // }).window;
 
       document.addEventListener(
         'ReactFeatureDidMount',
