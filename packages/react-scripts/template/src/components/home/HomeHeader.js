f58058c0f783f060a90c3a7efcc308e52c2a06e3
Logan Allred <redbugz@gmail.com>
2019-03-18 00:43:25 -0600

    Release 1.7.2 (#83)
    * tweak home page style (#81)
    * remove bg color
    * App styleguidist update and routing fixes (#82)
    * Update styleguidist config
    Fix routing when deployed
    * Release 1.7.2

--- packages/react-scripts/template/src/components/home/HomeHeader.js
+++ packages/react-scripts/template/src/components/home/HomeHeader.js
@@ -2,14 +2,11 @@ import React from 'react'
 import { css } from '@emotion/core'
 
 const styles = css`
-  background-color: #282c34;
-  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   font-size: calc(10px + 2vmin);
-  color: white;
 `
 
 const HomeHeader = ({ children }) => <header css={styles}>{children}</header>
--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom'
 import { UserProvider } from '@fs/zion-user'
 import RootErrorBoundary from '@fs/zion-error-boundary'
 import { I18nProvider, addTranslations } from '@fs/zion-locale'
+import { Router, NotFound } from '@fs/zion-router'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import translations from './locales'
 
 addTranslations(translations)
 
+const base = window.FS ? new URL(window.FS.appPath).pathname : ''
+
 const FrontierRoot = () => (
   <RootErrorBoundary>
     <Suspense fallback="Loading ...">
       <I18nProvider>
         <UserProvider>
-          <App />
+          <Router>
+            <App path={`${base}/*`} />
+            <NotFound default />
+          </Router>
         </UserProvider>
       </I18nProvider>
     </Suspense>
