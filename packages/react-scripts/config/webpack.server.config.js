d66a81a00bcc46eca3bf7838b24e9dfa9494287a
bitttttten <bitttttten@googlemail.com>
2019-03-27 14:08:00 +0100

    use url loader

--- packages/react-scripts/config/webpack.server.config.js
+++ packages/react-scripts/config/webpack.server.config.js
@@ -407,7 +407,7 @@ module.exports = function(webpackEnv) {
             // This loader doesn't use a "test" so it will catch all modules
             // that fall through the other loaders.
             {
-              loader: require.resolve('file-loader'),
+              loader: require.resolve('url-loader'),
               // Exclude `js` files to keep "css" loader working as it injects
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
@@ -415,6 +415,7 @@ module.exports = function(webpackEnv) {
               exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
               options: {
                 name: 'static/media/[name].[hash:8].[ext]',
+                limit: 8192,
               },
             },
             // ** STOP ** Are you adding a new loader?
