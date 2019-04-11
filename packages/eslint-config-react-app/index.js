a5058d609606be6643b9d4ba8a3f630e02b537d8
Matt Darveniza <matt.darveniza@biarri.com>
2019-04-11 10:26:33 +1000

    clarified comment

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -104,7 +104,7 @@ module.exports = {
         'no-restricted-globals': ['error'].concat(
           restrictedGlobals.filter(g => g !== 'self')
         ),
-        // Necessary to allow stubbed class declartions in workers
+        // Necessary to allow stubbed class declartions in typescript workers
         'no-useless-constructor': 'off',
       },
     },
