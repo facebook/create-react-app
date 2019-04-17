1f39be714fcf0e2de0bfb268efecbb6205d3da83
bitttttten <bitttttten@googlemail.com>
2019-04-17 08:56:41 +0200

    support webp in sharp loader

--- packages/react-scripts/config/webpack.server.config.js
+++ packages/react-scripts/config/webpack.server.config.js
@@ -229,7 +229,7 @@ module.exports = function(webpackEnv) {
           oneOf: [
             // https://www.npmjs.com/package/responsive-loader
             {
-              test: [/\.jpe?g$/, /\.png$/],
+              test: [/\.jpe?g$/, /\.webp/, /\.png$/],
               loader: 'responsive-loader',
               options: {
                 adapter: require('responsive-loader/sharp'),
