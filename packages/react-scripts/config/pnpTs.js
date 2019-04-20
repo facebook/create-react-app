7187048c5dd470deda8232e378af86b49a38af52
Maël Nison <mael@fb.com>
2019-04-20 12:35:03 +0100

    Adds the header

--- packages/react-scripts/config/pnpTs.js
+++ packages/react-scripts/config/pnpTs.js
@@ -1,3 +1,13 @@
+// @remove-on-eject-begin
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+// @remove-on-eject-end
+'use strict';
+
 const { resolveModuleName } = require('ts-pnp');
 
 exports.resolveModuleName = (
