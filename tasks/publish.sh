a3b66292a6ffc6051bcfd4521039a44d1e41313f
Will Smythe <wismythe@microsoft.com>
2019-04-29 12:41:05 -0400

    Remove --loglevel from lerna publish; used for debugging

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -49,4 +49,4 @@ if [ -z $CI ]; then
 fi
 
 # Go!
-NPM_CONFIG_OTP="$otp" ./node_modules/.bin/lerna publish --npm-client=npm "$@" --loglevel verbose
+NPM_CONFIG_OTP="$otp" ./node_modules/.bin/lerna publish --npm-client=npm "$@"
