875ae1e3aa4956c3847d21a151b580dffccac43f
Tyler Graf <tylergraf@gmail.com>
2019-05-07 15:24:20 -0600

    Move default Suspense into Root.

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,30 +1,30 @@
-import React, { Suspense } from 'react'
-import ReactDOM from 'react-dom'
-import Root from '@fs/zion-root'
-import { addTranslations } from '@fs/zion-locale'
-import { Router, NotFound } from '@fs/zion-router'
-import App from './components/App'
-import * as serviceWorker from './serviceWorker'
-import translations from './locales'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from '@fs/zion-root';
+import { addTranslations } from '@fs/zion-locale';
+import { Router, NotFound } from '@fs/zion-router';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import translations from './locales';
 
-addTranslations(translations)
+addTranslations(translations);
 
-const base = window.SERVER_DATA ? new URL(window.SERVER_DATA.appPath).pathname : ''
+const base = window.SERVER_DATA
+  ? new URL(window.SERVER_DATA.appPath).pathname
+  : '';
 
 const FrontierRoot = () => (
   <Root>
-    <Suspense fallback="Loading ...">
-      <Router>
-        <App path={`${base}/*`} />
-        <NotFound default />
-      </Router>
-    </Suspense>
+    <Router>
+      <App path={`${base}/*`} />
+      <NotFound default />
+    </Router>
   </Root>
-)
+);
 
-ReactDOM.render(<FrontierRoot />, document.getElementById('root'))
+ReactDOM.render(<FrontierRoot />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
