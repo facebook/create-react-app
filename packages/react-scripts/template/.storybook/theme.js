be7054ce046076fff894cf84bc84fa21dba3d565
Joey Cozza <joeycozza@gmail.com>
2019-05-16 09:23:06 -0600

    storybook now is on par with zion

---
+++ packages/react-scripts/template/.storybook/theme.js
@@ -0,0 +1,37 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+
+  colorPrimary: '#27C4F4',
+  colorSecondary: '#87B940',
+
+  // UI
+  // appBg: 'white',
+  // appContentBg: 'white',
+  // appBorderColor: 'grey',
+  // appBorderRadius: 4,
+
+  // Typography
+  fontBase: 'verdana, sans-serif',
+  fontCode: 'monospace',
+
+  // Text colors
+  // textColor: 'black',
+  // textInverseColor: 'rgba(255,255,255,0.9)',
+
+  // Toolbar default and active colors
+  // barTextColor: 'silver',
+  // barSelectedColor: 'black',
+  // barBg: 'hotpink',
+
+  // Form colors
+  // inputBg: 'white',
+  // inputBorder: 'silver',
+  // inputTextColor: 'black',
+  // inputBorderRadius: 4,
+
+  brandTitle: 'New React App',
+  brandImage: null,
+  // brandImage: 'https://edge.fscdn.org/assets/components/hf/assets/img/logos/tree-logotype-2x-750ccbd315132e50d07ad40c8e639a34.png',
+})
