deb21bcbcd377bc915b1d79d9d6a4d9f6d28f3b0
Matthew Larson <matthew.larson@familysearch.org>
2019-05-23 09:58:36 -0600

    card fixes

--- packages/react-scripts/template/src/components/example/NotSignedInCard.js
+++ packages/react-scripts/template/src/components/example/NotSignedInCard.js
@@ -1,20 +1,9 @@
 import React from 'react'
-import { css } from '@emotion/core'
 import { Button, Card, CardActions, CardContent } from '@fs/zion-ui'
 
-const cardStyle = css`
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
-`
-
-const cardContentStyle = css`
-  flex-grow: 1;
-`
-
 const NotSignedInCard = ({ userLoading }) => (
-  <Card css={cardStyle}>
-    <CardContent css={cardContentStyle}>
+  <Card>
+    <CardContent>
       {userLoading ? <p>User loading ...</p> : <p>You must sign in to see this content</p>}
     </CardContent>
     <CardActions>
