0ecbcc28c432cc3b7aec9e3508eff0b218728771
vadzappa <Vadzim Zapolski>
2019-05-29 00:40:11 +0300

    modified main entry point to render only when executed

--- packages/react-scripts/template-typescript/src/index.tsx
+++ packages/react-scripts/template-typescript/src/index.tsx
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export default function ({element}) {
+  if (element) {
+    ReactDOM.render(<App />, element);
+  } else {
+    console.error('Cant render to nowhere');
+  }
+};
