093b7a58f27f27b481d8fcee663bdbdada91336b
Avishay Maor <avishay@guesty.com>
2019-05-30 13:52:23 +0300

    Test version 2

--- packages/react-scripts/config/env.js
+++ packages/react-scripts/config/env.js
@@ -80,6 +80,8 @@ function getClientEnvironment(publicUrl) {
         // Useful for determining whether we’re running in production mode.
         // Most importantly, it switches React into the correct mode.
         NODE_ENV: process.env.NODE_ENV || 'development',
+        GUESTY_ENV: process.env.GUESTY_ENV || 'development',
+        CLUSTER: process.env.CLUSTER || 'development',
         // Useful for resolving the correct path to static assets in `public`.
         // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
         // This should only be used as an escape hatch. Normally you would put
