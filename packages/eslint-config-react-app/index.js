5c15642ddea9f77d28c3d2137e235b2dc15dec62
Ivan Aksamentov <i.aksamentov@smartreporting.de>
2019-06-14 17:56:58 +0200

    fix: eslintrc overrides key should be an array, not object
    Array format for `overrides` key in eslint config file is now enforced in ESLint v6. Anyone who uses `eslint-config-react-app` and ESLint v6 gets a type mismatch error. This commit fixes the error by converting the object into a one-element array containing this object.
    Only 2 characters are added: `[` and `]`. The large diff is due to formatting.

--- packages/eslint-config-react-app/index.js
+++ packages/eslint-config-react-app/index.js
@@ -52,57 +52,59 @@ module.exports = {
     },
   },
 
-  overrides: {
-    files: ['**/*.ts', '**/*.tsx'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        // typescript-eslint specific options
+        warnOnUnsupportedTypeScriptVersion: true,
       },
+      plugins: ['@typescript-eslint'],
+      // If adding a typescript-eslint version of an existing ESLint rule,
+      // make sure to disable the ESLint rule here.
+      rules: {
+        // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
+        'default-case': 'off',
+        // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
+        'no-dupe-class-members': 'off',
+        // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
+        'no-undef': 'off',
 
-      // typescript-eslint specific options
-      warnOnUnsupportedTypeScriptVersion: true,
+        // Add TypeScript specific rules (and turn off ESLint equivalents)
+        '@typescript-eslint/no-angle-bracket-type-assertion': 'warn',
+        'no-array-constructor': 'off',
+        '@typescript-eslint/no-array-constructor': 'warn',
+        '@typescript-eslint/no-namespace': 'error',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': [
+          'warn',
+          {
+            functions: false,
+            classes: false,
+            variables: false,
+            typedefs: false,
+          },
+        ],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            args: 'none',
+            ignoreRestSiblings: true,
+          },
+        ],
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'warn',
+      },
     },
-    plugins: ['@typescript-eslint'],
-    // If adding a typescript-eslint version of an existing ESLint rule,
-    // make sure to disable the ESLint rule here.
-    rules: {
-      // TypeScript's `noFallthroughCasesInSwitch` option is more robust (#6906)
-      'default-case': 'off',
-      // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/291)
-      'no-dupe-class-members': 'off',
-      // 'tsc' already handles this (https://github.com/typescript-eslint/typescript-eslint/issues/477)
-      'no-undef': 'off',
-
-      // Add TypeScript specific rules (and turn off ESLint equivalents)
-      '@typescript-eslint/no-angle-bracket-type-assertion': 'warn',
-      'no-array-constructor': 'off',
-      '@typescript-eslint/no-array-constructor': 'warn',
-      '@typescript-eslint/no-namespace': 'error',
-      'no-use-before-define': 'off',
-      '@typescript-eslint/no-use-before-define': [
-        'warn',
-        {
-          functions: false,
-          classes: false,
-          variables: false,
-          typedefs: false,
-        },
-      ],
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          args: 'none',
-          ignoreRestSiblings: true,
-        },
-      ],
-      'no-useless-constructor': 'off',
-      '@typescript-eslint/no-useless-constructor': 'warn',
-    },
-  },
+  ],
 
   // NOTE: When adding rules here, you need to make sure they are compatible with
   // `typescript-eslint`, as some rules such as `no-array-constructor` aren't compatible.
