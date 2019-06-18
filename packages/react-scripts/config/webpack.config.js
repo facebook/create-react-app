124667789f7fd1a90008b42930c6b97f04ec9252
James K Nelson <james@jamesknelson.com>
2019-06-18 18:49:04 +0900

    only produce a single initial chunk in node env

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -281,7 +281,7 @@ module.exports = function(webpackEnv, executionEnv) {
       // https://twitter.com/wSokra/status/969633336732905474
       // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
       splitChunks: {
-        chunks: 'all',
+        chunks: isEnvNode ? 'async' : 'all',
         name: false,
       },
       // Keep the runtime chunk separated to enable long term caching,
