914c95e87facb7ca5debf5bd5514f211807f71bf
Andrew Luca <aluca@winify.com>
2019-08-06 06:28:09 +0300

    Resolve relative paths for preprocessor styles (#5829)
    preprocessor will output sourceMap by default
    then check if sourceMaps are needed on resolve-url-loader
    
    Fixes #4653

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -124,12 +124,20 @@ module.exports = function(webpackEnv) {
       },
     ].filter(Boolean);
     if (preProcessor) {
-      loaders.push({
-        loader: require.resolve(preProcessor),
-        options: {
-          sourceMap: isEnvProduction && shouldUseSourceMap,
+      loaders.push(
+        {
+          loader: require.resolve('resolve-url-loader'),
+          options: {
+            sourceMap: isEnvProduction && shouldUseSourceMap,
+          },
         },
-      });
+        {
+          loader: require.resolve(preProcessor),
+          options: {
+            sourceMap: true,
+          },
+        }
+      );
     }
     return loaders;
   };
