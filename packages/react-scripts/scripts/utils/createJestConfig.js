ae671dc9fe974b86748f7a90ec4985448ff371bb
Igor Muchychka <mucsi96@gmail.com>
2019-08-07 05:42:57 +0200

    Specify roots in Jest config (#7480)

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -23,6 +23,8 @@ module.exports = (resolve, rootDir, isEjecting) => {
     : undefined;
 
   const config = {
+    roots: ['<rootDir>/src'],
+
     collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
 
     setupFiles: [
