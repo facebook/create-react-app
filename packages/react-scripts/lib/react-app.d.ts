f5b0c92660494c0efc6097c97491495ee30c108f
Phil Tobias <ptobias@digitalocean.com>
2019-08-09 12:26:06 -0700

    add dependencies, walrus, typekit, icons, App.tsx

--- packages/react-scripts/lib/react-app.d.ts
+++ packages/react-scripts/lib/react-app.d.ts
@@ -35,14 +35,16 @@ declare module '*.png' {
 }
 
 declare module '*.webp' {
-    const src: string;
-    export default src;
+  const src: string;
+  export default src;
 }
 
 declare module '*.svg' {
   import * as React from 'react';
 
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
 
   const src: string;
   export default src;
@@ -61,4 +63,9 @@ declare module '*.module.scss' {
 declare module '*.module.sass' {
   const classes: { readonly [key: string]: string };
   export default classes;
+}
+
+declare module '*.scss' {
+  const content: any;
+  export default content;
 }
--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,25 +1,129 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import FlexboxGrid from 'walrus/build/components/FlexboxGrid';
+import Typekit from 'walrus/build/components/Typekit';
+import SpinnerButton from 'walrus/build/components/SpinnerButton';
+import FloatLabel from 'walrus/build/components/FloatLabel';
+import styled from 'styled-components';
+import colors from 'walrus/build/scss/core/_colors.scss';
+import LoadingState from 'walrus/build/components/states/Loading';
+
+const ExamplePage = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+const ExampleGridItem = styled.div`
+  background: ${colors.primary};
+  min-height: 3rem;
+`;
+
+const LoadingWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const NameSchema = Yup.object().shape({
+  name: Yup.string().required('A value is required!'),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Typekit />
+      <ExamplePage>
+        <FlexboxGrid>
+          <FlexboxGrid.Col sm={12} lg={12}>
+            <LoadingWrap>
+              <LoadingState sammy={true} />
+            </LoadingWrap>
+          </FlexboxGrid.Col>
+          <FlexboxGrid.Col sm={12} lg={12}>
+            <div
+              style={{
+                width: '600px',
+                margin: '100px auto',
+                padding: '2em',
+                background: `${colors.primary_bg}`,
+              }}
+            >
+              <Formik
+                initialValues={{
+                  name: '',
+                }}
+                validationSchema={NameSchema}
+                onSubmit={(values, actions) => {
+                  actions.setSubmitting(true);
+                  setTimeout(() => {
+                    actions.setSubmitting(false);
+                  }, 500);
+                }}
+                render={({
+                  errors,
+                  touched,
+                  values,
+                  isSubmitting,
+                  isValid,
+                }) => (
+                  <Form>
+                    <FloatLabel
+                      placeholder="Enter a name"
+                      required={true}
+                      name="name"
+                      label="Name"
+                      error={errors.name}
+                      touched={touched.name}
+                      value={values.name}
+                      autoComplete={'off'}
+                    />
+
+                    <SpinnerButton
+                      className="do-u-mt--medium"
+                      type="submit"
+                      primary={true}
+                      fullWidth={true}
+                      disabled={!isValid || isSubmitting}
+                      spinning={isSubmitting}
+                    >
+                      Submit
+                    </SpinnerButton>
+                  </Form>
+                )}
+              />
+            </div>
+          </FlexboxGrid.Col>
+          <FlexboxGrid.Col sm={12} md={6} lg={4}>
+            <ExampleGridItem />
+          </FlexboxGrid.Col>
+          <FlexboxGrid.Col sm={12} md={6} lg={8}>
+            <ExampleGridItem />
+          </FlexboxGrid.Col>
+          <FlexboxGrid.Col sm={12} md={6} lg={7}>
+            <ExampleGridItem />
+          </FlexboxGrid.Col>
+          <FlexboxGrid.Col sm={12} md={6} lg={5}>
+            <ExampleGridItem />
+          </FlexboxGrid.Col>
+          <FlexboxGrid.Col sm={6} md={4} lg={2}>
+            <ExampleGridItem />
+          </FlexboxGrid.Col>
+          <FlexboxGrid.Col sm={6} md={8} lg={10}>
+            <ExampleGridItem />
+          </FlexboxGrid.Col>
+          <FlexboxGrid.Col sm={12} md={6} lg={5}>
+            <ExampleGridItem />
+          </FlexboxGrid.Col>
+          <FlexboxGrid.Col sm={12} md={6} lg={7}>
+            <ExampleGridItem />
+          </FlexboxGrid.Col>
+        </FlexboxGrid>
+      </ExamplePage>
+    </>
   );
 }
 
