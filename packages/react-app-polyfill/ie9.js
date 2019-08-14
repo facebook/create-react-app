13da0fc1c1238e894b81889e5009af3ae791b581
Jake Moxey <jakemoxey@gmail.com>
2019-08-14 14:56:46 +1000

    Update ie9.js

--- packages/react-app-polyfill/ie9.js
+++ packages/react-app-polyfill/ie9.js
@@ -12,6 +12,4 @@ require('./ie11');
 require('core-js/features/map');
 require('core-js/features/set');
 
-if (typeof window !== 'undefined') {
-  require('raf').polyfill(window);
-}
+require('raf').polyfill();
