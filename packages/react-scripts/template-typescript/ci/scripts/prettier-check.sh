7a1076fff3d0b6c5ff49024345914e30d4bd5359
Phil Tobias <ptobias@digitalocean.com>
2019-08-15 09:18:15 -0700

    update prettier check

---
+++ packages/react-scripts/template-typescript/ci/scripts/prettier-check.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+printf "Running prettier --list-different\n\n"
+if ! ./node_modules/.bin/prettier --list-different '{,!(tmp)/**/}*.{ts,tsx,js,json}'; then
+  printf "\nUnformatted files found by Prettier. Please run 'yarn format' and commit the changes.\n\n"
+  exit 1
+else
+  printf "No improperly formatted files found by Prettier.\n"
+fi
--- packages/react-scripts/template/ci/scripts/prettier-check.sh
+++ packages/react-scripts/template/ci/scripts/prettier-check.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 printf "Running prettier --list-different\n\n"
-if ! ./node_modules/.bin/prettier --list-different '{,!(tmp)/**/}*.{js,json}'; then
-  printf "\nUnformatted files found by Prettier. Please run 'npm run format' and commit the changes.\n\n"
+if ! ./node_modules/.bin/prettier --list-different '{,!(tmp)/**/}*.{ts,tsx,js,json}'; then
+  printf "\nUnformatted files found by Prettier. Please run 'yarn format' and commit the changes.\n\n"
   exit 1
 else
   printf "No improperly formatted files found by Prettier.\n"
