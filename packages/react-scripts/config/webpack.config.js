4868a6c4d33722534e2174cd9848c4c5be22b4ca
Tomáš Szabo <tomas.szabo@deftomat.com>
2019-08-23 09:38:46 +0200

    Minor performance improvements
    - use ESLint cache
    - disable syntactic errors check in fork-ts-checker
    - remove useless `watch` option in fork-ts-checker

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -334,6 +334,7 @@ module.exports = function(webpackEnv) {
           use: [
             {
               options: {
+                cache: true,
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
                 resolvePluginsRelativeTo: __dirname,
@@ -673,7 +674,7 @@ module.exports = function(webpackEnv) {
           }),
           async: isEnvDevelopment,
           useTypescriptIncrementalApi: true,
-          checkSyntacticErrors: true,
+          checkSyntacticErrors: false,
           resolveModuleNameModule: process.versions.pnp
             ? `${__dirname}/pnpTs.js`
             : undefined,
@@ -688,7 +689,6 @@ module.exports = function(webpackEnv) {
             '!**/src/setupProxy.*',
             '!**/src/setupTests.*',
           ],
-          watch: paths.appSrc,
           silent: true,
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
