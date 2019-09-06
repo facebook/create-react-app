903efe5cc6812ecf2edde5772db706f901bfd0a8
John Pangalos <jpangs88@gmail.com>
2019-09-06 20:18:08 +0200

    Fix for linting failure
    It seems like the linter is failing because of an error in the babel
    eslint package combined with dependencies being called in way that
    conflicts with yarn workspaces.

--- tasks/e2e-simple.sh
+++ tasks/e2e-simple.sh
@@ -96,7 +96,7 @@ startLocalRegistry "$root_path"/tasks/verdaccio.yaml
 ./node_modules/.bin/eslint --max-warnings 0 packages/react-scripts/
 
 cd packages/react-error-overlay/
-./node_modules/.bin/eslint --max-warnings 0 src/
+yarn workspace react-error-overlay run eslint --max-warnings 0 src/
 yarn test
 if [ $APPVEYOR != 'True' ]; then
   # Flow started hanging on AppVeyor after we moved to Yarn Workspaces :-(
