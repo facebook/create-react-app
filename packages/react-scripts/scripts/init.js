7cc35b40a57f3a62d408fac37797365bf83bf6fc
Tarik Huber <tarik.huber@ics.local>
2019-09-10 12:59:08 +0200

    Removed .gitignore file and set ignores for functions folder into root .gitignore file

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -165,26 +165,6 @@ module.exports = function(
     }
   }
 
-  // Rename gitignore after the fact to prevent npm from renaming it to .npmignore
-  // See: https://github.com/npm/npm/issues/1862
-  fs.move(
-    path.join(`${appPath}/functions`, 'gitignore'),
-    path.join(`${appPath}/functions`, '.gitignore'),
-    [],
-    err => {
-      if (err) {
-        // Append if there's already a `.gitignore` file there
-        if (err.code === 'EEXIST') {
-          const data = fs.readFileSync(path.join(appPath, 'gitignore'));
-          fs.appendFileSync(path.join(appPath, '.gitignore'), data);
-          fs.unlinkSync(path.join(appPath, 'gitignore'));
-        } else {
-          throw err;
-        }
-      }
-    }
-  );
-
   let command;
   let uArgs;
   let baseArgs;
