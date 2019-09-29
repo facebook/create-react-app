67c38df00aedf56b0f5da0786720af04a09c788a
Adam Mockor <adam.mockor@gmail.com>
2019-09-29 09:33:12 +0200

    move babel config to function

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -110,6 +110,47 @@ module.exports = function(webpackEnv, options = {}) {
   // Get environment variables to inject into our app.
   const env = getClientEnvironment(publicUrl);
 
+  const getBabelLoaderOptions = () => ({
+    customize: require.resolve('babel-preset-react-app/webpack-overrides'),
+    // @remove-on-eject-begin
+    babelrc: false,
+    configFile: false,
+    presets: [require.resolve('babel-preset-react-app')],
+    // Make sure we have a unique cache identifier, erring on the
+    // side of caution.
+    // We remove this when the user ejects because the default
+    // is sane and uses Babel options. Instead of options, we use
+    // the react-scripts and babel-preset-react-app versions.
+    cacheIdentifier: getCacheIdentifier(
+      isEnvProduction ? 'production' : isEnvDevelopment && 'development',
+      [
+        'babel-plugin-named-asset-import',
+        'babel-preset-react-app',
+        'react-dev-utils',
+        'react-scripts',
+      ]
+    ),
+    // @remove-on-eject-end
+    plugins: [
+      [
+        require.resolve('babel-plugin-named-asset-import'),
+        {
+          loaderMap: {
+            svg: {
+              ReactComponent: '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+            },
+          },
+        },
+      ],
+    ],
+    // This is a feature of `babel-loader` for webpack (not Babel itself).
+    // It enables caching results in ./node_modules/.cache/babel-loader/
+    // directory for faster rebuilds.
+    cacheDirectory: true,
+    cacheCompression: isEnvProduction,
+    compact: isEnvProduction,
+  });
+
   // common function to get style loaders
   const getStyleLoaders = (cssOptions, preProcessor) => {
     const loaders = [
@@ -395,52 +436,7 @@ module.exports = function(webpackEnv, options = {}) {
               test: /\.(js|mjs|jsx|ts|tsx)$/,
               include: paths.appSrc,
               loader: require.resolve('babel-loader'),
-              options: {
-                customize: require.resolve(
-                  'babel-preset-react-app/webpack-overrides'
-                ),
-                // @remove-on-eject-begin
-                babelrc: false,
-                configFile: false,
-                presets: [require.resolve('babel-preset-react-app')],
-                // Make sure we have a unique cache identifier, erring on the
-                // side of caution.
-                // We remove this when the user ejects because the default
-                // is sane and uses Babel options. Instead of options, we use
-                // the react-scripts and babel-preset-react-app versions.
-                cacheIdentifier: getCacheIdentifier(
-                  isEnvProduction
-                    ? 'production'
-                    : isEnvDevelopment && 'development',
-                  [
-                    'babel-plugin-named-asset-import',
-                    'babel-preset-react-app',
-                    'react-dev-utils',
-                    'react-scripts',
-                  ]
-                ),
-                // @remove-on-eject-end
-                plugins: [
-                  [
-                    require.resolve('babel-plugin-named-asset-import'),
-                    {
-                      loaderMap: {
-                        svg: {
-                          ReactComponent:
-                            '@svgr/webpack?-svgo,+titleProp,+ref![path]',
-                        },
-                      },
-                    },
-                  ],
-                ],
-                // This is a feature of `babel-loader` for webpack (not Babel itself).
-                // It enables caching results in ./node_modules/.cache/babel-loader/
-                // directory for faster rebuilds.
-                cacheDirectory: true,
-                // See #6846 for context on why cacheCompression is disabled
-                cacheCompression: false,
-                compact: isEnvProduction,
-              },
+              options: getBabelLoaderOptions(),
             },
             // Process any JS outside of the app with Babel.
             // Unlike the application JS, we only compile the standard ES features.
@@ -547,7 +543,15 @@ module.exports = function(webpackEnv, options = {}) {
             },
             {
               test: /\.mdx?$/,
-              use: ['babel-loader', '@mdx-js/loader'],
+              use: [
+                {
+                  loader: require.resolve('babel-loader'),
+                  options: getBabelLoaderOptions(),
+                },
+                {
+                  loader: require.resolve('@mdx-js/loader'),
+                },
+              ],
             },
             {
               test: /\.svg$/,
