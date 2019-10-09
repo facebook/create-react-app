d0b0abc7e62169e4bf3bd32a8246414ee89a83cf
Adam Močkoř <adam.mockor@gmail.com>
2019-10-09 14:27:21 +0200

    Feat/raw loader for documenting (#134)
    * remove support for documenting props via raw loader
    
    * revert App move
    
    * revert changes

--- packages/styleguide/src/utils/ComponentDocs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ComponentDocs } from './../components';
-
-export default props => (
-  <ComponentDocs
-    resolver={path => import(`!!raw-loader!./../components/${path}`)}
-    {...props}
-  />
-);
