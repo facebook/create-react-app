04b0fc86a09237285e06e9e510b5aa342c6b52dc
Adam Močkoř <adam.mockor@gmail.com>
2019-10-09 22:58:19 +0200

    remove support for ComponentDocs path prop to displaying props from source (#137)

--- packages/styleguide/src/components/ComponentDocs/ComponentDocs.js
+++ packages/styleguide/src/components/ComponentDocs/ComponentDocs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { string, array, oneOf, element, func, oneOfType } from 'prop-types';
+import { string, array, element, func, oneOfType } from 'prop-types';
 import styled from 'styled-components';
 
 import { DocsTable } from './../Table';
@@ -8,12 +8,7 @@ import { H3 } from './../Typography';
 import { Bar, BarItem } from '../Bar';
 import InfoBadge from '../Badge/InfoBadge';
 
-import {
-  getComponentInfo,
-  getTableData,
-  sortTableData,
-  excludeProps,
-} from './utils';
+import { getTableData, sortTableData, excludeProps } from './utils';
 
 function getComponentInfoFromComponent(component = {}) {
   return component.__docgenInfo && [component.__docgenInfo];
@@ -25,56 +20,25 @@ class ComponentDocs extends Component {
   static propTypes = {
     /** Show props of passed React component with `__docgenInfo` property (eg. component was processed with [babel-plugin-react-docgen](https://github.com/storybooks/babel-plugin-react-docgen)). */
     component: oneOfType([element, func]),
-    /** (deprecated) Path to component on file-system. `resolver` needs to be set to be able to utilize this approach to display components props. */
-    path: string,
     /** Component title. If component has `displayName set, title will be computed automatically. */
     title: string,
     /** Array of props which should be excluded from props table. */
     excludes: array,
-    /** (deprecated) Rendering scope of component. */
-    renderingScope: oneOf(['universal', 'react', 'static']),
-    /** (deprecated) To resolve component from `path` prop, `resolver` function must be passed to get component code as string. Webpack's `raw-loader` could be utilized to that and it could look like this `path => import(``!!raw-loader!./../components/${path}``)`. */
-    resolver: func,
   };
 
   static defaultProps = {
     excludes: [],
-    resolver: () => {},
   };
 
   state = {
     info: [],
   };
 
-  async componentWillMount() {
-    const { path } = this.props;
-
-    if (path) {
-      console.warn(
-        'Support for `path` prop of `ComponentDocs` and runtime generation of props documentation is deprecated and will be removed in next major version. Please use `component` prop with `babel-plugin-react-docgen` instead.'
-      );
-
-      let info;
-
-      try {
-        info = await getComponentInfo(path, this.props.resolver);
-      } catch (e) {
-        this.setState(() => ({ error: e }));
-      }
-
-      this.setState(() => ({
-        info,
-      }));
-    }
-  }
-
   render() {
     const data = sortTableData(
       excludeProps(
         this.props.excludes,
-        getTableData(
-          getComponentInfoFromComponent(this.props.component) || this.state.info
-        )
+        getTableData(getComponentInfoFromComponent(this.props.component))
       )
     );
 
