fea44de0b54b7d155d0379125378cc72786d884a
Tharun <rajendrantharun@live.com>
2019-10-13 14:42:31 +0530

    Added support for numeric supporters by using babel

--- packages/babel-preset-react-app/create.js
+++ packages/babel-preset-react-app/create.js
@@ -145,6 +145,10 @@ module.exports = function(api, opts, env) {
         require('@babel/plugin-proposal-decorators').default,
         false,
       ],
+      // Adds Numeric Seperators for TypeScript files
+      isTypeScriptEnabled && [
+        require('@babel/plugin-proposal-numeric-separator'),
+      ],
       // class { handleClick = () => { } }
       // Enable loose mode to use assignment instead of defineProperty
       // See discussion in https://github.com/facebook/create-react-app/issues/4263
