72fda031610c664a40503eac0ef49ff64c7df024
Kent C. Dodds <kent@doddsfamily.us>
2019-10-28 14:24:15 -0600

    fix setupTests in templates

--- packages/cra-template-typescript/template/src/App.test.tsx
+++ packages/cra-template-typescript/template/src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders welcome heading', () => {
+  const { getByRole } = render(<App />);
+  const headingElement = getByRole('heading');
+  expect(headingElement).toHaveTextContent(/welcome/i);
 });
