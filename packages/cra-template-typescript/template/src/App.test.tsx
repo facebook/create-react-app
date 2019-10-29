06b2617a70a7b9b2ef6344d19f6e1bbe00e89b5d
Ian Schmitz <ianschmitz@gmail.com>
2019-10-28 21:29:53 -0700

    Fix template tests

--- packages/cra-template-typescript/template/src/App.test.tsx
+++ packages/cra-template-typescript/template/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders welcome heading', () => {
   const { getByRole } = render(<App />);
-  const headingElement = getByRole('heading');
-  expect(headingElement).toHaveTextContent(/welcome/i);
+  const linkElement = getByRole('link');
+  expect(linkElement).toHaveTextContent(/learn react/i);
 });
