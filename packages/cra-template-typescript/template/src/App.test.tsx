83e448725cf99249d4f92686d8cd169b0898bc96
RJ <22164266+suprj@users.noreply.github.com>
2019-11-06 16:03:45 -0800

    Make base and TypeScript templates consistent

--- packages/cra-template-typescript/template/src/App.test.tsx
+++ packages/cra-template-typescript/template/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders welcome heading', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
