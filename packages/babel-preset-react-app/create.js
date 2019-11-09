d12b4b6c53de3d7211d158f70c450ef9b5be9786
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2019-11-09 02:38:36 -0300

    Add optional chaining and nullish coalescing operators support (#7438)

--- packages/babel-preset-react-app/create.js
+++ packages/babel-preset-react-app/create.js
@@ -196,6 +196,10 @@ module.exports = function(api, opts, env) {
       ],
       // Adds syntax support for import()
       require('@babel/plugin-syntax-dynamic-import').default,
+      // Adds syntax support for optional chaining (.?)
+      require('@babel/plugin-proposal-optional-chaining').default,
+      // Adds syntax support for default value using ?? operator
+      require('@babel/plugin-proposal-nullish-coalescing-operator').default,
       isEnvTest &&
         // Transform dynamic import to require
         require('babel-plugin-dynamic-import-node'),
