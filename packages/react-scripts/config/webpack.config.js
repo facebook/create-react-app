694a2a24608733c52acd32ce699da74de02b2925
Giuseppe Bandiera <giuseppe.bandiera@live.com>
2019-11-11 11:22:56 +0100

    Add flag DISABLE_ESLINT_DEV to disable eslint on dev server

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -354,26 +354,43 @@ module.exports = function (webpackEnv) {
         {
           test: /\.(js|mjs|jsx|ts|tsx)$/,
           enforce: 'pre',
-          use: [
-            {
-              options: {
-                cache: true,
-                formatter: require.resolve('react-dev-utils/eslintFormatter'),
-                eslintPath: require.resolve('eslint'),
-                resolvePluginsRelativeTo: __dirname,
-                // @remove-on-eject-begin
-                ignore: isExtendingEslintConfig,
-                baseConfig: isExtendingEslintConfig
-                  ? undefined
-                  : {
-                      extends: [require.resolve('eslint-config-react-app')],
-                    },
-                useEslintrc: isExtendingEslintConfig,
-                // @remove-on-eject-end
+          use: isEnvDevelopment && process.env.DISABLE_ESLINT_DEV === 'true'
+          ? undefined
+          : [
+              {
+                options: {
+                  cache: true,
+                  formatter: require.resolve('react-dev-utils/eslintFormatter'),
+                  eslintPath: require.resolve('eslint'),
+                  resolvePluginsRelativeTo: __dirname,
+                  // @remove-on-eject-begin
+                  ignore: process.env.EXTEND_ESLINT === 'true',
+                  baseConfig: (() => {
+                    // We allow overriding the config only if the env variable is set
+                    if (process.env.EXTEND_ESLINT === 'true') {
+                      const eslintCli = new eslint.CLIEngine();
+                      let eslintConfig;
+                      try {
+                        eslintConfig = eslintCli.getConfigForFile(
+                          paths.appIndexJs
+                        );
+                      } catch (e) {
+                        console.error(e);
+                        process.exit(1);
+                      }
+                      return eslintConfig;
+                    } else {
+                      return {
+                        extends: [require.resolve('eslint-config-react-app')],
+                      };
+                    }
+                  })(),
+                  useEslintrc: false,
+                  // @remove-on-eject-end
+                },
+                loader: require.resolve('eslint-loader'),
               },
-              loader: require.resolve('eslint-loader'),
-            },
-          ],
+            ],
           include: paths.appSrc,
         },
         {
