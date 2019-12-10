fc60de0ff68605cfda61e82b18f64091ebe309a7
Andrew Luca <thendrluca@gmail.com>
2019-12-10 12:24:14 +0200

    fix(react-scripts): enable css grid autoprefix
    Closes #8136

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -115,6 +115,7 @@ module.exports = function(webpackEnv) {
             require('postcss-preset-env')({
               autoprefixer: {
                 flexbox: 'no-2009',
+                grid: true,
               },
               stage: 3,
             }),
