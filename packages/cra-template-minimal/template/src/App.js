f2bd768d6b59c1de7b26bf8f6df4aa4e15f06712
Nick McCurdy <nick@nickmccurdy.com>
2020-03-14 06:09:34 -0400

    Merge App.js into index.js

--- packages/cra-template-minimal/template/src/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return <h1>React App</h1>;
-}
--- packages/cra-template-minimal/template/src/index.js
+++ packages/cra-template-minimal/template/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>React App</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
