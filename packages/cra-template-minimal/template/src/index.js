e1e534647c89e92fc3e475eb308c93cb1b74be9d
Nick McCurdy <nick@nickmccurdy.com>
2020-03-14 06:03:33 -0400

    Add minimal template

---
+++ packages/cra-template-minimal/template/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
