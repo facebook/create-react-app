e3e7471b63233e0b29d0339028f74723b975e132
Michaël De Boey <info@michaeldeboey.be>
2020-03-24 11:23:18 +0100

    Update template tests

--- packages/cra-template/template/src/App.test.js
+++ packages/cra-template/template/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
