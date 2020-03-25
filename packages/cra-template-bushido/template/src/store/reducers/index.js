bffa4647c91390933efc79b489ddb8331c69f69e
JimmyMcBride <mcbride967@gmail.com>
2020-03-24 19:16:09 -0500

    removed my templates

--- packages/cra-template-bushido/template/src/store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { counter } from "./counter";
-
-// Using combine reducers to break up reducers into different files
-export default combineReducers({
-  counter
-});
