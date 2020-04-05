c42053aa5d3427f646d1aed8e5870780f16390ed
Lior Frezi <liorf@rewire.to>
2020-04-05 22:28:02 +0300

    add babel plugin rewire for jest

--- packages/react-scripts/config/jest/babelTransform.js
+++ packages/react-scripts/config/jest/babelTransform.js
@@ -11,6 +11,7 @@ const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
   presets: [require.resolve('babel-preset-react-app')],
+  plugins: [require.resolve('babel-plugin-rewire')],
   babelrc: false,
   configFile: false,
 });
