08dad92cbd95a7d7f93d2f5397ee9fda60ef7428
Timothy Schubert <Timothy.Schubert@nab.com.au>
2020-04-14 21:09:34 +1000

    [#8829] Ensure all build errors are retained for logging when a build fails

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -188,11 +188,6 @@ function build(previousFileSizes) {
         );
       }
       if (messages.errors.length) {
-        // Only keep the first error. Others are often indicative
-        // of the same problem, but confuse the reader with noise.
-        if (messages.errors.length > 1) {
-          messages.errors.length = 1;
-        }
         return reject(new Error(messages.errors.join('\n\n')));
       }
       if (
