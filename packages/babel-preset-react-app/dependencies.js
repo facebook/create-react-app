24e768fed8405a83c8ff8ec4ae9bc66f863d181a
Moritz Gunz <moritz.gunz@gmail.com>
2020-04-17 14:36:40 +0200

    Optimize React.createElement property accesses

--- packages/babel-preset-react-app/dependencies.js
+++ packages/babel-preset-react-app/dependencies.js
@@ -142,6 +142,8 @@ module.exports = function(api, opts) {
           absoluteRuntime: absoluteRuntimePath,
         },
       ],
+      isEnvProduction &&
+        require('babel-plugin-transform-react-create-element').default,
     ].filter(Boolean),
   };
 };
