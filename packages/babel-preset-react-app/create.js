0dde7ea9245fd83260887f3028d773be7e06992c
Chetanya Kandhari <availchet@gmail.com>
2020-04-19 00:06:07 +0530

    Remove outdated comment

--- packages/babel-preset-react-app/create.js
+++ packages/babel-preset-react-app/create.js
@@ -82,7 +82,6 @@ module.exports = function(api, opts, env) {
           // Allow importing core-js in entrypoint and use browserlist to select polyfills
           useBuiltIns: 'entry',
           // Set the corejs version we are using to avoid warnings in console
-          // This will need to change once we upgrade to corejs@3
           corejs: 3,
           // Do not transform modules to CJS
           modules: false,
