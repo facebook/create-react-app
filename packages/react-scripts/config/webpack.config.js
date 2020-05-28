460b9538c9d3d97cde21583165649ea57af742bd
Richard Freling <4776235+rfreling@users.noreply.github.com>
2020-05-28 10:17:37 -0500

    Update syntax for including `style-loader`
    This simple syntax change will allow for other CRA extension utilities (like customize-cra) to add options the 'style-loader'.
    
    E.g. https://github.com/arackaf/customize-cra/blob/master/api.md#adjuststyleloaderscallback

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -88,7 +88,10 @@ module.exports = function (webpackEnv) {
   // common function to get style loaders
   const getStyleLoaders = (cssOptions, preProcessor) => {
     const loaders = [
-      isEnvDevelopment && require.resolve('style-loader'),
+      isEnvDevelopment && {
+        loader: require.resolve('style-loader'),
+        options: {}
+      },
       isEnvProduction && {
         loader: MiniCssExtractPlugin.loader,
         // css is located in `static/css`, use '../../' to locate index.html folder
