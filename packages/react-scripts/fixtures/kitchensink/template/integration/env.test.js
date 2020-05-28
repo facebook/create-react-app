1e40cd225e6847ddb4a6876ae7c7ac2c3e381cbe
Brody McKee <mrmckeb@hotmail.com>
2020-05-28 11:56:04 +0300

    tests: fix more failing tests

--- packages/react-scripts/fixtures/kitchensink/template/integration/env.test.js
+++ packages/react-scripts/fixtures/kitchensink/template/integration/env.test.js
@@ -31,7 +31,7 @@ describe('Integration', () => {
           'production'
         );
         expect(doc.getElementById('feature-file-env-x').textContent).toBe(
-          'x-from-production-env'
+          'x-from-original-local-env'
         );
       } else {
         expect(doc.getElementById('feature-file-env').textContent).toBe(
