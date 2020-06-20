fdbde1f3c256b43d5386b5ae3a75083dbd8f0aff
Ian Schmitz <ianschmitz@gmail.com>
2020-06-20 13:27:04 -0700

    Bump version of Verdaccio (#7787)

--- docusaurus/docs/deployment.md
+++ docusaurus/docs/deployment.md
@@ -40,7 +40,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
--- packages/cra-template/template/src/reportWebVitals.js
+++ packages/cra-template/template/src/reportWebVitals.js
@@ -1,13 +1,13 @@
-const reportWebVitals = (onPerfEntry) => {
+const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);  
+      getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
       getTTFB(onPerfEntry);
     });
   }
-}
+};
 
 export default reportWebVitals;
