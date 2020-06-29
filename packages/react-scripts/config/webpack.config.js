8984d92ea4a780cd31ba80f6a59c46d031507da8
Philippe Collin <pcollin@boreal-is.com>
2020-06-29 15:17:00 -0400

    add @emotion/babel-preset-css-prop babel preset

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -405,7 +405,7 @@ module.exports = function (webpackEnv) {
                 // @remove-on-eject-begin
                 babelrc: false,
                 configFile: false,
-                presets: [require.resolve('babel-preset-react-app')],
+                presets: [require.resolve('babel-preset-react-app'), require.resolve('@emotion/babel-preset-css-prop')],
                 // Make sure we have a unique cache identifier, erring on the
                 // side of caution.
                 // We remove this when the user ejects because the default
@@ -434,6 +434,7 @@ module.exports = function (webpackEnv) {
                         },
                       },
                     },
+                  
                   ],
                   isEnvDevelopment &&
                     shouldUseReactRefresh &&
