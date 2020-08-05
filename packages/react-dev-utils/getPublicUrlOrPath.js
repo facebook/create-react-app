eecb79b7b6d72dfc9098f36e6dad2e84da5c9052
Deniz Susman <susmandeniz@gmail.com>
2020-08-05 20:40:25 +0300

    Typo fix

--- packages/react-dev-utils/getPublicUrlOrPath.js
+++ packages/react-dev-utils/getPublicUrlOrPath.js
@@ -13,7 +13,7 @@ module.exports = getPublicUrlOrPath;
 
 /**
  * Returns a URL or a path with slash at the end
- * In production can be URL, abolute path, relative path
+ * In production can be URL, absolute path, relative path
  * In development always will be an absolute path
  * In development can use `path` module functions for operations
  *
