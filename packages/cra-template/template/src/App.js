3a051cef55c2df8a6915d74a039895c1e6de714e
Samuel Amagbakhen <amagbakhensamuel@gmail.com>
2020-08-11 16:21:04 +0100

    Refactored template to ES6+ syntax

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +22,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
