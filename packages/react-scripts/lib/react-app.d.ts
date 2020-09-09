e49e67ae3ca3534dbba3c5447df118a10b03e3bf
darthmaim <github@darthmaim.de>
2020-09-09 14:18:05 +0200

    Fix indentation in react-app.d.ts

--- packages/react-scripts/lib/react-app.d.ts
+++ packages/react-scripts/lib/react-app.d.ts
@@ -35,8 +35,8 @@ declare module '*.png' {
 }
 
 declare module '*.webp' {
-    const src: string;
-    export default src;
+  const src: string;
+  export default src;
 }
 
 declare module '*.svg' {
