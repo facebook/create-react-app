bfa989697f73e47bd2da82c7b87c5a04a1dde025
Parth Narielwala <pnarielwala@gmail.com>
2020-10-21 17:58:00 -0400

    use ignoreOrder param

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -591,6 +591,7 @@ module.exports = function(webpackEnv) {
           // both options are optional
           filename: 'static/css/[name].[contenthash:8].css',
           chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+          ignoreOrder: true,
         }),
       // Generate a manifest file which contains a mapping of all asset filenames
       // to their corresponding output file so that tools can pick it up without
