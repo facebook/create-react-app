2b1161b34641bb4d2f269661cd636bbcd4888406
Ian Sutherland <ian@iansutherland.ca>
2020-10-22 22:21:02 -0600

    Pass JSX runtime setting to Babel preset in Jest config (#9865)

--- packages/react-scripts/config/jest/babelTransform.js
+++ packages/react-scripts/config/jest/babelTransform.js
@@ -9,8 +9,28 @@
 
 const babelJest = require('babel-jest');
 
+const hasJsxRuntime = (() => {
+  if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
+    return false;
+  }
+
+  try {
+    require.resolve('react/jsx-runtime');
+    return true;
+  } catch (e) {
+    return false;
+  }
+})();
+
 module.exports = babelJest.createTransformer({
-  presets: [require.resolve('babel-preset-react-app')],
+  presets: [
+    [
+      require.resolve('babel-preset-react-app'),
+      {
+        runtime: hasJsxRuntime ? 'automatic' : 'classic',
+      },
+    ],
+  ],
   babelrc: false,
   configFile: false,
 });
