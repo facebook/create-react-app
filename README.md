4ee2764110ffc775bace39339754d86e0d2c6359
Nick Galloway <blackarctic@icloud.com>
2020-11-25 09:35:55 -0800

    Update to 4.0.1 from Facebook (#10)
    * Fix noFallthroughCasesInSwitch/jsx object is not extensible (#9921)
    
    Co-authored-by: Konstantin Simeonov <kon.simeonov@protonmail.com>
    
    * Add logo license to README
    
    * Remove trailing space in reportWebVitals.ts (#10040)
    
    * docs: add React Testing Library as a library requiring jsdom (#10052)
    
    Co-authored-by: Ian Schmitz <ianschmitz@gmail.com>
    
    * Increase Workbox's maximumFileSizeToCacheInBytes (#10048)
    
    * Create FUNDING.yml
    
    * replace inquirer with prompts (#10083)
    
    - remove `react-dev-utils/inquirer` public import
    
    * Prepare 4.0.1 release
    
    * Prepare 4.0.1 release
    
    * Publish
    
     - cra-template-typescript@1.1.1
     - cra-template@1.1.1
     - create-react-app@4.0.1
     - react-dev-utils@11.0.1
     - react-scripts@4.0.1
    
    Co-authored-by: Ryota Murakami <dojce1048@gmail.com>
    Co-authored-by: Konstantin Simeonov <kon.simeonov@protonmail.com>
    Co-authored-by: Ian Sutherland <ian@iansutherland.ca>
    Co-authored-by: sho90 <aznecosann@gmail.com>
    Co-authored-by: Anyul Rivas <anyulled@gmail.com>
    Co-authored-by: Ian Schmitz <ianschmitz@gmail.com>
    Co-authored-by: Jeffrey Posnick <jeffy@google.com>
    Co-authored-by: Evan Bacon <baconbrix@gmail.com>

--- README.md
+++ README.md
@@ -217,4 +217,4 @@ We are grateful to the authors of existing related projects for their ideas and 
 
 ## License
 
-Create React App is open source software [licensed as MIT](https://github.com/facebook/create-react-app/blob/master/LICENSE).
+Create React App is open source software [licensed as MIT](https://github.com/facebook/create-react-app/blob/master/LICENSE). The Create React App logo is licensed  under a [Creative Commons Attribution 4.0 International license](https://creativecommons.org/licenses/by/4.0/).
--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -714,6 +714,10 @@ module.exports = function (webpackEnv) {
           swSrc,
           dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
           exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
+          // Bump up the default maximum size (2mb) that's precached,
+          // to make lazy-loading failure scenarios less likely.
+          // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
+          maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         }),
       // TypeScript type checking
       useTypeScript &&
