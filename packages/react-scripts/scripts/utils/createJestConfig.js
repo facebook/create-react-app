18f9982b11fc198a1c93dff2eddcf2762be07056
Devin Riegle <riegledevin@gmail.com>
2020-11-30 00:54:19 -0500

    fix: Use <rootDir> for Jest testRunner path when ejecting

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -39,7 +39,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
       '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
     ],
     testEnvironment: 'jsdom',
-    testRunner: require.resolve('jest-circus/runner'),
+    testRunner: resolve('node_modules/jest-circus/runner.js'),
     transform: {
       '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': isEjecting
         ? '<rootDir>/node_modules/babel-jest'
