7a37033436831932f9fb829b45396222b654769d
Alessandro DiMarco <8059120+alesso-x@users.noreply.github.com>
2020-12-03 13:26:20 -0500

    enable terser plugin and optimize css assets plugin

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -244,85 +244,85 @@ module.exports = function (webpackEnv) {
       // module chunks which are built will work in web workers as well.
       globalObject: 'this',
     },
-    // @joor we do not support chunks yet
-    // optimization: {
-    //   minimize: isEnvProduction,
-    //   minimizer: [
-    //     // This is only used in production mode
-    //     new TerserPlugin({
-    //       terserOptions: {
-    //         parse: {
-    //           // We want terser to parse ecma 8 code. However, we don't want it
-    //           // to apply any minification steps that turns valid ecma 5 code
-    //           // into invalid ecma 5 code. This is why the 'compress' and 'output'
-    //           // sections only apply transformations that are ecma 5 safe
-    //           // https://github.com/facebook/create-react-app/pull/4234
-    //           ecma: 8,
-    //         },
-    //         compress: {
-    //           ecma: 5,
-    //           warnings: false,
-    //           // Disabled because of an issue with Uglify breaking seemingly valid code:
-    //           // https://github.com/facebook/create-react-app/issues/2376
-    //           // Pending further investigation:
-    //           // https://github.com/mishoo/UglifyJS2/issues/2011
-    //           comparisons: false,
-    //           // Disabled because of an issue with Terser breaking valid code:
-    //           // https://github.com/facebook/create-react-app/issues/5250
-    //           // Pending further investigation:
-    //           // https://github.com/terser-js/terser/issues/120
-    //           inline: 2,
-    //         },
-    //         mangle: {
-    //           safari10: true,
-    //         },
-    //         // Added for profiling in devtools
-    //         keep_classnames: isEnvProductionProfile,
-    //         keep_fnames: isEnvProductionProfile,
-    //         output: {
-    //           ecma: 5,
-    //           comments: false,
-    //           // Turned on because emoji and regex is not minified properly using default
-    //           // https://github.com/facebook/create-react-app/issues/2488
-    //           ascii_only: true,
-    //         },
-    //       },
-    //       sourceMap: shouldUseSourceMap,
-    //     }),
-    //     // This is only used in production mode
-    //     new OptimizeCSSAssetsPlugin({
-    //       cssProcessorOptions: {
-    //         parser: safePostCssParser,
-    //         map: shouldUseSourceMap
-    //           ? {
-    //               // `inline: false` forces the sourcemap to be output into a
-    //               // separate file
-    //               inline: false,
-    //               // `annotation: true` appends the sourceMappingURL to the end of
-    //               // the css file, helping the browser find the sourcemap
-    //               annotation: true,
-    //             }
-    //           : false,
-    //       },
-    //       cssProcessorPluginOptions: {
-    //         preset: ['default', { minifyFontValues: { removeQuotes: false } }],
-    //       },
-    //     }),
-    //   ],
-    //   // Automatically split vendor and commons
-    //   // https://twitter.com/wSokra/status/969633336732905474
-    //   // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-    //   splitChunks: {
-    //     chunks: 'all',
-    //     name: false,
-    //   },
-    //   // Keep the runtime chunk separated to enable long term caching
-    //   // https://twitter.com/wSokra/status/969679223278505985
-    //   // https://github.com/facebook/create-react-app/issues/5358
-    //   runtimeChunk: {
-    //     name: entrypoint => `runtime-${entrypoint.name}`,
-    //   },
-    // },
+    optimization: {
+      minimize: isEnvProduction,
+      minimizer: [
+        // This is only used in production mode
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              // We want terser to parse ecma 8 code. However, we don't want it
+              // to apply any minification steps that turns valid ecma 5 code
+              // into invalid ecma 5 code. This is why the 'compress' and 'output'
+              // sections only apply transformations that are ecma 5 safe
+              // https://github.com/facebook/create-react-app/pull/4234
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              // Disabled because of an issue with Uglify breaking seemingly valid code:
+              // https://github.com/facebook/create-react-app/issues/2376
+              // Pending further investigation:
+              // https://github.com/mishoo/UglifyJS2/issues/2011
+              comparisons: false,
+              // Disabled because of an issue with Terser breaking valid code:
+              // https://github.com/facebook/create-react-app/issues/5250
+              // Pending further investigation:
+              // https://github.com/terser-js/terser/issues/120
+              inline: 2,
+            },
+            mangle: {
+              safari10: true,
+            },
+            // Added for profiling in devtools
+            keep_classnames: isEnvProductionProfile,
+            keep_fnames: isEnvProductionProfile,
+            output: {
+              ecma: 5,
+              comments: false,
+              // Turned on because emoji and regex is not minified properly using default
+              // https://github.com/facebook/create-react-app/issues/2488
+              ascii_only: true,
+            },
+          },
+          sourceMap: shouldUseSourceMap,
+        }),
+        // This is only used in production mode
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorOptions: {
+            parser: safePostCssParser,
+            map: shouldUseSourceMap
+              ? {
+                  // `inline: false` forces the sourcemap to be output into a
+                  // separate file
+                  inline: false,
+                  // `annotation: true` appends the sourceMappingURL to the end of
+                  // the css file, helping the browser find the sourcemap
+                  annotation: true,
+                }
+              : false,
+          },
+          cssProcessorPluginOptions: {
+            preset: ['default', { minifyFontValues: { removeQuotes: false } }],
+          },
+        }),
+      ],
+      // @joor we do not support chunks yet
+      // // Automatically split vendor and commons
+      // // https://twitter.com/wSokra/status/969633336732905474
+      // // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+      // splitChunks: {
+      //   chunks: 'all',
+      //   name: false,
+      // },
+      // // Keep the runtime chunk separated to enable long term caching
+      // // https://twitter.com/wSokra/status/969679223278505985
+      // // https://github.com/facebook/create-react-app/issues/5358
+      // runtimeChunk: {
+      //   name: entrypoint => `runtime-${entrypoint.name}`,
+      // },
+    },
     resolve: {
       // This allows you to set a fallback for where webpack should look for modules.
       // We placed these paths second because we want `node_modules` to "win"
