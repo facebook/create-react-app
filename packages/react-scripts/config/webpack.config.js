e039ad375258ddb592539b374f4462455e8e1ebf
EhsanKhaki <ehsankhfr@gmail.com>
2020-12-08 05:08:19 +0000

    Move ESLint cache file into node_modules (#9977)
    Co-authored-by: Ian Schmitz <ianschmitz@gmail.com>

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -759,6 +759,10 @@ module.exports = function (webpackEnv) {
         eslintPath: require.resolve('eslint'),
         context: paths.appSrc,
         cache: true,
+        cacheLocation: path.resolve(
+          paths.appNodeModules,
+          '.cache/.eslintcache'
+        ),
         // ESLint class options
         cwd: paths.appPath,
         resolvePluginsRelativeTo: __dirname,
