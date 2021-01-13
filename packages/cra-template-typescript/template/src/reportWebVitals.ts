dd338da7b78a2f3429f81778e9d735e3d22fc30e
Michaël De Boey <info@michaeldeboey.be>
2021-01-13 21:49:04 +0100

    Update reportWebVitals.ts

--- packages/cra-template-typescript/template/src/reportWebVitals.ts
+++ packages/cra-template-typescript/template/src/reportWebVitals.ts
@@ -1,7 +1,7 @@
 import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
+  if (onPerfEntry) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
