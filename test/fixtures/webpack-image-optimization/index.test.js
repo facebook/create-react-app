32246ee3343927a3c00ff09ab0738be5d051f5ae
Sumanth Ratna <2022sratna@tjhsst.edu>
2021-02-17 14:39:24 -0500

    Add webpack-image-optimization test

---
+++ test/fixtures/webpack-image-optimization/index.test.js
@@ -0,0 +1,26 @@
+const testSetup = require('../__shared__/test-setup');
+
+const fs = require('fs-extra');
+const globby = require('globby');
+const path = require('path');
+
+test('optimizes in production', async () => {
+  const { fulfilled } = await testSetup.scripts.build();
+  expect(fulfilled).toBe(true);
+
+  console.log(testSetup.testDirectory, testSetup);
+  const buildDir = path.join(testSetup.testDirectory, 'build');
+  const builtImageFile = path.join(
+    buildDir,
+    globby.sync('**/*.png', { cwd: buildDir }).pop()
+  );
+  const recievedBuf = fs.readFileSync(builtImageFile);
+
+  const expectedImageFile = path.join(
+    testSetup.templateDirectory,
+    './src/images/webpack-optimized.png'
+  );
+  const expectedBuf = fs.readFileSync(expectedImageFile);
+
+  expect(recievedBuf).toStrictEqual(expectedBuf);
+});
