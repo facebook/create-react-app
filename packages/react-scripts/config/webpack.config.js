4d0be4a674300c43d476508166440a7cec0f58f7
Morten N.O. Henriksen <mh@gi2.dk>
2021-03-06 08:37:37 +0100

    Support Webpack 5 IgnorePlugin signature
    Reference:
    * https://webpack.js.org/plugins/ignore-plugin/#example-of-ignoring-moment-locales
    * #10006

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -703,7 +703,10 @@ module.exports = function (webpackEnv) {
       // solution that requires the user to opt into importing specific locales.
       // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
       // You can remove this if you don't use Moment.js:
-      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/locale$/,
+        contextRegExp: /moment$/,
+      }),
       // Generate a service worker script that will precache, and keep up to date,
       // the HTML & assets that are part of the webpack build.
       isEnvProduction &&
