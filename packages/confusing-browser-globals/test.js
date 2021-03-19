cb45333c03da6d4002ca1d9f83b9d545168a5f6a
Yann Eves <hello@yanneves.com>
2021-03-19 17:18:06 +0000

    feat(confusing-browser-globals): Add relevant messaging to no-restricted-globals eslint rule
    BREAKING CHANGE: export from confusing-browser-globals package changed from String[] to Object[] - no change required if used in eslint no-restricted-globals config

--- packages/confusing-browser-globals/test.js
+++ packages/confusing-browser-globals/test.js
@@ -15,6 +15,30 @@ it('should return an Array of globals', () => {
   expect(Array.isArray(globals)).toBe(true);
 });
 
-it('should contain "event" variable', () => {
-  expect(globals).toContain('event');
+it('should contain "event" variable preferring local parameter', () => {
+  expect(globals).toContainEqual({
+    name: 'event',
+    message: 'Use local parameter instead.',
+  });
+});
+
+it('should contain "location" variable preferring local parameter or window global', () => {
+  expect(globals).toContainEqual({
+    name: 'location',
+    message: 'Use local parameter or window.location instead.',
+  });
+});
+
+it('should contain "addEventListener" variable preferring local parameter or document global', () => {
+  expect(globals).toContainEqual({
+    name: 'addEventListener',
+    message: 'Use local parameter or document.addEventListener instead.',
+  });
+});
+
+it('should contain "self" variable preferring local parameter or WorkerGlobalScope global', () => {
+  expect(globals).toContainEqual({
+    name: 'self',
+    message: 'Use local parameter or WorkerGlobalScope.self instead.',
+  });
 });
