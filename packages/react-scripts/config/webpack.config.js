871b2e915acd18206ac4199d9bbe487726368a8f
Morten N.O. Henriksen <mh@gi2.dk>
2021-03-20 15:55:29 +0100

    simplify output path
    review feedback from @kumarlachhani

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -211,7 +211,7 @@ module.exports = function (webpackEnv) {
         : paths.appIndexJs,
     output: {
       // The build folder.
-      path: isEnvProduction ? paths.appBuild : undefined,
+      path: paths.appBuild,
       // Add /* filename */ comments to generated require()s in the output.
       pathinfo: isEnvDevelopment,
       // There will be one main bundle, and one file per asynchronous chunk.
