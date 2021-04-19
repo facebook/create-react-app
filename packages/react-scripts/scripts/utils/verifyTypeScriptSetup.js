56698a2ec0a465ddfb175e296fbc55e64863ee74
sander <sander@wieni.be>
2021-04-19 17:24:13 +0200

    Change appTypeDeclarations to @wieni/magic

--- packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
+++ packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
@@ -262,7 +262,7 @@ function verifyTypeScriptSetup() {
   if (!fs.existsSync(paths.appTypeDeclarations)) {
     fs.writeFileSync(
       paths.appTypeDeclarations,
-      `/// <reference types="react-scripts" />${os.EOL}`
+      `/// <reference types="@wieni/magic" />${os.EOL}`
     );
   }
 }
