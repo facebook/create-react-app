4bb31ba513c6111a0aeb96cf34ec4922109d1fb2
jayson991 <jaysonwu991@outlook.com>
2021-05-14 20:27:19 +0800

    [LEOP-23]: Add thread-loader to speed up webpack workflow (#93)
    * Backpackify
    
    * add thread-loader to webpack config
    
    * add thread-loader to webpack SSR config but comment that out to not use
    
    * add CHANGELOG and upgrade version to v9.1.1
    
    * restore build-brs.yml formation as fork branch
    
    Co-authored-by: Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
    Co-authored-by: Jayson Wu <jayson.wu@skyscanner.net>

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -39,6 +39,8 @@ const getCacheIdentifier = require('react-dev-utils/getCacheIdentifier');
 // @remove-on-eject-end
 const createEnvironmentHash = require('./webpack/persistentCache/createEnvironmentHash');
 
+const appPackageJson = require(paths.appPackageJson);
+
 const sassFunctions = require('bpk-mixins/sass-functions');
 const camelCase = require('lodash/camelCase');
 const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
@@ -546,6 +548,10 @@ module.exports = function (webpackEnv) {
               test: /\.(js|mjs)$/,
               exclude: /@babel(?:\/|\\{1,2})runtime/,
               use: [
+                {
+                  loader: require.resolve('thread-loader'),
+                  options: jsWorkerPool,
+                },
                 isEnvDevelopment && {
                   loader: require.resolve('cache-loader'),
                   options: {
