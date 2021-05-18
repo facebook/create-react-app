28aa67291833413997ba66f27a43ff6bb8f14271
Morten N.O. Henriksen <mh@gi2.dk>
2021-05-18 20:59:46 +0200

    Fix missing wds socket path update
    accordingly to review by @xiaokekeT

--- packages/react-dev-utils/WebpackDevServerUtils.js
+++ packages/react-dev-utils/WebpackDevServerUtils.js
@@ -320,7 +320,7 @@ function prepareProxy(proxy, appPublicFolder, servedPathname) {
   // If proxy is specified, let it handle any request except for
   // files in the public folder and requests to the WebpackDevServer socket endpoint.
   // https://github.com/facebook/create-react-app/issues/6720
-  const sockPath = process.env.WDS_SOCKET_PATH || '/sockjs-node';
+  const sockPath = process.env.WDS_SOCKET_PATH || '/ws';
   const isDefaultSockHost = !process.env.WDS_SOCKET_HOST;
   function mayProxy(pathname) {
     const maybePublicPath = path.resolve(
