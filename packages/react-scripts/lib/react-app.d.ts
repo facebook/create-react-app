68a42f124d924e92eb6249265d2fcca8e916804f
Jens Birkbak Aagaard <teamtunfiske@gmail.com>
2021-05-24 20:00:14 +0200

    Add common audio container support

--- packages/react-scripts/lib/react-app.d.ts
+++ packages/react-scripts/lib/react-app.d.ts
@@ -44,6 +44,36 @@ declare module '*.webp' {
     export default src;
 }
 
+declare module '*.webm' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.ogg' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.mp3' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.mp4' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.flac' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.wav' {
+    const src: string;
+    export default src;
+}
+
 declare module '*.svg' {
   import * as React from 'react';
 
