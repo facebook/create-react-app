933e46b530a8a2227d179dec03a6bdc0c2cd39f2
Supriya S <supriya.s@okcredit.in>
2021-05-27 08:39:37 +0530

    jasf

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -15,6 +15,8 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
+console.log(process.env);
+
 const spawn = require('react-dev-utils/crossSpawn');
 const args = process.argv.slice(2);
 
