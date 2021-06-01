68bb7353bab12487689d71ebe59e43c58b77f9af
laiweizhen <laiweizhen@imdada.cn>
2021-06-01 17:31:29 +0800

    File path processing first

--- packages/create-react-app/createReactApp.js
+++ packages/create-react-app/createReactApp.js
@@ -751,12 +751,6 @@ function getPackageInfo(installPackage) {
     return Promise.resolve({
       name: installPackage.match(/([^/]+)\.git(#.*)?$/)[1],
     });
-  } else if (installPackage.match(/.+@/)) {
-    // Do not match @scope/ when stripping off @version or @tag
-    return Promise.resolve({
-      name: installPackage.charAt(0) + installPackage.substr(1).split('@')[0],
-      version: installPackage.split('@')[1],
-    });
   } else if (installPackage.match(/^file:/)) {
     const installPackagePath = installPackage.match(/^file:(.*)?$/)[1];
     const { name, version } = require(path.join(
@@ -764,6 +758,12 @@ function getPackageInfo(installPackage) {
       'package.json'
     ));
     return Promise.resolve({ name, version });
+  } else if (installPackage.match(/.+@/)) {
+    // Do not match @scope/ when stripping off @version or @tag
+    return Promise.resolve({
+      name: installPackage.charAt(0) + installPackage.substr(1).split('@')[0],
+      version: installPackage.split('@')[1],
+    });
   }
   return Promise.resolve({ name: installPackage });
 }
