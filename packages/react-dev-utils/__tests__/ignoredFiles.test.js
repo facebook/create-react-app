027b95b713f3244842076a2efb46c25d4d50a2a6
Jørgen Sverre Lien Sellæg <jorgen.sellag@ibooking.no>
2021-06-01 16:54:13 +0200

    update ignoredFiles test to use anymatch as it does in webpack as well

--- packages/react-dev-utils/__tests__/ignoredFiles.test.js
+++ packages/react-dev-utils/__tests__/ignoredFiles.test.js
@@ -9,11 +9,13 @@
 
 const ignoredFiles = require('../ignoredFiles');
 
+const anymatch = require('anymatch');
+
 describe('ignore watch files regex', () => {
   it('normal file', () => {
     const appSrc = '/root/src/';
-    const isIgnored = ignoredFiles(appSrc).test('/foo');
-    const isIgnoredInSrc = ignoredFiles(appSrc).test('/root/src/foo');
+    const isIgnored = anymatch(ignoredFiles(appSrc), '/foo');
+    const isIgnoredInSrc = anymatch(ignoredFiles(appSrc), '/root/src/foo');
 
     expect(isIgnored).toBe(false);
     expect(isIgnoredInSrc).toBe(false);
@@ -21,15 +23,19 @@ describe('ignore watch files regex', () => {
 
   it('node modules', () => {
     const appSrc = '/root/src/';
-    const isIgnored = ignoredFiles(appSrc).test('/root/node_modules/foo');
+    const isIgnored = anymatch(ignoredFiles(appSrc), '/root/node_modules/foo');
 
     expect(isIgnored).toBe(true);
   });
 
   it('node modules inside source directory', () => {
     const appSrc = '/root/src/';
-    const isIgnored = ignoredFiles(appSrc).test('/root/src/node_modules/foo');
-    const isIgnoredMoreThanOneLevel = ignoredFiles(appSrc).test(
+    const isIgnored = anymatch(
+      ignoredFiles(appSrc),
+      '/root/src/node_modules/foo'
+    );
+    const isIgnoredMoreThanOneLevel = anymatch(
+      ignoredFiles(appSrc),
       '/root/src/bar/node_modules/foo'
     );
 
@@ -39,7 +45,8 @@ describe('ignore watch files regex', () => {
 
   it('path contains source directory', () => {
     const appSrc = '/root/src/';
-    const isIgnored = ignoredFiles(appSrc).test(
+    const isIgnored = anymatch(
+      ignoredFiles(appSrc),
       '/bar/root/src/node_modules/foo'
     );
 
@@ -48,7 +55,10 @@ describe('ignore watch files regex', () => {
 
   it('path starts with source directory', () => {
     const appSrc = '/root/src/';
-    const isIgnored = ignoredFiles(appSrc).test('/root/src2/node_modules/foo');
+    const isIgnored = anymatch(
+      ignoredFiles(appSrc),
+      '/root/src2/node_modules/foo'
+    );
 
     expect(isIgnored).toBe(true);
   });
