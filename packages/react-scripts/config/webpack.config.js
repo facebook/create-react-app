09cb3d2733420339465bc46103f4dd4417689544
Morten N.O. Henriksen <mh@gi2.dk>
2021-07-17 14:04:37 +0200

    Fix html-webpack-plugin asset module support output
    ref: https://github.com/jantimon/html-webpack-plugin/issues/1589#issuecomment-830845614

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -597,7 +597,7 @@ module.exports = function (webpackEnv) {
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
-              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+              exclude: [/^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
               type: 'asset/resource',
             },
             // ** STOP ** Are you adding a new loader?
