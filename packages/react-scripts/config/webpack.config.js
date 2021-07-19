ce6395caaeebcfc29edef3a8e71f655162158a6f
Jayson Wu <jaysonwu@skyscanner.net>
2021-07-19 19:43:13 +0800

    [LEOP-55]: Apply cache-loader on CI (#99)
    * apply cache-loader on ci
    
    * fix spell of cache-loader
    
    * only using SHOULD_USE_CACHE_LOADER to control and restore formation
    
    * restore webpack config formation
    
    Co-authored-by: Jayson Wu <jayson.wu@skyscanner.net>

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -185,12 +185,6 @@ module.exports = function (webpackEnv) {
           ? { publicPath: '../../' }
           : {},
       },
-      isEnvDevelopment && {
-        loader: require.resolve('cache-loader'),
-        options: {
-          cacheDirectory: paths.cacheLoaderDir,
-        },
-      },
       {
         loader: require.resolve('css-loader'),
         options: cssOptions,
@@ -614,12 +608,6 @@ module.exports = function (webpackEnv) {
                   loader: require.resolve('thread-loader'),
                   options: jsWorkerPool,
                 },
-                isEnvDevelopment && {
-                  loader: require.resolve('cache-loader'),
-                  options: {
-                    cacheDirectory: paths.cacheLoaderDir,
-                  },
-                },
                 {
                   loader: require.resolve('babel-loader'),
                   options: {
@@ -742,49 +730,39 @@ module.exports = function (webpackEnv) {
             {
               test: /\.(js|mjs)$/,
               exclude: /@babel(?:\/|\\{1,2})runtime/,
-              use: [
-                isEnvDevelopment && {
-                  loader: require.resolve('cache-loader'),
-                  options: {
-                    cacheDirectory: paths.cacheLoaderDir,
-                  },
-                },
-                {
-                  loader: require.resolve('babel-loader'),
-                  options: {
-                    babelrc: false,
-                    configFile: false,
-                    compact: false,
-                    presets: [
-                      [
-                        require.resolve('babel-preset-react-app/dependencies'),
-                        { helpers: true },
-                      ],
-                    ],
-                    cacheDirectory: true,
-                    // See #6846 for context on why cacheCompression is disabled
-                    cacheCompression: false,
-                    // @remove-on-eject-begin
-                    cacheIdentifier: getCacheIdentifier(
-                      isEnvProduction
-                        ? 'production'
-                        : isEnvDevelopment && 'development',
-                      [
-                        'babel-plugin-named-asset-import',
-                        'babel-preset-react-app',
-                        'react-dev-utils',
-                        'react-scripts',
-                      ]
-                    ),
-                    // @remove-on-eject-end
-                    // Babel sourcemaps are needed for debugging into node_modules
-                    // code.  Without the options below, debuggers like VSCode
-                    // show incorrect code and set breakpoints on the wrong lines.
-                    sourceMaps: shouldUseSourceMap,
-                    inputSourceMap: shouldUseSourceMap,
-                  },
-                },
-              ].filter(Boolean),
+              loader: require.resolve('babel-loader'),
+              options: {
+                babelrc: false,
+                configFile: false,
+                compact: false,
+                presets: [
+                  [
+                    require.resolve('babel-preset-react-app/dependencies'),
+                    { helpers: true },
+                  ],
+                ],
+                cacheDirectory: true,
+                // See #6846 for context on why cacheCompression is disabled
+                cacheCompression: false,
+                // @remove-on-eject-begin
+                cacheIdentifier: getCacheIdentifier(
+                  isEnvProduction
+                    ? 'production'
+                    : isEnvDevelopment && 'development',
+                  [
+                    'babel-plugin-named-asset-import',
+                    'babel-preset-react-app',
+                    'react-dev-utils',
+                    'react-scripts',
+                  ]
+                ),
+                // @remove-on-eject-end
+                // Babel sourcemaps are needed for debugging into node_modules
+                // code.  Without the options below, debuggers like VSCode
+                // show incorrect code and set breakpoints on the wrong lines.
+                sourceMaps: shouldUseSourceMap,
+                inputSourceMap: shouldUseSourceMap,
+              },
             },
             // "postcss" loader applies autoprefixer to our CSS.
             // "css" loader resolves paths in CSS and adds assets as dependencies.
