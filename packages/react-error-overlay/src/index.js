2c77dd769540cb4819764cbe2090e1401024750f
Morten N.O. Henriksen <mh@gi2.dk>
2021-08-02 22:19:54 +0200

    Ignore react-error-overlay lint issue

--- packages/react-error-overlay/src/index.js
+++ packages/react-error-overlay/src/index.js
@@ -15,7 +15,8 @@ import { applyStyles } from './utils/dom/css';
 // Importing iframe-bundle generated in the pre build step as
 // a text using webpack raw-loader. See webpack.config.js file.
 // $FlowFixMe
-import iframeScript from './iframeScript';
+// eslint-disable-next-line import/no-unresolved
+import iframeScript from 'iframeScript';
 
 import type { ErrorRecord } from './listenToRuntimeErrors';
 import type { ErrorLocation } from './utils/parseCompileError';
