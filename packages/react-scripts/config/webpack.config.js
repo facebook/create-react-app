71e2574e14e84cce8f3e39e89fe9ccdea7c80439
Darrel Fox <fox.darrel@googlemail.com>
2021-09-23 16:34:05 +0100

    [NO JIRA] Ident creation using Project Name (#103)
    * Add hashes that also factor in project name
    
    * added comment
    
    * commit runaway formatting
    
    Co-authored-by: Darrel Fox <darrel.fox@skyscanner.net>

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -26,7 +26,6 @@ const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
-const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 // const ESLintPlugin = require('eslint-webpack-plugin');
 const paths = require('./paths');
 const modules = require('./modules');
@@ -54,6 +53,10 @@ const jsWorkerPool = {
 };
 
 const appPackageJson = require(paths.appPackageJson);
+
+const getCSSModuleLocalIdent = require('../utils/getCSSModuleLocalIdentWithProjectName')(
+  appPackageJson.name
+);
 
 const sassFunctions = require('bpk-mixins/sass-functions');
 const camelCase = require('lodash/camelCase');
