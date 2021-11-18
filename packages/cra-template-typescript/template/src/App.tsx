ac5ebc524477d5375fd549d9eb7ae35587a37734
Atef Ben Ali <atefBB@users.noreply.github.com>
2021-11-18 13:50:31 +0100

    refactor: simplify `export` statement in `app` component

--- packages/cra-template-typescript/template/src/App.tsx
+++ packages/cra-template-typescript/template/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
