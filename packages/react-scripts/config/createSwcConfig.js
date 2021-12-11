2af14b965e1554856a184d4a757ea9244f8591db
Morten N.O. Henriksen <mh@gi2.dk>
2021-12-11 16:31:37 +0100

    Load tsconfig or jsconfig as base for swc configuration

--- packages/react-scripts/config/createSwcConfig.js
+++ packages/react-scripts/config/createSwcConfig.js
@@ -7,6 +7,11 @@
  */
 // @remove-on-eject-end
 'use strict';
+const fs = require('fs');
+const path = require('path');
+const merge = require('lodash.merge');
+const JSON5 = require('json5');
+const paths = require('./paths');
 const hasJsxRuntime = require('./hasJsxRuntime');
 
 module.exports = function createSwcConfig({
@@ -15,50 +20,135 @@ module.exports = function createSwcConfig({
   isEnvProduction = false,
   shouldUseReactRefresh = false,
 } = {}) {
-  const env =
-    isEnvDevelopment | isEnvProduction
+  const projectSwcConfig = mapProjectConfigToSwcConfig(getProjectConfig());
+
+  const generalOptions = {
+    jsx: true,
+    runtime: hasJsxRuntime ? 'automatic' : 'clasic',
+    sourceMaps: shouldUseSourceMap,
+    refresh: shouldUseReactRefresh,
+    useBroserlists: isEnvDevelopment | isEnvProduction,
+    development: isEnvDevelopment,
+  };
+
+  return {
+    ecmascript: merge(
+      {},
+      projectSwcConfig,
+      getBaseConfiguration({
+        ...generalOptions,
+      })
+    ),
+    typescript: merge(
+      {},
+      projectSwcConfig,
+      getBaseConfiguration({
+        ...generalOptions,
+        syntax: 'typescript',
+      })
+    ),
+  };
+};
+
+function getBaseConfiguration({
+  runtime = 'clasic',
+  syntax = 'ecmascript',
+  jsx = false,
+  sourceMaps = false,
+  refresh = false,
+  useBroserlists = false,
+  development = false,
+}) {
+  return {
+    sourceMaps,
+    jsc: {
+      parser: {
+        [syntax === 'typescript' ? 'tsx' : 'jsx']: jsx,
+        syntax,
+      },
+      transform: {
+        react: {
+          runtime,
+          refresh,
+          development,
+        },
+      },
+    },
+    env: useBroserlists
       ? {
           mode: 'entry',
           coreJs: 3,
         }
-      : {};
-  return {
-    ecmascript: {
-      sourceMaps: shouldUseSourceMap,
-      jsc: {
-        externalHelpers: true,
-        parser: {
-          jsx: true,
-          syntax: 'ecmascript',
-        },
-        transform: {
-          react: {
-            runtime: hasJsxRuntime ? 'automatic' : 'clasic',
-            refresh: shouldUseReactRefresh,
-            development: isEnvDevelopment,
-          },
-        },
-      },
-      env,
-    },
-    typescript: {
-      sourceMaps: shouldUseSourceMap,
-      jsc: {
-        externalHelpers: true,
-        parser: {
-          tsx: true,
-          syntax: 'typescript',
-          development: isEnvDevelopment,
-        },
-        transform: {
-          react: {
-            runtime: hasJsxRuntime ? 'automatic' : 'clasic',
-            refresh: shouldUseReactRefresh,
-            development: isEnvDevelopment,
-          },
-        },
-      },
-    },
-    env,
+      : {},
   };
-};
+}
+
+function mapProjectConfigToSwcConfig(projectConfig = {}) {
+  const {
+    compilerOptions: {
+      target,
+      importHelpers,
+      experimentalDecorators,
+      emitDecoratorMetadata,
+      baseUrl,
+      paths,
+    } = {},
+  } = projectConfig;
+  const result = { jsc: { externalHelpers: true, parser: {}, transform: {} } };
+
+  if (target != null) {
+    result.jsc.target = target;
+  }
+  if (importHelpers != null) {
+    result.jsc.externalHelpers = importHelpers;
+  }
+  if (experimentalDecorators != null) {
+    result.jsc.parser.decorators = experimentalDecorators;
+  }
+  if (emitDecoratorMetadata != null) {
+    result.jsc.transform.decoratorMetadata = emitDecoratorMetadata;
+  }
+
+  if (baseUrl != null) {
+    result.jsc.baseUrl = baseUrl;
+    if (paths != null) {
+      result.jsc.paths = paths;
+    }
+  }
+
+  return result;
+}
+
+function getProjectConfig(configPath, loadedPaths = []) {
+  const configPaths =
+    configPath != null ? [configPath] : [paths.appTsConfig, paths.appJsConfig];
+  const [projectConfigPath] = configPaths.filter(p => fs.existsSync(p));
+  if (projectConfigPath == null) {
+    return {};
+  }
+  if (loadedPaths.includes(projectConfigPath)) {
+    return {};
+  }
+  try {
+    const projectConfig = JSON5.parse(
+      fs.readFileSync(projectConfigPath, 'utf-8')
+    );
+    if (projectConfig.extends != null) {
+      const baseConfig = path.resolve(
+        path.dirname(projectConfigPath),
+        projectConfig.extends.replace('/', path.sep)
+      );
+      delete projectConfig.extends;
+      return merge(
+        getProjectConfig(baseConfig, [...loadedPaths, projectConfigPath]),
+        projectConfig
+      );
+    }
+    return projectConfig;
+  } catch (error) {
+    console.error(
+      `Error: Failed to load "${projectConfigPath}", error: "${error.message}"`
+    );
+    process.exit(1);
+  }
+}
