d232a648ae96091a1e32d38753dfae23006c8c57
Bnaya Peretz <me@bnaya.net>
2022-01-06 12:14:24 +0200

    wip

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -794,9 +794,16 @@ module.exports = function (webpackEnv) {
       // Ignore warnings raised by source-map-loader.
       // some third party packages may ship miss-configured sourcemaps, that interrupts the build
       // See: https://github.com/facebook/create-react-app/discussions/11278#discussioncomment-1780169
+      /**
+       *
+       * @param {import('webpack').WebpackError} warning
+       * @returns {boolean}
+       */
       function ignoreSourcemapsloaderWarnings(warning) {
         return (
+          warning.module &&
           warning.module.resource.includes('node_modules') &&
+          warning.details &&
           warning.details.includes('source-map-loader')
         );
       },
