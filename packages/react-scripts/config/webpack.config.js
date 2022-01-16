d9199924b6531397c668cf7a8da023ff11bcb77e
iwfan <wangfan@jinshuju.net>
2022-01-16 17:18:15 +0800

    Update public path for development

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -608,7 +608,9 @@ module.exports = function (webpackEnv) {
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
-      publicPath: paths.publicUrlOrPath,
+      publicPath: isEnvProduction
+        ? paths.publicUrlOrPath
+        : 'http://localhost:3001/',
       // Point sourcemap entries to original disk location (format as URL on Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
         ? info =>
