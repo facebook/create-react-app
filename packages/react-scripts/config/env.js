4a56d0eddb54f5aea5c36ff26bb1baf01506f28c
温州-全栈-闲人 <50684820+idler8@users.noreply.github.com>
2022-01-18 10:22:25 +0800

    Update env.js
    将`process.env`设置为'Object'，以防止未定义的环境变量导致的安全问题。

--- packages/react-scripts/config/env.js
+++ packages/react-scripts/config/env.js
@@ -99,12 +99,11 @@ function getClientEnvironment(publicUrl) {
       }
     );
   // Stringify all values so we can feed into webpack DefinePlugin
-  const stringified = {
-    'process.env': Object.keys(raw).reduce((env, key) => {
-      env[key] = JSON.stringify(raw[key]);
-      return env;
-    }, {}),
-  };
+  const stringified = Object.keys(raw).reduce((env, key) => {
+    env['process.env.' + key] = JSON.stringify(raw[key]);
+    return env;
+  }, {});
+  stringified["process.env"] = 'Object()';
 
   return { raw, stringified };
 }
