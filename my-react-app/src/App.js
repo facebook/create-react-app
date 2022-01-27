d6090fbec6e91b70bc660e7d5dcc7457e3c747e0
Hassan Akolade <akoladeakinwande@hotmail.com>
2022-01-27 01:05:18 +0000

    first component created

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './componment/Expenseitems'
+import Expenseitems from './componment/Expenseitems';
+
+ const newItem = new Expenseitems("Avengers") 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{newItem}
     </div>
   );
 }
---
+++ my-react-app/src/componment/Expenseitems.js
@@ -0,0 +1,9 @@
+
+let age = 20
+function Expenseitems(name){
+
+
+    return <div><h1>My first app 2022</h1>{name}</div>
+}
+
+export default Expenseitems
