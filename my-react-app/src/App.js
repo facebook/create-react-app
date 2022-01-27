7870001fc12c6dcaa08aa06745a6a252453a45fb
Hassan Akolade <akoladeakinwande@hotmail.com>
2022-01-27 01:35:37 +0000

    movie created

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import './components/Expenseitems'
 import Expenseitems from './components/Expenseitems';
 
- const newItem = new Expenseitems("Avengers") 
+const newItem = new Expenseitems("=======Movie 1======") 
+const newItem2 = new Expenseitems("=======Movie 1======") 
 function App() {
   return (
     <div className="App">
-{newItem}
+{newItem}{newItem2}
     </div>
   );
 }
--- my-react-app/src/components/Expenseitems.js
+++ my-react-app/src/components/Expenseitems.js
@@ -1,9 +1,24 @@
 import './Expenseitems.css'
-let age = 20
+
 function Expenseitems(name){
+const rating = 5
+const movieName = "Avengers"
+const info = "Superhero movie"
+    return  <div> 
+        
+        <h1>My first app 2022</h1>{name}
+        <div className="expense-item">
+        
 
+ 
 
-    return <div><h1>My first app 2022</h1>{name}</div>
+        <div className="expense-item__description">{movieName}</div>
+        <div className=""> <h2>{info}</h2></div>
+        <div className='expense-item__price'>{rating}</div>
+
+        
+      </div>
+      </div>
 }
 
 export default Expenseitems
