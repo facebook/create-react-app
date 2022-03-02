079b9797c94b23d5f41292afa8ccaf4a7e27d817
Salley Liu <salley.liu@skyscanner.net>
2022-03-02 11:11:45 +0800

    fix not find encodebase64 function error

--- packages/react-scripts/backpack-addons/sassFunctions.js
+++ packages/react-scripts/backpack-addons/sassFunctions.js
@@ -3,10 +3,12 @@ const sass = require('node-sass');
 
 module.exports = {
   sassOptions: {
-    'encodebase64($string)': str => {
-      const buffer = Buffer.from(str.getValue());
-
-      return sass.types.String(buffer.toString('base64'));
+    functions: {
+      'encodebase64($string)': str => {
+        const buffer = Buffer.from(str.getValue());
+  
+        return sass.types.String(buffer.toString('base64'));
+      }
     }
   }
 };
