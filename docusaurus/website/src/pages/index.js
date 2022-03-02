5bbf32caaff8b25bce546e7db81a237a2ea54eaf
dmitryvinn <dmitryvinn@gmail.com>
2022-03-01 22:45:09 -0800

    Adding social banner in support of Ukraine

--- docusaurus/website/src/pages/index.js
+++ docusaurus/website/src/pages/index.js
@@ -47,7 +47,7 @@ function Home() {
       <div>
           <div className={styles.banner}>
             Support Ukraine 🇺🇦{' '}
-            <Link to="http://www.facebook.com/fundraisers">
+            <Link to="https://opensource.facebook.com/support-ukraine">
               Help Provide Humanitarian Aid to Ukraine
             </Link>
             .
