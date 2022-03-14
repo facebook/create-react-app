28dc21a0c0ee9ba4bcd89210643f9cddba030879
inrmanish <inmanish868@gmail.com>
2022-03-14 16:45:12 +0530

    Solve Yarn Start

---
+++ solveyarnstart.jsx
@@ -0,0 +1,6 @@
+// I had this problem. I figured the best way is this.
+
+npm i -g create-react-app
+create-react-app my-react-app //or whatever you want your project to be
+cd my-react-app
+yarn start
