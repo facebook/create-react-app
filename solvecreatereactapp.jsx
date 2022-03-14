de9e0833fd378c5fd4646c47ecff34da55ce9563
inrmanish <inmanish868@gmail.com>
2022-03-14 16:24:36 +0530

    Solve Create React App

---
+++ solvecreatereactapp.jsx
@@ -0,0 +1,16 @@
+// From https://create-react-app.dev/docs/getting-started#creating-an-app
+
+npx create-react-app my-app
+
+// (npx comes with npm 5.2+ and higher)
+
+// But you used
+
+// `create-react-app my-app`
+// If you use npm 5.1 or earlier, you can't use npx. Instead, install create-react-app globally:
+
+npm install -g create-react-app
+
+// Now you can run:
+
+create-react-app my-app
