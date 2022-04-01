7de66fad04f11ed04e46116418fafe9e360c8bea
Martin Andrej Johan Grönlund <3044707+majg0@users.noreply.github.com>
2022-04-01 10:34:20 +0200

    Add WDS_SOCKET_PROTOCOL env var to WDS
    For when running WDS on HTTP and localhost while connecting via https proxy and
    - rather not make the WDS run HTTPS
    - rather not run the web socket traffic through the proxy

--- packages/react-dev-utils/webpackHotDevClient.js
+++ packages/react-dev-utils/webpackHotDevClient.js
@@ -59,7 +59,7 @@ if (module.hot && typeof module.hot.dispose === 'function') {
 // Connect to WebpackDevServer via a socket.
 var connection = new WebSocket(
   url.format({
-    protocol: window.location.protocol === 'https:' ? 'wss' : 'ws',
+    protocol: process.env.WDS_SOCKET_PROTOCOL || (window.location.protocol === 'https:' ? 'wss' : 'ws'),
     hostname: process.env.WDS_SOCKET_HOST || window.location.hostname,
     port: process.env.WDS_SOCKET_PORT || window.location.port,
     // Hardcoded in WebpackDevServer
