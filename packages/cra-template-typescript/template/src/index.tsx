fe93ea9f68ce1900193b99b8692fc0d709401485
이정환 <king199777@gmail.com>
2022-04-02 15:59:29 +0900

    Update cra-template for react18

--- packages/cra-template-typescript/template/src/index.tsx
+++ packages/cra-template-typescript/template/src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElment = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElment);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
