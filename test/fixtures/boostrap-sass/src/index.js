510bec6e2786ea37c5276a92491c9673e6233d45
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-06 12:13:49 +0800

    Delete test directory

--- test/fixtures/boostrap-sass/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.sass';
-
-ReactDOM.render(<div />, document.getElementById('root'));
--- test/fixtures/issue-5176-flow-class-properties/src/App.test.js
+++
@@ -1,6 +0,0 @@
-import App from './App';
-
-it('creates instance without', () => {
-  const app = new App();
-  expect(app.foo()).toBe('bar');
-});
