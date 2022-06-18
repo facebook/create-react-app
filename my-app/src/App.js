5f1d1aab9861e6b4ccfe5bb592d2ff86b6c96951
Chaerin Kim <decentralized0212@gmail.com>
2022-06-18 16:27:55 +0900

    앱 파일 생성

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
