f566b609ce1e5dd8cfdc282b84cfc56a56d80998
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2022-08-13 12:27:40 +0200

    replace identity-obj-proxy by jest-scss-transform

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -55,7 +55,7 @@ module.exports = (resolve, rootDir, isEjecting) => {
     modulePaths: modules.additionalModulePaths || [],
     moduleNameMapper: {
       '^react-native$': 'react-native-web',
-      '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+      '^.+\\.module\\.(css|sass|scss)$': 'jest-scss-transform',
       ...(modules.jestAliases || {}),
     },
     moduleFileExtensions: [...paths.moduleFileExtensions, 'node'].filter(
