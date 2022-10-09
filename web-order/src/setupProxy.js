8f83fe3a57d17b5eaacf2d6d853efafa02796a6a
_____Dy_____ <61171836+Mr-Dy986@users.noreply.github.com>
2022-10-09 21:10:00 +0000

    docs: fix link of "Building for Relative Paths" (#12691) <!-- Thank you for sending the PR!
    If you changed any code, please provide us with clear instructions on how you verified your changes work. Bonus points for screenshots!
    
    Happy contributing!
    -->
    
    <!--
    Thank you for sending the PR!
    
    If you changed any code, please provide us with clear instructions on how you verified your changes work. Bonus points for screenshots!
    
    Happy contributing!
    -->

---
+++ web-order/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+};
