3f2e7f2f3437d764da6f6e8ec36d88991a6dff92
Nnadozie Okeke <dozie.okeke@kindhealth.co>
2022-10-19 15:26:56 +0100

    bugfix - process not defined

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -367,6 +367,12 @@ module.exports = function (webpackEnv) {
     module: {
       strictExportPresence: true,
       rules: [
+        {
+          test: /\.m?js$/,
+          resolve: {
+            fullySpecified: false
+          },
+        },
         // Handle node_modules packages that contain sourcemaps
         shouldUseSourceMap && {
           enforce: 'pre',
