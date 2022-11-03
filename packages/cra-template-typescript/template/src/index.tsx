50200c527153be5d13a123b9bffceec37cb75c04
Nick McCurdy <nick@nickmccurdy.com>
2022-11-03 07:20:10 -0400

    Format

--- packages/cra-template-typescript/template/src/index.tsx
+++ packages/cra-template-typescript/template/src/index.tsx
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')!
-);
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <App />
