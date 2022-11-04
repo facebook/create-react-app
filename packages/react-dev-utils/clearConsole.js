bc0801b23aef5356cfbf1780e8ac5e5d830b1025
Allan Bogh <ajbogh@allanbogh.com>
2022-11-03 20:15:26 -0700

    Update clearConsole.js
    Add DISABLE_CLEAR_CONSOLE environment variable to allow developers to disable the clearing of the console during app startup. Useful if running multiple services at the same time and one service fails during startup. 
    
    Suggested usage in package.json: `DISABLE_CLEAR_CONSOLE=true npm start`
    
    Note: this variable will prevent any usage of clearConsole.
    
    ref #611

--- packages/react-dev-utils/clearConsole.js
+++ packages/react-dev-utils/clearConsole.js
@@ -8,6 +8,10 @@
 'use strict';
 
 function clearConsole() {
+  if(process.env.DISABLE_CLEAR_CONSOLE) {
+    return;
+  }
+  
   process.stdout.write(
     process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H'
   );
