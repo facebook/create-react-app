bb1783a20834d0637f06131a484b8bd00d9c39d1
Perry Poon <plrthink@jinshuju.net>
2022-12-04 00:18:18 +0800

    try to solve chunkloaderror with retry

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -43,6 +43,7 @@ const webpackOverrideConfig = require(path.resolve(
   paths.appPath,
   'webpack.override'
 ));
+const { RetryChunkLoadPlugin } = require('webpack-retry-chunk-load-plugin');
 
 // @remove-on-eject-begin
 const getCacheIdentifier = require('react-dev-utils/getCacheIdentifier');
@@ -802,6 +803,7 @@ module.exports = function (webpackEnv) {
             console.log('Sentry CLI Plugin: ' + err.message);
           },
         }),
+      new RetryChunkLoadPlugin({ maxRetries: 5, }),
     ].filter(Boolean);
 
   const config = {
