443e3ccbbd3dfb0186706567135c1804ac63531a
Jayson Wu <jaysonwu@skyscanner.net>
2023-02-09 13:43:45 +0800

    use PR version of BRS for testing

---
+++ tasks/e2e-brs.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Start in tasks/ even if run from root directory
+cd "$(dirname "$0")"
+
+# App temporary location
+# http://unix.stackexchange.com/a/84980
+temp_app_path=`mktemp -d 2>/dev/null || mktemp -d -t 'temp_app_path'`
+
+# Load functions for working with local NPM registry (Verdaccio)
+source local-registry.sh
+
+function cleanup {
+  echo 'Cleaning up.'
+  cd "$root_path"
+  # Uncomment when snapshot testing is enabled by default:
+  # rm ./packages/react-scripts/template/src/__snapshots__/App.test.js.snap
+  rm -rf "$temp_app_path"
+  # Restore the original NPM and Yarn registry URLs and stop Verdaccio
+  stopLocalRegistry
+}
+
+# Error messages are redirected to stderr
+function handle_error {
+  echo "$(basename $0): ERROR! An error was encountered executing line $1." 1>&2;
+  cleanup
+  echo 'Exiting with error.' 1>&2;
+  exit 1
+}
+
+function handle_exit {
+  cleanup
+  echo 'Exiting without error.' 1>&2;
+  exit
+}
+
+# Check for the existence of one or more files.
+function exists {
+  for f in $*; do
+    test -e "$f"
+  done
+}
+
+# Exit the script with a helpful error message when any error is encountered
+trap 'set +x; handle_error $LINENO $BASH_COMMAND' ERR
+
+# Cleanup before exit on any termination signal
+trap 'set +x; handle_exit' SIGQUIT SIGTERM SIGINT SIGKILL SIGHUP
+
+# Echo every command being executed
+set -x
+
+# Go to root
+cd ..
+root_path=$PWD
+
+npm pack --prefix packages/react-scripts
+
+# Install the app in a temporary location
+cd $temp_app_path
+npx create-react-app test-app --scripts-version=@skyscanner/backpack-react-scripts --template @skyscanner/backpack --use-npm
+
+# Enter the app directory
+cd test-app
+
+npm i "$root_path"/packages/react-scripts/skyscanner-backpack-react-scripts-10.0.4.tgz
+
+# Test the build
+npm run build
+# Check for expected output
+exists build/*.html
+exists build/static/js/*.js
+exists build/static/css/*.css
+# exists build/static/media/*.svg
+exists build/favicon.ico
+
+# Cleanup
+cleanup
