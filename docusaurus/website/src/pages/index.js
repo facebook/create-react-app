c1e128bb2085af486ab04699bed75b9215b0ee54
Anurag Kumar Singh <110897157+KumarAnurag03@users.noreply.github.com>
2023-03-24 00:07:31 +0530

    Update index.js

--- docusaurus/website/src/pages/index.js
+++ docusaurus/website/src/pages/index.js
@@ -99,7 +99,7 @@ function Home() {
                 className={styles.featureImage}
                 alt="Easy to get started in seconds"
                 src={
-                  'https://camo.githubusercontent.com/29765c4a32f03bd01d44edef1cd674225e3c906b/68747470733a2f2f63646e2e7261776769742e636f6d2f66616365626f6f6b2f6372656174652d72656163742d6170702f323762343261632f73637265656e636173742e737667'
+                  'https://www.propelleraero.com/dirtmate/'
                 }
               />
             </div>
