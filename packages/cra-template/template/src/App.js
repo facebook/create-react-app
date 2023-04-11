e6211ae65e448fbd1e3625ec1c1cf56a85c682d5
Mehdi <127740921+Mahdi-Hazrati@users.noreply.github.com>
2023-04-11 18:19:06 +0330

    Update "Learn react" url -> react.dev
    i just updated the url of create-react-app template to point correct url

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://react.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
