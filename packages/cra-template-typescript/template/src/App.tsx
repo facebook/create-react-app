714455f23957d3dde1cbb8020586c02ab9b1f3f0
Rohit Yadav <40739903+rohitsSpace@users.noreply.github.com>
2024-07-06 16:09:32 +0530

    Updates old link in App.tsx to correct link to React.dev

--- packages/cra-template-typescript/template/src/App.tsx
+++ packages/cra-template-typescript/template/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://react.dev/learn"
           target="_blank"
           rel="noopener noreferrer"
         >
