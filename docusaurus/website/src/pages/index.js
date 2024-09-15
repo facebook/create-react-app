42a21b33ca41d85b92a50df24b13eec78a2fe9f8
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-09-15 22:49:58 +0530

    fixed the bad request url at banner

--- docusaurus/website/src/pages/index.js
+++ docusaurus/website/src/pages/index.js
@@ -50,62 +50,62 @@ function Home() {
             className={clsx(styles.heroBannerLogo, 'margin-vert--md')}
             alt="Create React App logo"
             src={useBaseUrl('img/logo.svg')}
-          />
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          />{' '}
+          <h1 className="hero__title"> {siteConfig.title} </h1>{' '}
+          <p className="hero__subtitle"> {siteConfig.tagline} </p>{' '}
           <div className={styles.getStarted}>
             <Link
               className="button button--outline button--primary button--lg"
               to={useBaseUrl('docs/getting-started')}
             >
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </div>
+              Get Started{' '}
+            </Link>{' '}
+          </div>{' '}
+        </div>{' '}
+      </div>{' '}
       {features && features.length && (
         <div className={styles.features}>
           <div className="container">
             <div className="row">
+              {' '}
               {features.map(({ title, content }, idx) => (
                 <div key={idx} className={clsx('col col--4', styles.feature)}>
-                  <h2>{title}</h2>
-                  <p>{content}</p>
+                  <h2> {title} </h2> <p> {content} </p>{' '}
                 </div>
-              ))}
-            </div>
-          </div>
+              ))}{' '}
+            </div>{' '}
+          </div>{' '}
         </div>
-      )}
+      )}{' '}
       <div className={styles.gettingStartedSection}>
         <div className="container padding-vert--xl text--left">
           <div className="row">
             <div className="col col--4 col--offset-1">
-              <h2>Get started in seconds</h2>
+              <h2> Get started in seconds </h2>{' '}
               <p>
-                Whether you’re using React or another library, Create React App
-                lets you <strong>focus on code, not build tools</strong>.
+                Whether you’ re using React or another library, Create React App
+                lets you <strong> focus on code, not build tools </strong>.{' '}
                 <br />
                 <br />
-                To create a project called <i>my-app</i>, run this command:
-              </p>
+                To create a project called <i> my - app </i>, run this command:{' '}
+              </p>{' '}
               <CodeBlock className="language-sh">
-                npx create-react-app my-app
-              </CodeBlock>
+                npx create - react - app my - app{' '}
+              </CodeBlock>{' '}
               <br />
-            </div>
+            </div>{' '}
             <div className="col col--5 col--offset-1">
               <img
                 className={styles.featureImage}
                 alt="Easy to get started in seconds"
                 src={
-                  'https://camo.githubusercontent.com/29765c4a32f03bd01d44edef1cd674225e3c906b/68747470733a2f2f63646e2e7261776769742e636f6d2f66616365626f6f6b2f6372656174652d72656163742d6170702f323762343261632f73637265656e636173742e737667'
+                  'https://cdn.jsdelivr.net/gh/facebook/create-react-app@27b42ac/screencast.svg'
                 }
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+              />{' '}
+            </div>{' '}
+          </div>{' '}
+        </div>{' '}
+      </div>{' '}
       <div>
         <div className="container padding-vert--xl text--left">
           <div className="row">
@@ -114,22 +114,22 @@ function Home() {
                 className={styles.featureImage}
                 alt="Easy to update"
                 src={useBaseUrl('img/update.png')}
-              />
-            </div>
+              />{' '}
+            </div>{' '}
             <div className="col col--5 col--offset-1">
-              <h2>Easy to Maintain</h2>
+              <h2> Easy to Maintain </h2>{' '}
               <p>
-                Updating your build tooling is typically a daunting and
-                time-consuming task. When new versions of Create React App are
+                Updating your build tooling is typically a daunting and time -
+                consuming task.When new versions of Create React App are
                 released, you can upgrade using a single command:
-              </p>
+              </p>{' '}
               <CodeBlock className="language-sh">
-                npm install react-scripts@latest
-              </CodeBlock>
-            </div>
-          </div>
-        </div>
-      </div>
+                npm install react - scripts @latest{' '}
+              </CodeBlock>{' '}
+            </div>{' '}
+          </div>{' '}
+        </div>{' '}
+      </div>{' '}
     </Layout>
   );
 }
